328be05d6b2983a4bc84bdc04b9658a272aba783
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-07-05 00:04:28 +0530

    Time: 15 ms (99.96%) | Memory: 42.8 MB (68.63%) - LeetSync

--- power-of-three/power-of-three.java
+++ power-of-three/power-of-three.java
@@ -1,10 +1,8 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if (n==0)
-            return false;
-        else if (n==1)
+        if (n==1)
             return true;
-        else if (n%3!=0)
+        else if (n==0 || n%3!=0)
             return false;
 
         return isPowerOfThree(n/3);
