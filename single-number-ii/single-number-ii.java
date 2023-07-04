b5e5b98ecc50f8e80d02efe04600ed9ff7a7ef3a
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-07-04 19:51:03 +0530

    Time: 2 ms (81.65%) | Memory: 44.5 MB (5.94%) - LeetSync

--- single-number-ii/single-number-ii.java
+++ single-number-ii/single-number-ii.java
@@ -1,18 +1,16 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int res=0;
-        Map<Integer,Integer> map=new HashMap<>();
 
-        for (int val: nums)
-            map.put(val,map.getOrDefault(val,0)+1);
-
-        for (Map.Entry<Integer,Integer> m: map.entrySet())
+        for (int i=0;i<32;i++)
         {
-            if (m.getValue()==1)
-            {
-                res=m.getKey();
-                break;
-            }
+            int sum=0;
+
+            for (int val: nums)
+                sum+=(val>>i&1);
+            
+            if (sum%3!=0)
+                res=res|(1<<i);
         }
 
         return res;
