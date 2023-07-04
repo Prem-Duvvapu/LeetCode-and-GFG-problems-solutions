948cf8f621609757936b5734c25e39435013e63d
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-07-04 17:48:42 +0530

    Time: 4 ms (99.85%) | Memory: 56.2 MB (86.35%) - LeetSync

---
+++ continuous-subarrays/continuous-subarrays.java
@@ -0,0 +1,36 @@
+class Solution {
+    public long continuousSubarrays(int[] nums) {
+        long res=1;
+        int minVal=nums[0]-2;
+        int maxVal=nums[0]+2;
+        int j=0;
+
+        for (int i=1;i<nums.length;i++)
+        {
+                if (nums[i]>=minVal && nums[i]<=maxVal)
+                {
+                        minVal=Math.max(nums[i]-2,minVal);
+                        maxVal=Math.min(nums[i]+2,maxVal);
+                }
+                else
+                {
+                        j=i-1;
+                        minVal=nums[i]-2;
+                        maxVal=nums[i]+2;
+
+                        while (nums[j]>=(nums[i]-2) && nums[j]<=(nums[i]+2))
+                        {
+                                minVal=Math.max(nums[j]-2,minVal);
+                                maxVal=Math.min(nums[j]+2,maxVal);
+                                j--;
+                        }
+
+                        j++;
+                }
+
+                res+=(i-j+1);
+        }
+
+        return res;
+    }
+}
