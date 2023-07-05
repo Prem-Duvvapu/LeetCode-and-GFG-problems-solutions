17faa2af50cf817217bc2f0e019c8ea7ca21cbd7
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-07-05 08:37:32 +0530

    Time: 20 ms (81.68%) | Memory: 44.4 MB (26.62%) - LeetSync

---
+++ 4sum/4sum.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        long t=target;
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> quad = new ArrayList<>();
+        kSum(4, 0, target, nums, res, quad);
+        return res;
+    }
+    
+    private void kSum(int k, int start, long target, int[] nums, List<List<Integer>> res, List<Integer> quad) {
+        if (k != 2) {
+            for (int i = start; i < nums.length - k + 1; i++) {
+                if (i > start && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                quad.add(nums[i]);
+                kSum(k - 1, i + 1, target - nums[i], nums, res, quad);
+                quad.remove(quad.size() - 1);
+            }
+            return;
+        }
+        
+        // base case, two sum II
+        int left = start;
+        int right = nums.length - 1;
+        
+        while (left < right) {
+            if ((long)(nums[left] + nums[right]) < target) {
+                left++;
+            } else if ((long)(nums[left] + nums[right]) > target) {
+                right--;
+            } else {
+                List<Integer> temp = new ArrayList<>(quad);
+                temp.add(nums[left]);
+                temp.add(nums[right]);
+                res.add(temp);
+                left++;
+                right--;
+                
+                while (left < right && nums[left] == nums[left - 1]) {
+                    left++;
+                }
+            }
+        }
+    }
+}
