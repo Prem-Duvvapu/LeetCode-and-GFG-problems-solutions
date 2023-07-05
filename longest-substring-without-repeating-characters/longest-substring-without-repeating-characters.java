2e7e39f9196b936d9088e0e29eb9087ab7cc922d
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-07-05 08:05:07 +0530

    Time: 6 ms (88.82%) | Memory: 43.5 MB (68.25%) - LeetSync

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -8,10 +8,8 @@ class Solution {
         {
             char ch=s.charAt(right);
 
-            if (!map.containsKey(ch))
-                map.put(ch,right);
-            else
-                left=Math.max(left,map.get(ch)+1);//try for acca
+            if (map.containsKey(ch))
+                left=Math.max(left,map.get(ch)+1);//try for acca and abcab
                 
             map.put(ch,right);
             res=Math.max(right-left+1,res);
