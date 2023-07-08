5a56b8c806a472a7ba1382b4d7bc13195850c30d
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-07-08 17:50:06 +0530

    Time: 1 ms (95.68%) | Memory: 41.9 MB (96.74%) - LeetSync

--- generate-parentheses/generate-parentheses.java
+++ generate-parentheses/generate-parentheses.java
@@ -1,22 +1,30 @@
 class Solution {
-    private void helper(int n,List<String> res,int open,int close,String curr)
+    private void helper(int n,List<String> res,int open,int close,StringBuilder curr)
     {
         if (curr.length()==2*n)
         {
-            res.add(curr);
+            res.add(curr.toString());
             return;
         }
 
         if (open<n)
-            helper(n,res,open+1,close,curr+'(');
+        {
+            curr.append('(');
+            helper(n,res,open+1,close,curr);
+            curr.deleteCharAt(curr.length()-1);
+        }
         
         if (close<open)
-            helper(n,res,open,close+1,curr+')');
+        {
+            curr.append(')');
+            helper(n,res,open,close+1,curr);
+            curr.deleteCharAt(curr.length()-1);
+        }
     }
 
     public List<String> generateParenthesis(int n) {
         List<String> res=new ArrayList<>();
-        helper(n,res,0,0,"");
+        helper(n,res,0,0,new StringBuilder());
         return res;
     }
 }
