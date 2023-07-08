dea41a047b2626caef480661da56183e29b9d75a
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-07-08 16:57:05 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (81.79%) - LeetSync

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,20 +1,20 @@
 class Solution {
     String[] letters={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 
-    private void helper(String digits,List<String> res,int index,String curr)
+    private void helper(String digits,List<String> res,int index,StringBuilder curr)
     {
         if (curr.length()==digits.length())
         {
-            res.add(curr);
+            res.add(curr.toString());
             return;
         }
 
         int dgt=digits.charAt(index)-'2';
         for (char ch: letters[dgt].toCharArray())
         {
-            curr+=ch;
+            curr.append(ch);
             helper(digits,res,index+1,curr);
-            curr=curr.substring(0,curr.length()-1);
+            curr.deleteCharAt(curr.length()-1);
         }
     }
 
@@ -22,7 +22,7 @@ class Solution {
         List<String> res=new ArrayList<>();
         if (digits.length()==0)
             return res;
-        helper(digits,res,0,"");
+        helper(digits,res,0,new StringBuilder());
         return res;
     }
 }
