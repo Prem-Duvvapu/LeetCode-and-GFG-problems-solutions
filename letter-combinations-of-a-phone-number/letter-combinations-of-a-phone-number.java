1dfe3e95677ba7a58d1bdfaa410f04c7bcf7da60
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-07-08 16:55:53 +0530

    Time: 5 ms (47.36%) | Memory: 41.9 MB (11.61%) - LeetSync

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,31 +1,28 @@
 class Solution {
     String[] letters={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 
-    private void helper(String digits,List<String> res,int position,StringBuilder sb)
+    private void helper(String digits,List<String> res,int index,String curr)
     {
-        if (sb.length()==digits.length())
+        if (curr.length()==digits.length())
         {
-            res.add(sb.toString());
+            res.add(curr);
             return;
         }
 
-        int index=digits.charAt(position)-'2';
-        for (char ch: letters[index].toCharArray())
+        int dgt=digits.charAt(index)-'2';
+        for (char ch: letters[dgt].toCharArray())
         {
-            sb.append(ch);
-            helper(digits,res,position+1,sb);
-            sb.deleteCharAt(sb.length()-1);
+            curr+=ch;
+            helper(digits,res,index+1,curr);
+            curr=curr.substring(0,curr.length()-1);
         }
     }
 
     public List<String> letterCombinations(String digits) {
         List<String> res=new ArrayList<>();
-
         if (digits.length()==0)
             return res;
-
-        helper(digits,res,0,new StringBuilder());
+        helper(digits,res,0,"");
         return res;
     }
-
 }
