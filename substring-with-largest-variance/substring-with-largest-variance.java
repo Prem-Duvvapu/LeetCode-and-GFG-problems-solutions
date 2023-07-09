a969316ffa2c3af0579770ee6d9e881f77606cb9
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-07-10 02:45:17 +0530

    Time: 451 ms (6.83%) | Memory: 44.3 MB (18.40%) - LeetSync

--- substring-with-largest-variance/substring-with-largest-variance.java
+++ substring-with-largest-variance/substring-with-largest-variance.java
@@ -1,3 +1,5 @@
+//modified Kadane's algorithm
+
 class Solution {
     private int helper(String s,int res,char ch1,char ch2)
     {
@@ -25,6 +27,8 @@ class Solution {
     public int largestVariance(String s) {
         int res=0;
         int[] freq=new int[26];
+        StringBuilder sb=new StringBuilder(s);
+        sb.reverse();
 
         for (char ch: s.toCharArray())
             freq[ch-'a']++;
@@ -39,8 +43,6 @@ class Solution {
                     continue;
 
                 res=helper(s,res,ch1,ch2);
-                StringBuilder sb=new StringBuilder(s);
-                sb.reverse();
                 res=helper(sb.toString(),res,ch1,ch2);
             }
         }
