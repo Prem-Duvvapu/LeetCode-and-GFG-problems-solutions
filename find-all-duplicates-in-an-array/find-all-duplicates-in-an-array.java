e6802022dae74af07010e1773b0dd9753c31e7e8
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-07-12 16:35:28 +0530

    Time: 6 ms (91.66%) | Memory: 53.6 MB (35.44%) - LeetSync

--- find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -4,16 +4,12 @@ class Solution {
 
         for (int i=0;i<nums.length;i++)
         {
-            int val=Math.abs(nums[i])%100000;
-            if (nums[val-1]>0)
+            int val=Math.abs(nums[i]);
+            if (nums[val-1]<0)
+                res.add(val);
+            else
                 nums[val-1]*=-1;
-            else if (nums[val-1]<0)
-                nums[val-1]=Math.abs(nums[val-1])+100000;
         }
-
-        for (int i=0;i<nums.length;i++)
-            if (nums[i]>100000)
-                res.add(i+1);
 
         return res;
     }
