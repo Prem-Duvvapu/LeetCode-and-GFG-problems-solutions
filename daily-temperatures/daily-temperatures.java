821a8004cbcb0f059d6a58b4442396622c96ac50
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-07-14 00:48:49 +0530

    Time: 30 ms (88.59%) | Memory: 60.2 MB (10.66%) - LeetSync

--- daily-temperatures/daily-temperatures.java
+++ daily-temperatures/daily-temperatures.java
@@ -3,7 +3,7 @@ class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int n=temperatures.length;
         int[] res=new int[n];
-        Stack<Integer> s=new Stack<>();
+        Deque<Integer> s=new ArrayDeque<>();
 
         for (int currIndex=0;currIndex<n;currIndex++)
         {
