73777e14478451d554f4356faba669f3f0649606
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-07-13 23:53:18 +0530

    Time: 25 ms (77.90%) | Memory: 44.6 MB (86.08%) - LeetSync

--- next-greater-element-ii/next-greater-element-ii.java
+++ next-greater-element-ii/next-greater-element-ii.java
@@ -1,21 +1,27 @@
+//Decreasing monotonic Stack
+//Iterate through the nums array two times because the -1 values got in first iteration for result array can be replaced with its highest previous values in the nums array if exist by iterating starting from the first second time.
+
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int n = nums.length; // get the length of the array nums
-        int[] res = new int[n]; // create an array of size n to store the result
-        Arrays.fill(res, -1); // fill the result array with -1 initially
-        Deque<Integer> stack = new ArrayDeque<>(); // create a stack using ArrayDeque since its efficient
-        
-        // loop through the array twice, because we need to compare each element with every other element
-        for (int i = 0; i < 2 * n; i++) {
-            int num = nums[i % n]; // get the current element by taking modulus with n
-            while (!stack.isEmpty() && nums[stack.peek()] < num) {
-                // while stack is not empty and top element of stack is less than current element
-                res[stack.pop()] = num; // update result array with current element for top element of stack
+        int n=nums.length;
+        int[] res=new int[n];
+        Arrays.fill(res,-1);
+        Stack<Integer> mStack=new Stack<>();
+
+        for (int i=0;i<2*n;i++)
+        {
+            int val=nums[i%n];
+
+            while (!mStack.isEmpty() && val>nums[mStack.peek()])
+            {
+                int index=mStack.pop();
+                res[index]=val;
             }
-            if (i < n) {
-                stack.push(i); // push the current index onto the stack if i < n
-            }
+
+            if(i<n)
+                mStack.push(i);
         }
-        return res; // return the result array
+
+        return res;
     }
 }
