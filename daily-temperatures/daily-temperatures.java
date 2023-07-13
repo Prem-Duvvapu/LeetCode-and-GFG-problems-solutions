facb3c151e6f1ea9b17c7afd94027323b56fde96
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-07-14 00:47:45 +0530

    Time: 150 ms (59.50%) | Memory: 55.5 MB (63.62%) - LeetSync

---
+++ daily-temperatures/daily-temperatures.java
@@ -0,0 +1,20 @@
+//Decreasing monotonic Stack - elements in the stack are in decreasing order from bottom to top
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n=temperatures.length;
+        int[] res=new int[n];
+        Stack<Integer> s=new Stack<>();
+
+        for (int currIndex=0;currIndex<n;currIndex++)
+        {
+            while (!s.isEmpty() && temperatures[currIndex]>temperatures[s.peek()])
+            {
+                int index=s.pop();
+                res[index]=(currIndex-index);
+            }
+            s.push(currIndex);
+        }
+
+        return res;
+    }
+}
