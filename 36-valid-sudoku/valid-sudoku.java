7bd82b21184509219a8abdaa042fb6a7dd468b66
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-07-15 15:03:53 +0530

    Time: 2 ms (86.84%) | Memory: 43.7 MB (38.10%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,31 +1,31 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean[][] rowCheck=new boolean[10][10];
-        boolean[][] colCheck=new boolean[10][10];
-        boolean[][] subBoxCheck=new boolean[10][10];
+        boolean[][] rowCheck=new boolean[10][10]; //row number (1 to 9)
+        boolean[][] colCheck=new boolean[10][10]; //col number (1 to 9)
+        boolean[][] subBoxCheck=new boolean[10][10]; //subBox number (1 to 9)
 
         for (int i=0;i<9;i++)
         {
             for (int j=0;j<9;j++)
             {
                 char ch=board[i][j];
-                if (ch=='.')
+                if (ch=='.')    //we need only integer characters
                     continue;
 
-                int val=(ch-'0');
+                int val=(ch-'0'); //char to integer
 
-                if (rowCheck[i+1][val]) //row number (1 to 9)
+                if (rowCheck[i+1][val]) //checking if value already exists in that row
                     return false;
-                rowCheck[i+1][val]=true;
+                rowCheck[i+1][val]=true; //now note that the value exists in the row
 
-                if (colCheck[j+1][val]) //col number (1 to 9)
+                if (colCheck[j+1][val]) //checking if value already exists in that column
                     return false;
-                colCheck[j+1][val]=true;
+                colCheck[j+1][val]=true; //now note that the value exists in the column
 
-                int n=i/3*3+j/3+1;    //subBox number (1 to 9)
-                if (subBoxCheck[n][val])
+                int n=i/3*3+j/3+1;    
+                if (subBoxCheck[n][val]) //checking if value already exists in that subBox
                     return false;
-                subBoxCheck[n][val]=true;
+                subBoxCheck[n][val]=true; //now note that the value exists in the subBox
             }
         }
 
