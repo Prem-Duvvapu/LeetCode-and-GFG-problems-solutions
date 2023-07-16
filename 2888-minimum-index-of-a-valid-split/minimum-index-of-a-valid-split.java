6a960e95278e48aa7c16555d66bf1aace798c3c5
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-07-16 16:40:57 +0530

    Time: 16 ms (20.00%) | Memory: 56.7 MB (20.00%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
@@ -4,6 +4,8 @@ class Solution {
         int n=nums.size();
         int dominant=nums.get(0);
         int cnt=1;
+        
+        //find the dominant element
         for (int i=1;i<n;i++)
         {
             if (nums.get(i)==dominant)
@@ -14,11 +16,13 @@ class Solution {
                     cnt--;
         }
         
+        //calculate frequency of the dominant element
         int dCnt=0;
         for (int val: nums)
-                if (val==dominant)
-                        dCnt++;
+            if (val==dominant)
+                    dCnt++;
         
+        //check if split is possible at current index
         int preCnt=0;
         for (int i=0;i<n;i++)
         {
@@ -27,6 +31,7 @@ class Solution {
             if (preCnt*2>(i+1) && (dCnt-preCnt)*2>(n-1-i))
                     return i;
         }
+
         return -1;
     }
 }
