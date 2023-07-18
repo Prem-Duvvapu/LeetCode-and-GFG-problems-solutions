b578b45dfa9955ced7cc231e701f128febab8fb9
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-07-19 00:39:35 +0530

    Time: 1 ms (99.80%) | Memory: 48.7 MB (43.50%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,24 @@
+//Boyer-Moore Voting algorithm
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n=nums.length;
+        int res=nums[0];
+        int cnt=1;
+
+        for (int i=1;i<n;i++)
+        {
+            if (nums[i]==res)
+                cnt++;
+            else if (cnt==0)
+            {
+                res=nums[i];
+                cnt=1;
+            }
+            else
+                cnt--;
+        }
+
+        return res;
+    }
+}
