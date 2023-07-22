387fc6697475e8c5cb586bc03a18b1b82ab46fce
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-07-22 15:07:11 +0530

    Time: 135 ms (5.15%) | Memory: 53.1 MB (6.50%) - LeetSync

--- 688-knight-probability-in-chessboard/knight-probability-in-chessboard.java
+++ 688-knight-probability-in-chessboard/knight-probability-in-chessboard.java
@@ -1,13 +1,13 @@
 class Solution {
     int[] r={1,2,1,2,-1,-2,-1,-2};
     int[] c={2,1,-2,-1,2,1,-2,-1};
+    Map<String,Double> map=new HashMap<>();
 
     public double knightProbability(int n, int k, int row, int column) {
-        Double[][][] dp=new Double[n][n][k+1];
-        return prob(n,k,row,column,dp);
+        return prob(n,k,row,column);
     }
 
-    private double prob(int n,int k,int row,int col,Double[][][] dp)
+    private double prob(int n,int k,int row,int col)
     {
         if (row<0 || row>=n || col<0 || col>=n)
             return 0;
@@ -15,14 +15,16 @@ class Solution {
         if (k==0)
             return 1;
 
-        if (dp[row][col][k]!=null)
-            return dp[row][col][k];
+        String s=Integer.toString(row)+"prem"+Integer.toString(col)+"duvvapu"+Integer.toString(k);
+        if (map.containsKey(s))
+            return map.get(s);
 
         double probability=0;
 
         for (int i=0;i<8;i++)
-            probability+=prob(n,k-1,row+r[i],col+c[i],dp)/8.0;
+            probability+=prob(n,k-1,row+r[i],col+c[i])/8.0;
 
-        return dp[row][col][k]=probability;
+        map.put(s,probability);
+        return probability;
     }
 }
