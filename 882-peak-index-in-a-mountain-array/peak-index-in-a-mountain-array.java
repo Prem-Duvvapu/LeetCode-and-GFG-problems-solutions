542e35e853c48f36881ce8a881adb12a91aff0d8
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-07-25 09:44:54 +0530

    Time: 1 ms (15.54%) | Memory: 55.5 MB (49.19%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int maxIndex=0;
+
+        for (int i=1;i<arr.length;i++)
+            if (arr[i]>arr[maxIndex])
+                maxIndex=i;
+
+        return maxIndex;
+    }
+}
