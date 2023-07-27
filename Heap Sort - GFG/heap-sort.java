22b9491891ca322cff8553a5b0dbc0b1d1b73587
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-07-27 22:50:12 +0530

    Added solution - LeetHub

--- Heap Sort - GFG/heap-sort.java
+++ Heap Sort - GFG/heap-sort.java
@@ -43,9 +43,6 @@ class Solution
         // Your code here
          int startIdx = (N / 2) - 1;
  
-        // Perform reverse level order traversal
-        // from last non-leaf node and heapify
-        // each node
         for (int i = startIdx; i >= 0; i--) {
             heapify(arr, N, i);
         }
@@ -55,25 +52,21 @@ class Solution
     void heapify(int arr[], int N, int i)
     {
         // Your code here
-        int largest = i; // Initialize largest as root
-        int l = 2 * i + 1; // left = 2*i + 1
-        int r = 2 * i + 2; // right = 2*i + 2
+        int largest = i; 
+        int l = 2 * i + 1; 
+        int r = 2 * i + 2; 
  
-        // If left child is larger than root
         if (l < N && arr[l] > arr[largest])
             largest = l;
  
-        // If right child is larger than largest so far
         if (r < N && arr[r] > arr[largest])
             largest = r;
  
-        // If largest is not root
         if (largest != i) {
             int swap = arr[i];
             arr[i] = arr[largest];
             arr[largest] = swap;
  
-            // Recursively heapify the affected sub-tree
             heapify(arr, N, largest);
         }
     }
@@ -85,14 +78,11 @@ class Solution
         for (int i = N / 2 - 1; i >= 0; i--)
             heapify(arr, N, i);
  
-        // One by one extract an element from heap
         for (int i = N - 1; i > 0; i--) {
-            // Move current root to end
             int temp = arr[0];
             arr[0] = arr[i];
             arr[i] = temp;
  
-            // call max heapify on the reduced heap
             heapify(arr, i, 0);
         }
     }
