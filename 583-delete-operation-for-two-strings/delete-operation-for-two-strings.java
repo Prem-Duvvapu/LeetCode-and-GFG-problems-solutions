5cf7e59808e3daec11b12f9944607fd092d447cb
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-01 01:41:43 +0530

    Time: 13 ms (9.30%) | Memory: 43.8 MB (36.79%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -0,0 +1,38 @@
+class Solution {
+    String s1;
+    String s2;
+    int[][] dp;
+
+    public int solve(int i,int j)
+    {
+        if (i==s1.length() && j==s2.length())
+            return 0;
+
+        if (i==s1.length())
+            return 1+solve(i,j+1);
+
+        if (j==s2.length())
+            return 1+solve(i+1,j);
+
+        if (dp[i][j]!=-1)
+            return dp[i][j];
+
+        if (s1.charAt(i)==s2.charAt(j))
+            return solve(i+1,j+1);
+
+        int delete_char_i=1+solve(i+1,j);
+        int delete_char_j=1+solve(i,j+1);
+
+        return dp[i][j]=Math.min(delete_char_i,delete_char_j);
+    }
+
+    public int minDistance(String word1, String word2) {
+        s1=word1;
+        s2=word2;
+        dp=new int[s1.length()][s2.length()];
+        for (int i=0;i<s1.length();i++)
+            for (int j=0;j<s2.length();j++)
+                dp[i][j]=-1;
+        return solve(0,0);
+    }
+}
