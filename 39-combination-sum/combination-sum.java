e809056ee0b8203694dc66257d85d0cb04950f13
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-06 15:48:32 +0530

    Time: 2 ms (79.34%) | Memory: 43.9 MB (77.13%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,30 +1,26 @@
 class Solution {
-    private void combinations(int pos,int target,List<Integer> curr,int[] candidates,List<List<Integer>> res)
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res=new ArrayList<>();
+        helper(res,candidates,target,0,new ArrayList<>(),0);
+        return res;
+    }
+
+    private void helper(List<List<Integer>> res,int[] candidates,int target,int index,List<Integer> curr,int total)
     {
-        if (target==0)
+        if (total==target)
         {
-            res.add(new ArrayList<>(curr));
+            List<Integer> temp=new ArrayList<>(curr);
+            res.add(temp);
             return;
         }
 
-        if (target<0 || pos==candidates.length)
+        if (index>=candidates.length || total>target)
             return;
 
-        //pick
-        System.out.println(new ArrayList<>(curr)+" "+target);
-        curr.add(candidates[pos]);
-        target-=candidates[pos];
-        combinations(pos,target,curr,candidates,res);
+        curr.add(candidates[index]);
+        helper(res,candidates,target,index,curr,total+candidates[index]);//consider
+
         curr.remove(curr.size()-1);
-        target+=candidates[pos];
-
-        //not pick
-        combinations(pos+1,target,curr,candidates,res);
-    }
-
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res=new ArrayList<>();
-        combinations(0,target,new ArrayList<>(),candidates,res);
-        return res;
+        helper(res,candidates,target,index+1,curr,total);
     }
 }
