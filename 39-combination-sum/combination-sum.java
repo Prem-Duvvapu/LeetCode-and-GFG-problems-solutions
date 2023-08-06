b73d646ad96b9d0bcd821946b7728d2a946a21eb
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-06 15:43:38 +0530

    Time: 39 ms (5.00%) | Memory: 44.6 MB (5.87%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    private void combinations(int pos,int target,List<Integer> curr,int[] candidates,List<List<Integer>> res)
+    {
+        if (pos==candidates.length)
+        {
+            if (target==0)
+                res.add(new ArrayList<>(curr));
+            return;
+        }
+
+        if (candidates[pos]<=target){
+        //pick
+        System.out.println(new ArrayList<>(curr)+" "+target);
+        curr.add(candidates[pos]);
+        target-=candidates[pos];
+        combinations(pos,target,curr,candidates,res);
+        curr.remove(curr.size()-1);
+        target+=candidates[pos];}
+
+        //not pick
+        combinations(pos+1,target,curr,candidates,res);
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res=new ArrayList<>();
+        combinations(0,target,new ArrayList<>(),candidates,res);
+        return res;
+    }
+}
