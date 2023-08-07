5daf21d8a0f49100cda95437c8602cd942c32edb
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-07 19:22:09 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (98.68%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -4,23 +4,20 @@ class Solution {
         int n=matrix[0].length;
         int left=0;
         int right=m*n-1;
-        return binSearch(matrix,left,right,n,target);
-    }
-
-    private boolean binSearch(int[][] matrix,int left,int right,int col,int target)
-    {
+        
         while (left<=right)
         {
             int mid=left+(right-left)/2;
-            int r=mid/col;
-            int c=mid%col;
-            if (matrix[r][c]==target)
+            int row=mid/n;
+            int col=mid%n;
+            if (matrix[row][col]==target)
                 return true;
-            else if (matrix[r][c]<target)
-                left=mid+1;
-            else
+            else if (matrix[row][col]>target)
                 right=mid-1;
+            else
+                left=mid+1;
         }
+
         return false;
     }
 }
