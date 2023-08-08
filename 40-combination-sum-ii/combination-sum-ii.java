beb573dca86b8c23671632409bb8a63407f8a3a2
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-08 12:39:02 +0530

    Time: 3 ms (67.80%) | Memory: 41.8 MB (99.76%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,30 +1,27 @@
-//Striver's approach
-
 class Solution {
-    private void combinations(int pos,int target,List<Integer> curr,int[] candidates,List<List<Integer>> res)
+    private void combinations(int index,int target,List<Integer> currList,int[] candidates,List<List<Integer>> res)
     {
         if (target==0)
         {
-            res.add(new ArrayList<>(curr));
+            res.add(new ArrayList<>(currList));
             return;
         }
 
-        for (int i=pos;i<candidates.length;i++)
+        if (index==candidates.length || candidates[index]>target)
+            return;
+
+        for (int i=index;i<candidates.length;i++)
         {
-            if (i>pos && (candidates[i]==candidates[i-1]))
+            if (i>index && candidates[i-1]==candidates[i])
                 continue;
 
-            if (candidates[i]>target)
-                break;
-
-            curr.add(candidates[i]);
-            combinations(i+1,target-candidates[i],curr,candidates,res);
-            curr.remove(curr.size()-1);
+            currList.add(candidates[i]);
+            combinations(i+1,target-candidates[i],currList,candidates,res);
+            currList.remove(currList.size()-1);
         }
     }
 
-    public List<List<Integer>> combinationSum2(int[] candidates, int target)
-    {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> res=new ArrayList<>();
         Arrays.sort(candidates);
         combinations(0,target,new ArrayList<>(),candidates,res);
