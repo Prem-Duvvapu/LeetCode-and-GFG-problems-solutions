92dc98cfbc58cacbc1ad3f415247cc1eaa6cf424
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-08 15:20:00 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (99.72%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,19 +1,24 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res=new ArrayList<>();
-        int n=nums.length;
-
-        for (int i=0;i<(1<<n);i++)
+    private void solve(int index,List<Integer> currList,int[] nums,List<List<Integer>> res)
+    {
+        if (index==nums.length)
         {
-            List<Integer> currList=new ArrayList<>();
-
-            for (int j=0;j<n;j++)
-                if ((i&(1<<j))!=0)
-                    currList.add(nums[j]);
-                
-            res.add(currList);
+            res.add(new ArrayList<>(currList));
+            return;
         }
 
+        //pick
+        currList.add(nums[index]);
+        solve(index+1,currList,nums,res);
+        currList.remove(currList.size()-1);
+
+        //not pick
+        solve(index+1,currList,nums,res);
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
+        solve(0,new ArrayList<>(),nums,res);
         return res;
     }
 }
