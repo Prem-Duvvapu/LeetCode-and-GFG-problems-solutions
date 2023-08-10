022f531f53549f0f3507631dcfdc8f06f8c91156
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-10 15:15:29 +0530

    Time: 1 ms (100.00%) | Memory: 43.5 MB (98.81%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,41 +1,6 @@
+//Striver's approach
+
 class Solution {
-    private boolean isValid(char[][] curr,int row,int col,int n)
-    {
-        //row check
-        int r=row;
-        int c=col;
-        while (c>=0)
-        {
-            if (curr[r][c]=='Q')
-                return false;
-            c--;
-        }
-        
-        //upper-left diagonal
-        r=row;
-        c=col;
-        while (r>=0 && c>=0)
-        {
-            if (curr[r][c]=='Q')
-                return false;
-            r--;
-            c--;
-        }
-
-        //lower-left diagonal
-        r=row;
-        c=col;
-        while (r<n && c>=0)
-        {
-            if (curr[r][c]=='Q')
-                return false;
-            r++;
-            c--;
-        }
-
-        return true;
-    }
-
     private void solve(int col,int n,char[][] curr,boolean[] rc,boolean[] udc,boolean[] ldc,List<List<String>> res)
     {
         if (col==n)
@@ -50,13 +15,17 @@ class Solution {
 
         for (int row=0;row<n;row++) //row
         {
-            if (!rc[row] && !udc[row+col] && !ldc[n-1+col-row])
+            if (!rc[row] && !udc[row+col] && !ldc[n-1+col-row]) //is safe or not
             {
+                //mark as present
                 rc[row]=true;
                 udc[row+col]=true;
                 ldc[n-1+col-row]=true;
+
                 curr[row][col]='Q';
                 solve(col+1,n,curr,rc,udc,ldc,res);
+
+                //remove mark
                 curr[row][col]='.';
                 rc[row]=false;
                 udc[row+col]=false;
@@ -68,6 +37,7 @@ class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> res=new ArrayList<>();
         char[][] curr=new char[n][n];
+
         for (int i=0;i<n;i++)
             for (int j=0;j<n;j++)
                 curr[i][j]='.';
@@ -75,8 +45,8 @@ class Solution {
         boolean[] rc=new boolean[n];//row check
         boolean[] udc=new boolean[2*n-1];//upper diagonal check
         boolean[] ldc=new boolean[2*n-1];//lower diagonal check
-        solve(0,n,curr,rc,udc,ldc,res);
 
+        solve(0,n,curr,rc,udc,ldc,res);
         return res;
     }
 }
