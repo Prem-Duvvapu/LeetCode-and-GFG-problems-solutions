7f097efd564d49ffe002dcb6f85880474cee4d20
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-12 10:10:45 +0530

    Time: 1 ms (98.63%) | Memory: 39.9 MB (81.37%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.java
+++ 60-permutation-sequence/permutation-sequence.java
@@ -1,19 +1,4 @@
 class Solution {
-    private void solve(StringBuilder sb,List<Character> list,int val,int n,int fact)
-    {
-        if (n==0)
-        {
-            sb.append(list.get(0));
-            return;
-        }
-
-        int index=val/fact;
-        sb.append(list.get(index));
-        list.remove(index);
-        val=val%fact;
-        solve(sb,list,val,n-1,fact/n);
-    }
-
     public String getPermutation(int n, int k) {
         List<Character> list=new ArrayList<>();
         for (int i=1;i<=n;i++)
@@ -22,7 +7,16 @@ class Solution {
         for (int i=1;i<n;i++)
             fact*=i;
         StringBuilder sb=new StringBuilder();
-        solve(sb,list,k-1,n-1,fact);
+        k=k-1;
+        for (int i=n-1;i>0;i--)
+        {
+            int index=k/fact;
+            sb.append(list.get(index));
+            list.remove(index);
+            k=k%fact;
+            fact=fact/i;
+        }
+        sb.append(list.get(0));
         return sb.toString();
     }
 }
