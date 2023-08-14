ec685e2e9bc33a15562766b28333a3865364ec18
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-14 15:12:36 +0530

    Time: 0 ms (100.00%) | Memory: 39.1 MB (80.23%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,21 +1,12 @@
 class Solution {
-    int[] dp=new int[46];
-    
-    Solution()
-    {
-        Arrays.fill(dp,-1);
-    }
-
     public int climbStairs(int n) {
-        if (n==0 || n==1)
-            return 1;
+        int[] dp=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        
+        for (int i=2;i<=n;i++)
+            dp[i]=dp[i-1]+dp[i-2];
 
-        if (dp[n]!=-1)
-            return dp[n];
-
-        int oneStep=climbStairs(n-1);
-        int twoSteps=climbStairs(n-2);
-
-        return dp[n]=oneStep+twoSteps;
+        return dp[n];
     }
 }
