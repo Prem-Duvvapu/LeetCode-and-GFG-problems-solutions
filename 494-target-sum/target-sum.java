813739507e7a68731812bc5e558a2751b94918c5
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-15 11:57:24 +0530

    Time: 118 ms (44.91%) | Memory: 45.1 MB (5.01%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    private int solve(int pos,int[] nums,int target)
+    private int solve(int pos,int[] nums,int target,Map<String,Integer> map)
     {
         if (pos==nums.length)
         {
@@ -9,13 +9,20 @@ class Solution {
                 return 0;
         }
 
-        int plus=solve(pos+1,nums,target-nums[pos]);
-        int minus=solve(pos+1,nums,target+nums[pos]);
+        String s=Integer.toString(pos)+" "+Integer.toString(target);
+        if (map.containsKey(s))
+            return map.get(s);
+
+        int plus=solve(pos+1,nums,target-nums[pos],map);
+        int minus=solve(pos+1,nums,target+nums[pos],map);
+
+        map.put(s,plus+minus);
         
         return plus+minus;
     }
 
     public int findTargetSumWays(int[] nums, int target) {
-        return solve(0,nums,target);
+        Map<String,Integer> map=new HashMap<>();
+        return solve(0,nums,target,map);
     }
 }
