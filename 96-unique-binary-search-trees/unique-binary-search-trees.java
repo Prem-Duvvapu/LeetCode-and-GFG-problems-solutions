0bbed3eff7601cb16af30371213e9da32e34b5cc
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-15 19:22:09 +0530

    Time: 0 ms (100.00%) | Memory: 39 MB (85.21%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,25 +1,27 @@
 class Solution {
-    private int solve(int start,int end)
+    private int solve(int start,int end,int[][] dp)
     {
-        // if (start>end)
-        //     return 0;
-
         if (start>=end)
             return 1;
 
-        int res=0;
+        if (dp[start][end]!=-1)
+            return dp[start][end];
 
+        int res=0;
         for (int i=start;i<=end;i++)
         {
-            int left=solve(start,i-1);
-            int right=solve(i+1,end);
-            res=res+left*right;
+            int left=solve(start,i-1,dp);
+            int right=solve(i+1,end,dp);
+            res+=(left*right);
         }
 
-        return res;
+        return dp[start][end]=res;
     }
 
     public int numTrees(int n) {
-        return solve(1,n);
+        int[][] dp=new int[n+1][n+1];
+        for (int[] arr: dp)
+            Arrays.fill(arr,-1);
+        return solve(1,n,dp);
     }
 }
