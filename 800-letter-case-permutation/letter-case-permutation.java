e4b0fe82b7a8bf4687d4268c955aca3eea42ae88
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-15 12:15:07 +0530

    Time: 2 ms (88.11%) | Memory: 44.9 MB (16.85%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.java
+++ 800-letter-case-permutation/letter-case-permutation.java
@@ -1,24 +1,35 @@
 class Solution {
-    private void solve(int pos,String curr,String s,List<String> res)
+    private void solve(int pos,StringBuilder curr,String s,List<String> res)
     {
         if (pos==s.length())
         {
-            res.add(new String(curr));
+            String sub=curr.toString();
+            res.add(sub);
             return;
         }
 
         if (Character.isDigit(s.charAt(pos)))
-            solve(pos+1,curr+s.charAt(pos),s,res);
+        {
+            char ch=s.charAt(pos);
+            curr.append(ch);
+            solve(pos+1,curr,s,res);
+            curr.deleteCharAt(curr.length()-1);
+        }
         else
         {
-            solve(pos+1,curr+Character.toLowerCase(s.charAt(pos)),s,res);
-            solve(pos+1,curr+Character.toUpperCase(s.charAt(pos)),s,res);
+            char ch=s.charAt(pos);
+            curr.append(Character.toLowerCase(ch));
+            solve(pos+1,curr,s,res);
+            curr.deleteCharAt(curr.length()-1);
+            curr.append(Character.toUpperCase(ch));
+            solve(pos+1,curr,s,res);
+            curr.deleteCharAt(curr.length()-1);
         }
     }
 
     public List<String> letterCasePermutation(String s) {
         List<String> res=new ArrayList<>();
-        solve(0,"",s,res);
+        solve(0,new StringBuilder(),s,res);
         return res;
     }
 }
