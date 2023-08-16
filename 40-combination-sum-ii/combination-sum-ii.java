9b797396df3448019d629d4c4bde5500c443cb42
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-16 19:26:56 +0530

    Time: 3 ms (68.39%) | Memory: 42.2 MB (89.49%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -14,13 +14,13 @@ class Solution {
             return;
         }
 
-        if (pos==candidates.length || candidates[pos]>target)
-            return;
-
         for (int i=pos;i<candidates.length;i++)
         {
             if (i>pos && candidates[i]==candidates[i-1])
                 continue;
+
+            if (candidates[i]>target)
+                return;
 
             curr.add(candidates[i]);
             solve(i+1,curr,target-candidates[i],candidates,res);
