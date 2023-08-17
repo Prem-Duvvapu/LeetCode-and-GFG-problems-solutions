74cc465094531ddc21a16e4d692936ed5a58996b
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-17 12:50:37 +0530

    Time: 17 ms (55.53%) | Memory: 59.5 MB (7.57%) - LeetSync

--- 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.java
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.java
@@ -1,20 +1,40 @@
 class Solution {
-        public int minimizeMax(int[] A, int p) {
-        Arrays.sort(A);
-        int n = A.length, left = 0, right = A[n - 1] - A[0];
-        while (left < right) {
-            int mid = (left + right) / 2, k = 0;
-            for (int i = 1; i < n && k < p; ++i) {
-                if (A[i] - A[i - 1] <= mid) {
-                    k++;
-                    i++;
-                }
+    private boolean isValid(int diff,int p,int[] nums)
+    {
+        int cnt=0;
+
+        for (int i=0;i<nums.length-1;i++)
+        {
+            if ((nums[i+1]-nums[i])<=diff)
+            {
+                cnt++;
+                i++;
             }
-            if (k >= p)
-                right = mid;
-            else
-                left = mid + 1;
         }
-        return left;
+
+        return (cnt>=p);
+    }
+
+    public int minimizeMax(int[] nums, int p) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        int left=0;
+        int right=nums[n-1]-nums[0];
+        int result=0;
+
+        //binary search
+        while (left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if (isValid(mid,p,nums))
+            {
+                result=mid;
+                right=mid-1;
+            }
+            else
+                left=mid+1;
+        }
+
+        return result;
     }
 }
