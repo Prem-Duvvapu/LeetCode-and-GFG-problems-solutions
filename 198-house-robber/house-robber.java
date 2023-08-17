eec447ee36fbef68b88f50fb70579b3c994c8c46
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-17 16:10:38 +0530

    Time: 0 ms (100.00%) | Memory: 39.5 MB (97.61%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        int[] dp=new int[n];
+        Arrays.fill(dp,-1);
+        return solve(n-1,nums,dp);
+    }
+
+    private int solve(int pos,int[] nums,int[] dp)
+    {
+        if (pos==0)
+            return nums[0];
+
+        if (pos==1)
+            return Math.max(nums[1],nums[0]);
+
+        if (dp[pos]!=-1)
+            return dp[pos];
+
+        int pick=nums[pos]+solve(pos-2,nums,dp);
+        int notPick=solve(pos-1,nums,dp);
+
+        return dp[pos]=Math.max(pick,notPick);
+    }
+}
+
+//TC:- O(n)
+//SC:- O(n)+O(n)
