eaefaa38bab39e012c13fa876f1082ec2ed8e48b
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-19 10:45:37 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (72.86%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,34 +1,34 @@
+//Memoization
+
 class Solution {
     int mod=2000000000;
 
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
-        int[][] unqPaths=new int[m][n];
+        int[][] dp=new int[m][n];//step-1
+        for (int[] arr: dp)
+            Arrays.fill(arr,-1);
+        return solve(m-1,n-1,obstacleGrid,dp);
+    }
 
-        //first row
-        for (int i=0;i<n;i++)
-        {
-            if (obstacleGrid[0][i]==1)
-                break;
-            
-            unqPaths[0][i]=1;
-        }
+    private int solve(int m,int n,int[][] obstacleGrid,int[][] dp)
+    {
+        if (m<0 || n<0)
+            return 0;
 
-        //first column
-        for (int i=0;i<m;i++)
-        {
-            if (obstacleGrid[i][0]==1)
-                break;
+        if (obstacleGrid[m][n]==1)
+            return 0;
 
-            unqPaths[i][0]=1;
-        }
+        if (m==0 && n==0)
+            return 1;
 
-        for (int i=1;i<m;i++)
-            for (int j=1;j<n;j++)
-                if (obstacleGrid[i][j]!=1)
-                    unqPaths[i][j]=unqPaths[i][j-1]+unqPaths[i-1][j];
+        if (dp[m][n]!=-1)//step-3
+            return dp[m][n];
 
-        return unqPaths[m-1][n-1];
+        int up=solve(m-1,n,obstacleGrid,dp);
+        int left=solve(m,n-1,obstacleGrid,dp);
+
+        return dp[m][n]=(up+left)%mod;//step-2
     }
 }
