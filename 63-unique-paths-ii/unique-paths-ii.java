3f38d4c35821cd67e269d39678eba5ab6fafe08b
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-19 14:44:58 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (15.94%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,4 +1,4 @@
-//Memoization
+//Space Optimization
 
 class Solution {
     int mod=2000000000;
@@ -6,32 +6,36 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
-        int[][] dp=new int[m][n];
+        int[] prev=new int[n];
 
         for (int i=0;i<m;i++)
         {
+            int[] curr=new int[n];
+
             for (int j=0;j<n;j++)
             {
                 if (obstacleGrid[i][j]==1)
-                    dp[i][j]=0;
+                    curr[j]=0;
                 else if (i==0 && j==0)
-                    dp[i][j]=1;
+                    curr[j]=1;
                 else
                 {
                     int up=0;
                     int left=0;
 
                     if (i>0)
-                        up=dp[i-1][j];
+                        up=prev[j];
 
                     if (j>0)
-                        left=dp[i][j-1];
+                        left=curr[j-1];
 
-                    dp[i][j]=(up+left)%mod;
+                    curr[j]=(up+left)%mod;
                 }
             }
+
+            System.arraycopy(curr,0,prev,0,n);
         }
 
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 }
