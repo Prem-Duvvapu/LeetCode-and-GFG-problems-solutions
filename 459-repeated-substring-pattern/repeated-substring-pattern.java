b7181ba282141b9a5430607592d8ef53c3dfd651
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-21 12:00:47 +0530

    Time: 524 ms (6.60%) | Memory: 45.3 MB (5.09%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
         int n=s.length();
-        
+
         for (int l=1;l<=n/2;l++)
         {
-            if (n%l==0)
+            if (n%l==0)//O(2*sqrt(n))
             {
                 int times=n/l;
-                String sub=s.substring(0,l);
+                String sub=s.substring(0,l);//O(n)
                 String curr="";
 
-                while (times-- > 0)
+                while (times-- > 0)//O(n)
                     curr+=sub;
 
-                if (curr.equals(s))
+                if (curr.equals(s))//O(n)
                     return true;
             }
         }
@@ -21,3 +21,8 @@ class Solution {
         return false;
     }
 }
+
+/*
+Maximum no. of factors for a number 'n'=2*sqrt(n)
+T.C:- O(n*sqrt(n))
+*/
