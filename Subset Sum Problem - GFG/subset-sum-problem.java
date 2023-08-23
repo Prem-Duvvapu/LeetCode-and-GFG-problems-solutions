26dada160fd0f97e4db2b64d4c3d97298036cc25
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-23 16:07:51 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.java
+++ Subset Sum Problem - GFG/subset-sum-problem.java
@@ -37,52 +37,29 @@ class Solution{
 
 
     static Boolean isSubsetSum(int N, int a[], int target){
-        boolean[][] dp=new boolean[N][target+1];
+        boolean[] prev=new boolean[target+1];
+        boolean[] curr=new boolean[target+1];
         
-        //Base Case
-        for (int i=0;i<N;i++)
-            dp[i][0]=true;
-            
+        prev[0]=true;
+        curr[0]=true;
+        
         if (a[0]<=target)
-            dp[0][a[0]]=true;
-        
+            prev[a[0]]=true;
+            
         for (int i=1;i<N;i++)
         {
             for (int j=1;j<=target;j++)
             {
-                boolean notTake=dp[i-1][j];
+                boolean notTake=prev[j];
                 boolean take=false;
                 if (j>=a[i])
-                    take=dp[i-1][j-a[i]];
+                    take=prev[j-a[i]];
                     
-                dp[i][j]=(take || notTake);
+                curr[j]=(take || notTake);
             }
+            System.arraycopy(curr,0,prev,0,target+1);
         }
         
-        return dp[N-1][target];
-    }
-    
-    static Boolean solve(int index,int target,int[] a,int[][] dp)
-    {
-        if (target==0)
-            return true;
-            
-        if (index==0)
-            return (a[0]==target);
-            
-        if (dp[index][target]!=-1)
-            return (dp[index][target]==1);
-            
-        Boolean notTake=solve(index-1,target,a,dp);
-        Boolean take=false;
-        if (target>=a[index])
-            take=solve(index-1,target-a[index],a,dp);
-            
-        if (take || notTake)
-            dp[index][target]=1;
-        else
-            dp[index][target]=0;
-            
-        return (take || notTake);
+        return prev[target];
     }
 }
