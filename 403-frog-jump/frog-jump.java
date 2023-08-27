f5bc7c6a52b3f4ada8ecf4a66355cd9fb1a2c966
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-27 11:48:50 +0530

    Time: 716 ms (5.01%) | Memory: 215.7 MB (5.01%) - LeetSync

---
+++ 403-frog-jump/frog-jump.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean canCross(int[] stones) {
+        int n=stones.length;
+        if (n>1 && stones[1]>1)
+            return false;
+
+        int[][] dp=new int[2000][10000];
+        for (int[] arr: dp)
+            Arrays.fill(arr,-1);
+        return solve(1,1,stones,n,dp);
+    }
+
+    private boolean solve(int pos,int last,int[] stones,int n,int[][] dp)
+    {
+        if (pos==n-1)
+            return true;
+
+        if (dp[pos][last]!=-1)
+            return (dp[pos][last]==1);
+
+        for (int i=pos+1;i<n;i++)
+        {
+            if (stones[i]==stones[pos]+last && solve(i,last,stones,n,dp))
+            {
+                dp[pos][last]=1;
+                return true;
+            }
+
+            if (stones[i]==stones[pos]+last-1 && solve(i,last-1,stones,n,dp))
+            {
+                dp[pos][last]=1;
+                return true;
+            }
+
+            if (stones[i]==stones[pos]+last+1 && solve(i,last+1,stones,n,dp))
+            {
+                dp[pos][last]=1;
+                return true;
+            }
+        }
+
+        dp[pos][last]=0;
+        return false;
+    }
+}
