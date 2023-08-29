13593380f2ce14410a7bfc44061bc7fa9bba57f8
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-08-29 17:54:10 +0530

    Time: 7 ms (99.66%) | Memory: 44.3 MB (58.59%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -1,19 +1,11 @@
 class Solution {
     public int bestClosingTime(String customers) {
         int n=customers.length();
-        int sum=0;
-        for (char ch: customers.toCharArray())
-        {
-            if (ch=='Y')
-                sum++;
-            else
-                sum--;
-        }
-
         int maxSum=-1*n;
         int currSum=0;
-        int ans=-1;
+        int ans=0;
         int i=0;
+
         for (i=0;i<n;i++)
         {
             if (currSum>maxSum)
