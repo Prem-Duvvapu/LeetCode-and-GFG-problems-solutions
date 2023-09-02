e79e8a7f5cd021494b420577e6ff7fecb1ac6d78
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-09-02 23:58:58 +0530

    Time: 8 ms (100.00%) | Memory: 43.4 MB (50.00%) - LeetSync

---
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean canBeEqual(String s1, String s2) {
+        int n=s1.length();
+        String a="";
+        String b="";
+        String c="";
+        String d="";
+        
+        for (int i=0;i<n;i++)
+        {
+            char ch=s1.charAt(i);
+            if (i%2==0)
+                a+=ch;
+            else
+                c+=ch;
+        }
+        
+        for (int i=0;i<n;i++)
+        {
+            char ch=s2.charAt(i);
+            if (i%2==0)
+                b+=ch;
+            else
+                d+=ch;
+        }
+        
+        return (isAnagram(a,b) && isAnagram(c,d));
+    }
+    
+    private boolean isAnagram(String a,String b)
+    {
+        int[] cnt=new int[26];
+        for (char ch: a.toCharArray())
+            cnt[ch-'a']++;
+        
+        for (char ch: b.toCharArray())
+            cnt[ch-'a']--;
+        
+        for (int val: cnt)
+            if (val!=0)
+                    return false;
+        
+        return true;
+    }
+}
