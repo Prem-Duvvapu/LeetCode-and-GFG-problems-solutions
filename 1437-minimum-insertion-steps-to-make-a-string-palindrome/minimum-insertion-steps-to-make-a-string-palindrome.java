47b8f856366d9e22186b2285974636c3e8cee12b
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-09-02 14:23:12 +0530

    Time: 23 ms (82.19%) | Memory: 40.7 MB (97.60%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,6 +1,6 @@
 //Find string length - longest palindromic subsequence of string
 //To find longest palindromic subsequence of a string, take the string and the reverse of that string and find their longest common subsequence
-//Tabulation
+//Space Optimization
 
 
 class Solution {
@@ -14,19 +14,23 @@ class Solution {
         StringBuilder sb=new StringBuilder(s);
         String t=sb.reverse().toString();
 
-        int[][] dp=new int[n+1][n+1];
+        int[] prev=new int[n+1];
+        int[] curr=new int[n+1];
 
         for (int i=1;i<=n;i++)
         {
             for (int j=1;j<=n;j++)
             {
                 if (s.charAt(i-1)==t.charAt(j-1))
-                    dp[i][j]=1+dp[i-1][j-1];
+                    curr[j]=1+prev[j-1];
                 else
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                    curr[j]=Math.max(prev[j],curr[j-1]);
             }
+
+            for (int k=1;k<=n;k++)
+                prev[k]=curr[k];
         }
 
-        return dp[n][n];
+        return prev[n];
     }
 }
