d74e2ebc8a894962c6153e4608562f86672c0301
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-09-02 14:39:00 +0530

    Time: 7 ms (87.77%) | Memory: 43.5 MB (74.22%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,38 +1,26 @@
 class Solution {
-    String s1;
-    String s2;
-    int[][] dp;
-
-    public int solve(int i,int j)
-    {
-        if (dp[i][j]!=-1)
-            return dp[i][j];
-
-        if (i==s1.length())
-            return dp[i][j]=s2.length()-j;
-
-        if (j==s2.length())
-            return dp[i][j]=s1.length()-i;
-
-        if (dp[i][j]!=-1)
-            return dp[i][j];
-
-        if (s1.charAt(i)==s2.charAt(j))
-            return dp[i][j]=solve(i+1,j+1);
-
-        int delete_char_i=1+solve(i+1,j);
-        int delete_char_j=1+solve(i,j+1);
-
-        return dp[i][j]=Math.min(delete_char_i,delete_char_j);
+    public int minDistance(String word1, String word2) {
+        int n=word1.length();
+        int m=word2.length();
+        int lcs=longestCommonSubsequence(word1,word2,n,m);
+        return ((n-lcs)+(m-lcs));
     }
 
-    public int minDistance(String word1, String word2) {
-        s1=word1;
-        s2=word2;
-        dp=new int[s1.length()+1][s2.length()+1];
-        for (int i=0;i<=s1.length();i++)
-            for (int j=0;j<=s2.length();j++)
-                dp[i][j]=-1;
-        return solve(0,0);
+    private int longestCommonSubsequence(String s,String t,int n,int m)
+    {
+        int[][] dp=new int[n+1][m+1];
+
+        for (int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=m;j++)
+            {
+                if (s.charAt(i-1)==t.charAt(j-1))
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+
+        return dp[n][m];
     }
 }
