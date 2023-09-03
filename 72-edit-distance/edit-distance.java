de35f9c14c2fa3c2b44cf4939ebf3e24cb8c8504
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-09-03 22:40:42 +0530

    Time: 2 ms (100.00%) | Memory: 43.5 MB (68.80%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,28 +1,30 @@
+//Memoization(right shift by 1)
+
 class Solution {
     public int minDistance(String word1, String word2) {
         int n=word1.length();
         int m=word2.length();
-        int[][] dp=new int[n][m];
+        int[][] dp=new int[n+1][m+1];
 
         for (int[] arr: dp)
             Arrays.fill(arr,-1);
 
-        return solve(n-1,m-1,word1,word2,dp);
+        return solve(n,m,word1,word2,dp);
     }
 
     private int solve(int i,int j,String s,String t,int[][] dp)
     {
-        if (i<0)
-            return j+1;
+        if (i==0)
+            return j;
 
-        if (j<0)
-            return i+1;
+        if (j==0)
+            return i;
 
         if (dp[i][j]!=-1)
             return dp[i][j];
 
         int match=1000,insert=1000,delete=1000,replace=1000;
-        if (s.charAt(i)==t.charAt(j))
+        if (s.charAt(i-1)==t.charAt(j-1))
             match=solve(i-1,j-1,s,t,dp);
         else
         {
