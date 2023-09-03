b0c1224ff1b241967018e5164157eebac6eae6f6
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-09-03 15:12:33 +0530

    Time: 0 ms (100.00%) | Memory: 39.3 MB (55.00%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,33 +1,37 @@
-//Tabulation
+//Space Optimization
 
 class Solution {
     int mod=(int)(2e9);
 
     public int uniquePaths(int m, int n) {
-        int[][] dp=new int[m][n];
+        int[] prev=new int[n];
+        int[] curr=new int[n];
         
         for (int i=0;i<m;i++)
         {
             for (int j=0;j<n;j++)
             {
                 if (i==0 && j==0)
-                    dp[i][j]=1;
+                    curr[j]=1;
                 else
                 {
                     int left=0;
                     int top=0;
 
                     if (j-1>=0)
-                        left=dp[i][j-1];
+                        left=curr[j-1];
                     
                     if (i-1>=0)
-                        top=dp[i-1][j];
+                        top=prev[j];
 
-                    dp[i][j]=(left+top)%mod;
+                    curr[j]=(left+top)%mod;
                 }
             }
+
+            for (int k=0;k<n;k++)
+                prev[k]=curr[k];
         }
 
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 }
