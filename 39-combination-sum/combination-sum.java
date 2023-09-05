3022ed03da79aaf3d927b928c31262c536c981aa
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-09-05 23:09:06 +0530

    Time: 2 ms (75.77%) | Memory: 43.8 MB (62.62%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,28 +1,31 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Arrays.sort(candidates);
         List<List<Integer>> res=new ArrayList<>();
-        solve(0,new ArrayList<>(),candidates,target,res);
+        Arrays.sort(candidates);
+        solve(0,new ArrayList<>(),target,candidates,res);
         return res;
     }
 
-    private void solve(int pos,List<Integer> curr,int[] candidates,int target,List<List<Integer>> res)
+    private void solve(int pos,List<Integer> currList,int target,int[] candidates,List<List<Integer>> res)
     {
         if (target==0)
         {
-            res.add(new ArrayList<>(curr));
+            res.add(new ArrayList<>(currList));
             return;
         }
 
-        if (pos==candidates.length || candidates[pos]>target)
+        if (pos==candidates.length)
             return;
 
-        //pick
-        curr.add(candidates[pos]);
-        solve(pos,curr,candidates,target-candidates[pos],res);
-        curr.remove(curr.size()-1);
+        if (candidates[pos]>target)
+            return;
+
+        //pick and stay there since infinite supply
+        currList.add(candidates[pos]);
+        solve(pos,currList,target-candidates[pos],candidates,res);
+        currList.remove(currList.size()-1);
 
         //not pick
-        solve(pos+1,curr,candidates,target,res);
+        solve(pos+1,currList,target,candidates,res);
     }
 }
