9c0162ea9f23533378b9b3c24f4504399d1992ef
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-09-05 14:54:11 +0530

    Time: 17 ms (79.19%) | Memory: 47.4 MB (11.08%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -5,20 +5,20 @@ class Solution {
         int m=p.length();
         int n=s.length();
         Boolean[][] dp=new Boolean[m+1][n+1];
-        return solve(m-1,n-1,p,s,dp);
+        return solve(m,n,p,s,dp);
     }
 
     private boolean solve(int i,int j,String p,String s,Boolean[][] dp)
     {
-        if (i<0 && j<0)
+        if (i==0 && j==0)
             return true;
 
-        if (i<0 && j>=0)
+        if (i==0 && j>0)
             return false;
 
-        if (j<0 && i>=0)
+        if (j==0 && i>0)
         {
-            for (int k=0;k<=i;k++)
+            for (int k=0;k<i;k++)
                 if (p.charAt(k)!='*')
                     return false;
 
@@ -28,9 +28,9 @@ class Solution {
         if (dp[i][j]!=null)
             return dp[i][j];
 
-        if (p.charAt(i)=='?' || p.charAt(i)==s.charAt(j))
+        if (p.charAt(i-1)=='?' || p.charAt(i-1)==s.charAt(j-1))
             return dp[i][j]=solve(i-1,j-1,p,s,dp);
-        else if (p.charAt(i)=='*')
+        else if (p.charAt(i-1)=='*')
             return dp[i][j]=(solve(i-1,j,p,s,dp) || solve(i,j-1,p,s,dp));
 
         return dp[i][j]=false;
