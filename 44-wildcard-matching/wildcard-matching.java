d529155a5df30ab4030ac1b9087d404053ae9be4
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-09-05 15:03:08 +0530

    Time: 33 ms (24.31%) | Memory: 47 MB (24.64%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -2,37 +2,37 @@
 
 class Solution {
     public boolean isMatch(String s, String p) {
-        int m=p.length();
-        int n=s.length();
-        Boolean[][] dp=new Boolean[m+1][n+1];
-        return solve(m,n,p,s,dp);
-    }
+        int n=p.length();
+        int m=s.length();
+        Boolean[][] dp=new Boolean[n+1][m+1];
 
-    private boolean solve(int i,int j,String p,String s,Boolean[][] dp)
-    {
-        if (i==0 && j==0)
-            return true;
+        dp[0][0]=true;
 
-        if (i==0 && j>0)
-            return false;
+        for (int j=1;j<=m;j++)
+            dp[0][j]=false;
 
-        if (j==0 && i>0)
+        for (int i=1;i<=n;i++)
         {
+            dp[i][0]=true;
+
             for (int k=0;k<i;k++)
                 if (p.charAt(k)!='*')
-                    return false;
-
-            return true;
+                    dp[i][0]=false;
         }
 
-        if (dp[i][j]!=null)
-            return dp[i][j];
+        for (int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=m;j++)
+            {
+                if (p.charAt(i-1)=='?' || p.charAt(i-1)==s.charAt(j-1))
+                    dp[i][j]=dp[i-1][j-1];
+                else if (p.charAt(i-1)=='*')
+                    dp[i][j]=(dp[i-1][j] || dp[i][j-1]);
+                else
+                    dp[i][j]=false;
+            }
+        }
 
-        if (p.charAt(i-1)=='?' || p.charAt(i-1)==s.charAt(j-1))
-            return dp[i][j]=solve(i-1,j-1,p,s,dp);
-        else if (p.charAt(i-1)=='*')
-            return dp[i][j]=(solve(i-1,j,p,s,dp) || solve(i,j-1,p,s,dp));
-
-        return dp[i][j]=false;
+        return dp[n][m];
     }
 }
