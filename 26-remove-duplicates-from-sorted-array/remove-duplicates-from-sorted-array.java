c2b1a4790f955c990122f96c5243d360199b5427
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-09-07 00:21:16 +0530

    Time: 1 ms (97.53%) | Memory: 44.2 MB (17.47%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -3,10 +3,8 @@ class Solution {
         int pos=1;
 
         for (int i=1;i<nums.length;i++)
-        {
             if (nums[i]>nums[i-1])
                 nums[pos++]=nums[i];
-        }
 
         return pos;
     }
