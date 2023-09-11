f53e635f4d974dc6d0b6d8c46a2f99f65694b052
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-09-11 13:14:15 +0530

    Time: 1 ms (93.78%) | Memory: 44.2 MB (20.89%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,11 +1,11 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res=new ArrayList<>();
-        solve(new ArrayList<>(),new boolean[nums.length],nums,res);
+        solve(0,new ArrayList<>(),nums,res);
         return res;
     }
 
-    private void solve(List<Integer> currList,boolean[] visited,int[] nums,List<List<Integer>> res)
+    private void solve(int pos,List<Integer> currList,int[] nums,List<List<Integer>> res)
     {
         if (currList.size()==nums.length)
         {
@@ -13,16 +13,20 @@ class Solution {
             return;
         }
 
-        for (int i=0;i<nums.length;i++)
+        for (int i=pos;i<nums.length;i++)
         {
-            if (visited[i])
-                continue;
-
-            currList.add(nums[i]);
-            visited[i]=true;
-            solve(currList,visited,nums,res);
-            visited[i]=false;
+            swap(nums,pos,i);
+            currList.add(nums[pos]);
+            solve(pos+1,currList,nums,res);
             currList.remove(currList.size()-1);
+            swap(nums,pos,i);
         }
+    }
+
+    private void swap(int[] nums,int i,int j)
+    {
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
 }
