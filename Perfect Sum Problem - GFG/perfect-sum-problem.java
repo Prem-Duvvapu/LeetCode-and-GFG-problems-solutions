37795b4afd377c5279ec614dccd9d21a3ae45916
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-09-14 22:59:13 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.java
+++ Perfect Sum Problem - GFG/perfect-sum-problem.java
@@ -27,36 +27,40 @@ class GfG
 
 class Solution{
     int mod=(int)(1e9)+7;
-	public int perfectSum(int a[],int n, int sum) 
+
+	public int perfectSum(int arr[],int n, int sum) 
 	{
-	    int[] prev=new int[sum+1];
-	    int[] curr=new int[sum+1];
-	    
-	    prev[0]=0;
-	    curr[0]=0;
-	    
-	    if (a[0]==0)
-	        prev[0]=2;
-	    else
-	        prev[0]=1;
-	        
-	    if (a[0]!=0 && a[0]<=sum)
-	        prev[a[0]]=1;
-	        
-	    for (int i=1;i<n;i++)
-	    {
-	        for (int target=0;target<=sum;target++)
-	        {
-	            int notPick=prev[target];
-	            int pick=0;
-	            if (a[i]<=target)
-	                pick=prev[target-a[i]];
-	           
-	            curr[target]=(pick+notPick)%mod;
-	        }
-	        System.arraycopy(curr,0,prev,0,sum+1);
-	    }
-	    
-	    return prev[sum];
+	    int[][] dp=new int[n][sum+1];
+	    for (int i=0;i<n;i++)
+	        for (int j=0;j<=sum;j++)
+	            dp[i][j]=-1;
+	    return solve(0,sum,arr,dp);
 	} 
+	
+	
+	private int solve(int pos,int sum,int[] arr,int[][] dp)
+	{
+	    if (sum<0)
+	        return 0;
+	        
+	    if (pos==arr.length-1)
+	    {
+	        if (sum==0 && arr[pos]==0)
+	            return 2;
+	        else if (sum==0 || arr[pos]==sum)
+	            return 1;
+	        return 0;
+	    }
+	        
+	    if (dp[pos][sum]!=-1)
+	        return dp[pos][sum];
+	    
+	    //pick
+	    int pick=solve(pos+1,sum-arr[pos],arr,dp);
+	    
+	    //not pick
+	    int notPick=solve(pos+1,sum,arr,dp);
+	    
+	    return dp[pos][sum]=(pick+notPick)%mod;
+	}
 }
