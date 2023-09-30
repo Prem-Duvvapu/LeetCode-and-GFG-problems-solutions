78f082308655927f32ee78f6d543d4e085969969
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-09-30 11:53:22 +0530

    Added solution - LeetHub

--- Boolean Matrix - GFG/boolean-matrix.java
+++ Boolean Matrix - GFG/boolean-matrix.java
@@ -54,29 +54,50 @@ class Solution
         int n=matrix.length;
         int m=matrix[0].length;
         
-        boolean[] rowCheck=new boolean[n];
-        boolean[] colCheck=new boolean[m];
+        boolean firstRow=false;
+        boolean firstCol=false;
         
-        for (int i=0;i<n;i++)
+        for (int j=0;j<m;j++)
         {
-            for (int j=0;j<m;j++)
+            if (matrix[0][j]==1)
             {
-                if (matrix[i][j]==1)
-                {
-                    rowCheck[i]=true;
-                    colCheck[j]=true;
-                }
+                firstRow=true;
+                break;
             }
         }
         
         for (int i=0;i<n;i++)
-            if (rowCheck[i])
-                for (int j=0;j<m;j++)
-                    matrix[i][j]=1;
+        {
+            if (matrix[i][0]==1)
+            {
+                firstCol=true;
+                break;
+            }
+        }
         
-        for (int j=0;j<m;j++)
-            if (colCheck[j])
-                for (int i=0;i<n;i++)
+        for (int i=1;i<n;i++)
+        {
+            for (int j=1;j<m;j++)
+            {
+                if (matrix[i][j]==1)
+                {
+                    matrix[0][j]=1;
+                    matrix[i][0]=1;
+                }
+            }
+        }
+        
+        for (int i=1;i<n;i++)
+            for (int j=1;j<m;j++)
+                if (matrix[0][j]==1 || matrix[i][0]==1)
                     matrix[i][j]=1;
+            
+        if (firstRow)
+            for (int j=0;j<m;j++)
+                matrix[0][j]=1;
+                
+        if (firstCol)
+            for (int i=0;i<n;i++)
+                matrix[i][0]=1;
     }
 }
