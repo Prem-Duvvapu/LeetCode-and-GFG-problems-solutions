1553340860b028f098ee738136bf8a962f44c7eb
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-10-25 13:15:13 +0530

    Time: 65 ms (32.81%) | Memory: 59.6 MB (33.07%) - LeetSync

---
+++ 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] mostCompetitive(int[] nums, int k) {
+        int n=nums.length;
+        Stack<Integer> stack=new Stack<>();
+        for (int i=0;i<n;i++)
+        {
+            while (!stack.isEmpty() && stack.peek()>nums[i] && (stack.size()-1+n-i)>=k)
+                stack.pop();
+
+            if (stack.size()<k)
+                stack.push(nums[i]);
+        }
+
+        int[] res=new int[k];
+        for (int i=k-1;i>=0;i--)
+            res[i]=stack.pop();
+
+        return res;
+    }
+}
+
