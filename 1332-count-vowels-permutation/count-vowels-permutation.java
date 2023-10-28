ca998e24b87daff7a9d66947f617a14262859c32
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-10-28 17:37:06 +0530

    Time: 15 ms (58.09%) | Memory: 43.1 MB (69.31%) - LeetSync

--- 1332-count-vowels-permutation/count-vowels-permutation.java
+++ 1332-count-vowels-permutation/count-vowels-permutation.java
@@ -1,42 +1,28 @@
-//Memoization
+//Tabulation
 class Solution {
     int mod=(int)(1e9)+7;
     int a=0,e=1,i=2,o=3,u=4;
-    int[][] dp;
+    long[][] dp;
 
     public int countVowelPermutation(int n) {
-        int result=0;
-        dp=new int[5][n];
+        long result=0;
+        dp=new long[5][n];
 
-        for (int[] arr: dp)
-            Arrays.fill(arr,-1);
+        for (int i=0;i<5;i++)
+            dp[i][0]=1;
 
-        result=(result+solve(a,n-1))%mod;
-        result=(result+solve(e,n-1))%mod;
-        result=(result+solve(i,n-1))%mod;
-        result=(result+solve(o,n-1))%mod;
-        result=(result+solve(u,n-1))%mod;
+        for (int pos=1;pos<n;pos++)
+        {
+            dp[a][pos]=dp[e][pos-1]%mod;
+            dp[e][pos]=(dp[a][pos-1]+dp[i][pos-1])%mod;
+            dp[i][pos]=(dp[a][pos-1]+dp[e][pos-1]+dp[o][pos-1]+dp[u][pos-1])%mod;
+            dp[o][pos]=(dp[i][pos-1]+dp[u][pos-1])%mod;
+            dp[u][pos]=dp[a][pos-1]%mod;
+        }
 
-        return result;
-    }
+        for (int i=0;i<5;i++)
+            result=(result+dp[i][n-1])%mod;
 
-    private int solve(int prev,int pos)
-    {
-        if (pos==0)
-            return 1;
-
-        if (dp[prev][pos]!=-1)
-            return dp[prev][pos];
-
-        if (prev==a)
-            return dp[prev][pos]=solve(e,pos-1)%mod;
-        else if (prev==e)
-            return dp[prev][pos]=(solve(a,pos-1)%mod+solve(i,pos-1)%mod)%mod;
-        else if (prev==i)
-            return dp[prev][pos]=((solve(a,pos-1)%mod+solve(e,pos-1)%mod)%mod+(solve(o,pos-1)%mod+solve(u,pos-1)%mod)%mod)%mod;
-        else if (prev==o)
-            return dp[prev][pos]=(solve(i,pos-1)%mod+solve(u,pos-1)%mod)%mod;
-        
-        return dp[prev][pos]=solve(a,pos-1)%mod;
+        return (int)result;
     }
 }
