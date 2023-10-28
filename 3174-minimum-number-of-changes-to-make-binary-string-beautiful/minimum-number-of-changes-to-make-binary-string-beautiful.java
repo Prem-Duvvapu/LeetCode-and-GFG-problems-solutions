622c990823c3251a4e766ebf4f5500a21a34bf29
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-10-28 23:46:13 +0530

    Time: 3 ms (100.00%) | Memory: 43.7 MB (50.00%) - LeetSync

---
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minChanges(String s) {
+        int n=s.length();
+        int cnt=0;
+        
+        for (int i=1;i<n;i+=2)
+            if (s.charAt(i)!=s.charAt(i-1))
+                cnt++;
+        
+        return cnt;
+    }
+}
