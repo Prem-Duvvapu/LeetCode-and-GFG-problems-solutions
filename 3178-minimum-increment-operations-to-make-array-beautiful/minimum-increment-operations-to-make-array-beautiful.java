095c99bedb2459a2f2bfa9362c95cba1d8a9b62a
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-10-29 10:40:49 +0530

    Time: 4 ms (100.00%) | Memory: 61.3 MB (33.33%) - LeetSync

--- 3178-minimum-increment-operations-to-make-array-beautiful/minimum-increment-operations-to-make-array-beautiful.java
+++ 3178-minimum-increment-operations-to-make-array-beautiful/minimum-increment-operations-to-make-array-beautiful.java
@@ -1,50 +1,18 @@
-//Memoization
+//Tabulation
 class Solution {
     public long minIncrementOperations(int[] nums, int k) {
         int n=nums.length;
-        long[] dp=new long[n];
+        long[] dp=new long[n+1];
         
-        for (int i=0;i<n;i++)
-                dp[i]=-1;
+        for (int i=n-3;i>=0;i--)
+        {
+            long a=Math.max(0,k-nums[i])+dp[i+1];
+            long b=Math.max(0,k-nums[i+1])+dp[i+2];
+            long c=Math.max(0,k-nums[i+2])+dp[i+3];
+
+            dp[i]=Math.min(a,Math.min(b,c));
+        }
         
-        long ans=solve(0,nums,k,dp);
-        return ans;
-    }
-    
-    private long solve(int pos,int[] nums,int k,long[] dp)
-    {
-        if (pos>nums.length-3)
-            return 0;
-        
-        if (dp[pos]!=-1)
-            return dp[pos];
-        
-        long a=Math.max(0,k-nums[pos])+solve(pos+1,nums,k,dp);
-        long b=Math.max(0,k-nums[pos+1])+solve(pos+2,nums,k,dp);
-        long c=Math.max(0,k-nums[pos+2])+solve(pos+3,nums,k,dp);
-        
-        return dp[pos]=Math.min(a,Math.min(b,c));  
+        return dp[0];
     }
 }
-
-// for (int i=0;i<=n-3;i++)
-//         {
-//             int mx=nums[i];
-//             int index=i;
-//             for (int j=i;j<i+3;j++)
-//             {
-//                 if (nums[j]>=mx)
-//                 {
-//                     mx=nums[j];
-//                     index=j;
-//                 }
-//             }
-            
-//             if (mx<k)
-//             {
-//                 ans+=(k-mx);
-//                 nums[index]=k;
-//             }
-//         }
-        
-//         return ans;
