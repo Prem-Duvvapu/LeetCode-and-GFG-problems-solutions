3efee3d0b7323c496585c4917ea8f87d3e4ff4af
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-11-04 20:35:00 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (9.89%) - LeetSync

--- 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.java
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.java
@@ -1,12 +1,11 @@
 class Solution {
-    public int getLastMoment(int n, int[] left, int[] right) {
-        Arrays.sort(left);
-        Arrays.sort(right);
+    public int getLastMoment(int n, int[] left, int[] right)
+    {
         int ans=0;
-        if (left.length>0)
-            ans=Math.max(ans,left[left.length-1]);
-        if (right.length>0)
-            ans=Math.max(ans,n-right[0]);
+        for (int val: left)
+            ans=Math.max(ans,val);
+        for (int val: right)
+            ans=Math.max(ans,n-val);
 
         return ans;
     }
