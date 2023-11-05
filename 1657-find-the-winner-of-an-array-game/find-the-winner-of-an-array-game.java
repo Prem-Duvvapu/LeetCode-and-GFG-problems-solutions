db0e78a34e5792bdc368aeb6d4dabc4088f743bc
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-11-05 09:51:04 +0530

    Time: 0 ms (100.00%) | Memory: 56.4 MB (65.94%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.java
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.java
@@ -1,36 +1,21 @@
 class Solution {
     public int getWinner(int[] arr, int k) {
         int n=arr.length;
-        int ans=arr[0];
-        Map<Integer,Integer> map=new HashMap<>();
+        int max=arr[0];
+        int win=0;
 
-        int maxVal=arr[0];
-        for (int val: arr)
-            maxVal=Math.max(maxVal,val);
-
-        int i=0;
-        int j=1;
-
-        while (true)
+        for (int i=1;i<n;i++)
         {
-            if (arr[i]==maxVal || arr[j]==maxVal)
-                return maxVal;
-            if (arr[i]>arr[j])
+            if (arr[i]>max)
             {
-                map.put(arr[i],map.getOrDefault(arr[i],0)+1);
-                if (map.get(arr[i])==k)
-                    return arr[i];
-                
-                j=(Math.max(i,j)+1)%n;
+                max=arr[i];
+                win=0;
             }
-            else
-            {
-                map.put(arr[j],map.getOrDefault(arr[j],0)+1);
-                if (map.get(arr[j])==k)
-                    return arr[j];
-                
-                i=(Math.max(i,j)+1)%n;
-            }
+
+            if (++win==k)
+                return max;
         }
+
+        return max;
     }
 }
