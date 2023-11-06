61790f47cca361f2393c7fc19ae5b3468515483e
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-11-06 19:44:35 +0530

    Time: 28 ms (99.80%) | Memory: 88.6 MB (35.86%) - LeetSync

--- 1955-seat-reservation-manager/seat-reservation-manager.java
+++ 1955-seat-reservation-manager/seat-reservation-manager.java
@@ -1,26 +1,20 @@
 class SeatManager {
-    boolean[] arr;
     int last;
     PriorityQueue<Integer> q;
 
     public SeatManager(int n) {
         last=1;
-        arr=new boolean[n+1];
         q=new PriorityQueue<>();
     }
     
     public int reserve() {
         if (!q.isEmpty())
-        {
-            arr[q.peek()]=true;
             return q.poll();
-        }
-        arr[last]=true;
+
         return last++;
     }
     
     public void unreserve(int seatNumber) {
-        arr[seatNumber]=false;
         q.add(seatNumber);
     }
 }
