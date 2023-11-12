3f2d9455771b460ecee771b386fdc8ba442c4f82
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-11-12 12:13:05 +0530

    Time: 20 ms (100.00%) | Memory: 40.2 MB (50.00%) - LeetSync

---
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long distributeCandies(int n, int limit) {
+        long ans=0;
+        int amin=Math.max(0,n-2*limit);
+        int amax=Math.min(limit,n);
+
+        for (int a=amin;a<=amax;a++)
+        {
+            int bmin=Math.max(0,n-a-limit);
+            int bmax=Math.min(limit,n-a);
+            int diff=bmax-bmin+1;
+            if (diff>0)
+                ans+=diff;
+        }
+
+        return ans;
+    }
+}
