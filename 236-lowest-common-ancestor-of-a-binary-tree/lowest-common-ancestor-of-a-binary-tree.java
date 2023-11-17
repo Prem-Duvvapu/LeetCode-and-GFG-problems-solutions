2c409e50c8dddc7f67e282b633353904cc601831
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-11-18 00:46:59 +0530

    Time: 6 ms (97.51%) | Memory: 44 MB (17.94%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -9,10 +9,7 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root==null)
-            return null;
-
-        if (root==p || root==q)
+        if (root==null || root==p || root==q)
             return root;
 
         TreeNode left=lowestCommonAncestor(root.left,p,q);
@@ -22,7 +19,7 @@ class Solution {
             return right;
         else if (right==null)
             return left;
-        else
-            return root;
+        
+        return root;
     }
 }
