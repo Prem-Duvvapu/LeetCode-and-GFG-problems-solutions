b2aefc3da877250d661626c9a7fe594c9cfd9a67
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-11-17 22:55:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (49.94%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -14,43 +14,24 @@
  * }
  */
 
- //from vertical order traversal
-class Tuple
-{
-    TreeNode node;
-    int row;
-
-    Tuple(TreeNode _node,int _row)
-    {
-        node=_node;
-        row=_row;
-    }
-}
-
+ //Recursive(revere preorder)
+ //Root Right Left
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> res=new ArrayList<>();
-        if (root==null)
-            return res;
-        TreeMap<Integer,Integer> map=new TreeMap<>();
-        Queue<Tuple> q=new ArrayDeque<>();
-        q.offer(new Tuple(root,0));
-
-        while (!q.isEmpty())
-        {
-            Tuple curr=q.poll();
-            map.put(curr.row,curr.node.val);
-
-            if (curr.node.left!=null)
-                q.add(new Tuple(curr.node.left,curr.row+1));
-            
-            if (curr.node.right!=null)
-                q.add(new Tuple(curr.node.right,curr.row+1));
-        }
-
-        for (Integer val: map.values())
-            res.add(val);
-
+        reverseInorder(root,0,res);
         return res;
+    }
+
+    private void reverseInorder(TreeNode root,int level,List<Integer> res)
+    {
+        if (root==null)
+            return;
+
+        if (level==res.size())
+            res.add(root.val);
+
+        reverseInorder(root.right,level+1,res);
+        reverseInorder(root.left,level+1,res);
     }
 }
