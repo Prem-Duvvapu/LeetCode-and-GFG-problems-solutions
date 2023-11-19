f2d7d6b0f6ef24ef4d4339bd249595d79af08c77
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-11-19 11:50:09 +0530

    Time: 34 ms (81.58%) | Memory: 54.9 MB (39.47%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.java
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.java
@@ -2,17 +2,14 @@ class Solution {
     public int reductionOperations(int[] nums) {
         int n=nums.length;
         Arrays.sort(nums);
-        int maxVal=nums[0];
         int k=0;
         int res=0;
 
         for (int i=1;i<n;i++)
         {
-            if (nums[i]>maxVal)
-            {
-                maxVal=nums[i];
+            if (nums[i]!=nums[i-1])
                 k++;
-            }
+
             res+=k;
         }
 
