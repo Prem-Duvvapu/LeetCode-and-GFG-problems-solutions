238ecc25c7e9f9c18fad5109e43dec3f2efa8b9d
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-11-19 11:47:24 +0530

    Time: 33 ms (92.11%) | Memory: 55.1 MB (30.92%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.java
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.java
@@ -1,15 +1,20 @@
 class Solution {
     public int reductionOperations(int[] nums) {
         int n=nums.length;
-        Map<Integer,Integer> map=new TreeMap<>();
+        Arrays.sort(nums);
+        int maxVal=nums[0];
+        int k=0;
         int res=0;
 
-        for (int val: nums)
-            map.put(val,map.getOrDefault(val,0)+1);
-
-        int k=0;
-        for (Integer freq: map.values())
-            res+=(freq*(k++));
+        for (int i=1;i<n;i++)
+        {
+            if (nums[i]>maxVal)
+            {
+                maxVal=nums[i];
+                k++;
+            }
+            res+=k;
+        }
 
         return res;
     }
