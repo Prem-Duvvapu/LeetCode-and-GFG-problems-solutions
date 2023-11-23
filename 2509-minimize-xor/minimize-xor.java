c67287614bc906d345a3f6e92dcbf21abf6ff5ce
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-11-23 23:34:39 +0530

    Time: 0 ms (100.00%) | Memory: 39.3 MB (39.13%) - LeetSync

---
+++ 2509-minimize-xor/minimize-xor.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int minimizeXor(int num1, int num2) {
+        int cnt1=0;
+        int cnt2=0;
+        
+        for (int i=0;i<32;i++)
+        {
+            if ((num1&(1<<i))>0)
+                cnt1++;
+            if ((num2&(1<<i))>0)
+                cnt2++;
+        }
+
+        if (cnt1==cnt2)
+            return num1;
+
+        int x=0;
+        if (cnt2<cnt1)
+        {
+            for (int i=31;i>=0 && cnt2>0;i--)
+            {
+                if ((num1&(1<<i))>0)
+                {
+                    x+=(1<<i);
+                    cnt2--;
+                }
+            }
+            return x;
+        }
+
+        x=num1;
+        cnt2-=cnt1;
+        for (int i=0;i<31 && cnt2>0;i++)
+        {
+            if ((num1&(1<<i))==0)
+            {
+                x+=(1<<i);
+                cnt2--;
+            }
+        }
+        return x;
+    }
+}
