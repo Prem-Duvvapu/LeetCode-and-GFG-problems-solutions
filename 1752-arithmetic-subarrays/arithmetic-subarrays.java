33591aef1cf849dac1f9b56e75005596f876421c
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-11-23 11:35:30 +0530

    Time: 47 ms (16.76%) | Memory: 44.9 MB (9.62%) - LeetSync

---
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+        int n=nums.length;
+        int m=l.length;
+        List<Boolean> res=new ArrayList<>();
+
+        for (int i=0;i<m;i++)
+        {
+            List<Integer> curr=new ArrayList<>();
+            for (int j=l[i];j<=r[i];j++)
+                curr.add(nums[j]);
+            Collections.sort(curr);
+            int diff=curr.get(1)-curr.get(0);
+            int k=1;
+            for (;k<curr.size();k++)
+            {
+                if (curr.get(k)-curr.get(k-1)!=diff)
+                {
+                    res.add(false);
+                    break;
+                }
+            }
+            if (k==curr.size())
+                res.add(true);
+        }
+
+        return res;
+
+    }
+}
