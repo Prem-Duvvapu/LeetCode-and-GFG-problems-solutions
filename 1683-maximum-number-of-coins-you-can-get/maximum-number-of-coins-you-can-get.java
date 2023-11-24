f4bbd34019dac44c0f0f6c6b8ef3c1f066fcc3fa
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-11-24 12:38:59 +0530

    Time: 27 ms (98.94%) | Memory: 55.2 MB (47.44%) - LeetSync

--- 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.java
+++ 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.java
@@ -1,10 +1,11 @@
+//Ex:- SSSMLMLML
 class Solution {
     public int maxCoins(int[] piles) {
         int n=piles.length;
         Arrays.sort(piles);
         int res=0;
 
-        for (int cnt=0,i=n-2;cnt<n/3;cnt++,i-=2)
+        for (int i=n/3;i<n;i+=2)
             res+=piles[i];
 
         return res;
