5568ff6d2f8aa834e631cafc96cda1633cf95ae0
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-12-14 17:38:24 +0530

    Time: 6 ms (82.88%) | Memory: 44.8 MB (13.05%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.java
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.java
@@ -1,15 +1,30 @@
 class Solution {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
-        List<List<Integer>> res = new ArrayList();
-  Map<Integer, List<Integer>> groups = new HashMap<>();
-  for (int i = 0; i < groupSizes.length; ++i) {
-    List<Integer> list = groups.computeIfAbsent(groupSizes[i], k -> new ArrayList());
-    list.add(i);
-    if (list.size() == groupSizes[i]) {
-      res.add(list);
-      groups.put(groupSizes[i], new ArrayList());
-    }
-  }
-  return res;
+        int n=groupSizes.length;
+        List<List<Integer>> res=new ArrayList<>();
+        Map<Integer,List<Integer>> map=new HashMap<>();
+        for (int i=0;i<n;i++)
+        {
+            int curr=groupSizes[i];
+            if (!map.containsKey(curr))
+                map.put(curr,new ArrayList<>());
+
+            map.get(curr).add(i);
+        }
+
+        for (Map.Entry<Integer,List<Integer>> m: map.entrySet())
+        {
+            int currSize=m.getKey();
+            List<Integer> currList=m.getValue();
+            for (int i=0;i<currList.size();i+=currSize)
+            {
+                List<Integer> temp=new ArrayList<>();
+                for (int j=i;j<i+currSize;j++)
+                    temp.add(currList.get(j));
+                res.add(temp);
+            }
+        }
+
+        return res;
     }
 }
