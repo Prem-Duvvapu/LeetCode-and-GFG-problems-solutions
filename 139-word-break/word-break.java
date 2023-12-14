62911f2fb99b6036654f0e8b637c88f7730f8314
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-12-14 23:26:05 +0530

    Time: 18 ms (5.18%) | Memory: 44.5 MB (5.97%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,85 +1,37 @@
-class TrieNode
-{
-    TrieNode[] children;
-    boolean endOfWord;
-
-    TrieNode()
-    {
-        children=new TrieNode[26];
-        endOfWord=false;
-    }
-}
-
-class Trie
-{
-    TrieNode root;
-
-    Trie()
-    {
-        root=new TrieNode();
-    }
-
-    public void insert(String word)
-    {
-        TrieNode curr=root;
-        for (char ch: word.toCharArray())
-        {
-            if (curr.children[ch-'a']==null)
-                curr.children[ch-'a']=new TrieNode();
-            
-            curr=curr.children[ch-'a'];
-        }
-        curr.endOfWord=true;
-    }
-
-    public boolean search(String word)
-    {
-        TrieNode curr=root;
-        for (char ch: word.toCharArray())
-        {
-            if (curr.children[ch-'a']==null)
-                return false;
-
-            curr=curr.children[ch-'a'];
-            
-        }
-        return curr.endOfWord;
-    }
-}
-
 class Solution {
-    private boolean solve(int pos,String s,Trie t,int[] dp)
-    {
-        if (pos==s.length() || dp[pos]==1)
-            return true;
-        
-        if (dp[pos]==0)
-            return false;
-
-        for (int i=pos;i<s.length();i++)
-        {
-            String sub=s.substring(pos,i+1);
-            if (t.search(sub))
-            {
-                if (solve(pos+sub.length(),s,t,dp))
-                {
-                    dp[pos]=1;
-                    return true;
-                }
+    Map<String,Boolean> map;
+    public boolean func(int idx,String s,StringBuilder sb,HashSet<String> hs){
+        if(idx==s.length()){
+            if(hs.contains(sb.toString())){
+                return true;
             }
+            return false;
         }
 
-        dp[pos]=0;
-        return false;
+        String key=idx+" "+sb.toString();
+        if (map.containsKey(key))
+            return map.get(key);
+        
+        if(!hs.contains(sb.toString())){
+            sb.append(s.charAt(idx));
+            return func(idx+1,s,sb,hs);
+        }
+        
+        sb.append(s.charAt(idx));
+        boolean x1=func(idx+1,s,sb,hs);
+        sb.setLength(0);
+        boolean x2=func(idx,s,sb,hs);
+        map.put(key,(x1 || x2));
+        return x1 || x2;
     }
-
     public boolean wordBreak(String s, List<String> wordDict) {
-        int n=s.length();
-        int[] dp=new int[n];
-        Arrays.fill(dp,-1);
-        Trie t=new Trie();
-        for (String word: wordDict)
-            t.insert(word);
-        return solve(0,s,t,dp);
+        map=new HashMap<>();
+        HashSet<String> hs=new HashSet<>();
+        for(int i=0;i<wordDict.size();i++){
+            hs.add(wordDict.get(i));
+        }
+
+        return func(0,s,new StringBuilder(),hs);
+        
     }
 }
