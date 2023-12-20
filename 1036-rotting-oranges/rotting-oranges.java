2c2b37527d51693a348ca0ae92eeffdcb6b28da8
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-12-21 01:09:55 +0530

    Time: 2 ms (84.27%) | Memory: 42.6 MB (11.96%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -0,0 +1,60 @@
+class Pair {
+    int row;
+    int col;
+
+    Pair(int _row,int _col) {
+        row=_row;
+        col=_col;
+    }
+}
+
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        Queue<Pair> q=new LinkedList<>();
+        int freshCnt=0;
+        boolean[][] visited=new boolean[m][n];
+
+        for (int i=0;i<m;i++) {
+            for (int j=0;j<n;j++) {
+                if (grid[i][j]==1)
+                    freshCnt++;
+                else if (grid[i][j]==2) {
+                    q.add(new Pair(i,j));
+                    visited[i][j]=true;
+                }
+            }
+        }
+
+        if (freshCnt==0)
+            return 0;
+
+        int[] drow={-1,0,1,0};
+        int[] dcol={0,1,0,-1};
+        int time=-1;
+
+        while (!q.isEmpty()) {
+            int qlen=q.size();
+            time++;
+            while (qlen-- > 0) {
+                Pair curr=q.poll();
+                for (int i=0;i<4;i++) {
+                    int nrow=curr.row+drow[i];
+                    int ncol=curr.col+dcol[i];
+
+                    if (nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol]==1 && !visited[nrow][ncol]) {
+                        q.add(new Pair(nrow,ncol));
+                        visited[nrow][ncol]=true;
+                        freshCnt--;
+                    }
+                }
+            }
+        }
+
+        if (freshCnt>0)
+            return -1;
+
+        return time;
+    }
+}
