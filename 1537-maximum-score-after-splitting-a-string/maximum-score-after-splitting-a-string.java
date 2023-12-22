e6848cbb41b314c5a190d2db199850f7ed2fdfaf
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-12-22 11:55:29 +0530

    Time: 1 ms (97.78%) | Memory: 41.3 MB (33.33%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -3,16 +3,19 @@ class Solution {
         int n=s.length();
         int[] suffix=new int[n];
         int zeroCnt=0;
+        int oneCnt=0;
         int maxScore=0;
 
-        suffix[n-1]=s.charAt(n-1)-'0';
-        for (int i=n-2;i>=0;i--)
-            suffix[i]=suffix[i+1]+s.charAt(i)-'0';
+        for (int i=0;i<n;i++)
+            if (s.charAt(i)=='1')
+                oneCnt++;
         
         for (int i=0;i<n-1;i++) {
             if (s.charAt(i)=='0')
                 zeroCnt++;
-            maxScore=Math.max(maxScore,zeroCnt+suffix[i+1]);
+            else
+                oneCnt--;
+            maxScore=Math.max(maxScore,zeroCnt+oneCnt);
         }
 
         return maxScore;
