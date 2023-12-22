4458919b9ea32c1a093e50806b463323e327ee9f
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-12-22 23:01:12 +0530

    Time: 36 ms (16.60%) | Memory: 45.4 MB (7.28%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.java
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.java
@@ -1,21 +1,37 @@
 class Solution {
     public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+        int n=nums.length;
+        int m=l.length;
         List<Boolean> res=new ArrayList<>();
-        for (int i=0;i<l.length;i++) {
-            List<Integer> temp=new ArrayList<>();
+
+        for (int i=0;i<m;i++) {
+            int minVal=Integer.MAX_VALUE;
+            int maxVal=Integer.MIN_VALUE;
+            Set<Integer> set=new HashSet<>();
+
             for (int j=l[i];j<=r[i];j++) {
-                temp.add(nums[j]);
+                minVal=Math.min(minVal,nums[j]);
+                maxVal=Math.max(maxVal,nums[j]);
+                set.add(nums[j]);
             }
-            Collections.sort(temp);
+
+            if ((maxVal-minVal)%(r[i]-l[i])!=0) {
+                res.add(false);
+                continue;
+            }
+
+            int d=(maxVal-minVal)/(r[i]-l[i]);
+            int curr=minVal+d;
             boolean flag=true;
-            int diff=temp.get(1)-temp.get(0);
-            for (int k=2;k<temp.size();k++) {
-                if (temp.get(k)-temp.get(k-1)!=diff) {
+            while (curr<maxVal) {
+                if (!set.contains(curr)) {
                     res.add(false);
                     flag=false;
                     break;
                 }
+                curr+=d;
             }
+
             if (flag)
                 res.add(true);
         }
