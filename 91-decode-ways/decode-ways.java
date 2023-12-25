4eae5f234df4a7386081b3c6ea2ef3ab97f7621c
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-12-25 18:21:11 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (6.41%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,20 +1,22 @@
-//tabulation
+//Space optimization
 class Solution {
     public int numDecodings(String s) {
         int n=s.length();
-        int[] dp=new int[n+1];
-        dp[n]=1;
+        int dp=0,dp1=1,dp2=0;
         
         for (int pos=n-1;pos>=0;pos--) {
             if (s.charAt(pos)=='0')
-                dp[pos]=0;
+                dp=0;
             else
-                dp[pos]=dp[pos+1];
+                dp=dp1;
 
             if (pos+1<n && (s.charAt(pos)=='1' || s.charAt(pos)=='2' && s.charAt(pos+1)<'7'))
-                dp[pos]+=dp[pos+2];
+                dp+=dp2;
+
+            dp2=dp1;
+            dp1=dp;
         }
 
-        return dp[0];
+        return dp;
     }
 }
