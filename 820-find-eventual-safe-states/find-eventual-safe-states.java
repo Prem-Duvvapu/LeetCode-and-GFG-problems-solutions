78a4604464587f138b31decc67c0a6ab9793e192
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-12-25 22:57:52 +0530

    Time: 4 ms (98.36%) | Memory: 56 MB (5.10%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -4,30 +4,33 @@ class Solution {
         int n=graph.length;
         boolean[] visited=new boolean[n];
         boolean[] path=new boolean[n];
+        boolean[] safe=new boolean[n];
         List<Integer> res=new ArrayList<>();
 
         for (int i=0;i<n;i++)
             if (!visited[i])
-                dfs(i,visited,path,res,graph);
+                dfs(i,visited,path,safe,graph);
 
-        Collections.sort(res);
+        for (int i=0;i<n;i++)
+            if (safe[i])
+                res.add(i);
         return res;
     }
 
-    private boolean dfs(int i,boolean[] visited,boolean[] path,List<Integer> res,int[][] graph) {
+    private boolean dfs(int i,boolean[] visited,boolean[] path,boolean[] safe,int[][] graph) {
         visited[i]=true;
         path[i]=true;
 
         for (int neighbour: graph[i]) {
             if (!visited[neighbour]) {
-                if (dfs(neighbour,visited,path,res,graph))
+                if (dfs(neighbour,visited,path,safe,graph))
                     return true;
             }
             else if (path[neighbour])
                 return true;
         }
 
-        res.add(i);
+        safe[i]=true;
         path[i]=false;
         return false;
     }
