a41fd587fb07f9ff52c3f931872c76f8afaefb17
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-12-25 18:17:12 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (13.24%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,25 +1,20 @@
+//tabulation
 class Solution {
     public int numDecodings(String s) {
         int n=s.length();
-        int[] dp=new int[n];
-        Arrays.fill(dp,-1);
-        return solve(0,s,n,dp);
-    }
+        int[] dp=new int[n+1];
+        dp[n]=1;
+        
+        for (int pos=n-1;pos>=0;pos--) {
+            if (s.charAt(pos)=='0')
+                dp[pos]=0;
+            else
+                dp[pos]=dp[pos+1];
 
-    private int solve(int pos,String s,int n,int[] dp) {
-        if (pos==n)
-            return 1;
+            if (pos+1<n && (s.charAt(pos)=='1' || s.charAt(pos)=='2' && s.charAt(pos+1)<'7'))
+                dp[pos]+=dp[pos+2];
+        }
 
-        if (s.charAt(pos)=='0')
-            return 0;
-
-        if (dp[pos]!=-1)
-            return dp[pos];
-
-        int res=solve(pos+1,s,n,dp);
-        if (pos+1<n && (s.charAt(pos)=='1' || s.charAt(pos)=='2' && s.charAt(pos+1)<'7'))
-            res+=solve(pos+2,s,n,dp);
-
-        return dp[pos]=res;
+        return dp[0];
     }
 }
