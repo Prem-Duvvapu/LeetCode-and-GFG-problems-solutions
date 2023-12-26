961c1a2084b7d93254ee7371df99f5dc52ff919a
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-12-26 17:22:53 +0530

    Time: 6 ms (47.73%) | Memory: 44.2 MB (59.95%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,41 @@
+//using topological sort
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int v=numCourses;
+        int n=prerequisites.length;
+        List<Integer>[] graph=new ArrayList[v];
+        for (int i=0;i<v;i++)
+            graph[i]=new ArrayList<>();
+
+        for (int[] i: prerequisites)
+            graph[i[1]].add(i[0]);
+
+        //topo sort
+        int[] indegree=new int[v];
+        Queue<Integer> q=new LinkedList<>();
+
+        for (int i=0;i<v;i++)
+            for (int node: graph[i])
+                indegree[node]++;
+
+        for (int i=0;i<v;i++)
+            if (indegree[i]==0)
+                q.add(i);
+
+        while (!q.isEmpty()) {
+            int curr=q.poll();
+
+            for (int neighbour: graph[curr]) {
+                indegree[neighbour]--;
+                if (indegree[neighbour]==0)
+                    q.add(neighbour);
+            }
+        }
+
+        for (int i=0;i<v;i++)
+            if (indegree[i]>0)
+                return false;
+
+        return true;
+    }
+}
