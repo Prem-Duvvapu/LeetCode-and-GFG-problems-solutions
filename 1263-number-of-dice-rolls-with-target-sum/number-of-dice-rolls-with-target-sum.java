6aeed5dcbbaa7a4bbe1dbdd2a5ec4fae2dd8db23
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2023-12-26 11:35:09 +0530

    Time: 14 ms (43.24%) | Memory: 41.7 MB (26.87%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -1,31 +1,22 @@
-//Memoization
+//Tabulation
 class Solution {
     int mod=(int)1e9+7;
     public int numRollsToTarget(int n, int k, int target) {
         int[][] dp=new int[n+1][target+1];
-        for (int i=0;i<=n;i++)
-            for (int j=0;j<=target;j++)
-                dp[i][j]=-1;
-        return solve(n,k,target,dp);
-    }
+        dp[0][0]=1;
 
-    private int solve(int n,int k,int target,int[][] dp) {
-        if (n==0) {
-            if (target==0)
-                return 1;
-            return 0;
+        for (int pos=1;pos<=n;pos++) {
+            for (int i=1;i<=target;i++) {
+                int cnt=0;
+                for (int j=1;j<=k;j++) {
+                    if (i-j>=0)
+                        cnt=(cnt+dp[pos-1][i-j])%mod;
+                }
+                dp[pos][i]=cnt;
+            }
         }
+        
 
-        if (target<0)
-            return 0;
-
-        if (dp[n][target]!=-1)
-            return dp[n][target];
-
-        int cnt=0;
-        for (int i=1;i<=k;i++)
-            cnt=(cnt+solve(n-1,k,target-i,dp))%mod;
-
-        return dp[n][target]=cnt;
+        return dp[n][target];
     }
 }
