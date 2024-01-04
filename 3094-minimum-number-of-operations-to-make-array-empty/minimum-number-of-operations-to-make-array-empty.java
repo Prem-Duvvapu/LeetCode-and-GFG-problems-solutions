a96c5550f1b734fffd8986af9efa5e94691cd1b4
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2024-01-04 19:39:10 +0530

    Time: 42 ms (14.72%) | Memory: 57.2 MB (47.92%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.java
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.java
@@ -1,24 +1,40 @@
 class Solution {
     public int minOperations(int[] nums) {
         int n=nums.length;
-        int ans=0;
         Map<Integer,Integer> map=new HashMap<>();
-        
-        for (int val: nums)
-            map.put(val,map.getOrDefault(val,0)+1);
-        
-        for (Map.Entry<Integer,Integer> m: map.entrySet())
-        {
+        for (int val: nums) {
+            if (!map.containsKey(val))
+                map.put(val,0);
+            map.put(val,map.get(val)+1);
+        }
+
+        System.out.println(map);
+
+        int ops=0;
+        for (Map.Entry<Integer,Integer> m: map.entrySet()) {
             if (m.getValue()==1)
                 return -1;
+
+            int curr=m.getValue();
+            int rem=curr%3;
+            if (rem==0) {
+                ops+=(curr/3);
+                continue;
+            }
+            else if (rem==1) {
+                int temp=curr/3-1;
+                ops+=temp;
+                curr=curr-3*temp;
+            }
+            else {
+                int temp=curr/3;
+                ops+=temp;
+                curr=curr-3*temp;
+            }
             
-            if (m.getValue()%3==0)
-                ans+=(m.getValue()/3);
-            else
-                ans+=(m.getValue()/3+1);
+            ops+=(curr/2);
         }
-        
-        return ans;
-            
+
+        return ops;
     }
 }
