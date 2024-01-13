618208fe439aa08a8282e97e46cd522d06fa8ee4
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2024-01-13 14:11:29 +0530

    Time: 6 ms (99.74%) | Memory: 45.2 MB (28.67%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -3,16 +3,16 @@ class Solution {
         int n=s.length();
         int res=0;
         int[] cntS=new int[26];
-        int[] cntT=new int[26];
 
         for (char ch: s.toCharArray())
             cntS[ch-'a']++;
 
         for (char ch: t.toCharArray())
-            cntT[ch-'a']++;
+            cntS[ch-'a']--;
 
         for (int i=0;i<26;i++)
-            res+=Math.max(0,cntS[i]-cntT[i]);
+            if (cntS[i]>0)
+                res+=cntS[i];
 
         return res;
     }
