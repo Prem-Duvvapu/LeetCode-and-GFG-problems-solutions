ad5da673df04175bc4233d134a62d748193324b3
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2024-01-18 23:45:46 +0530

    Time: 0 ms (100.00%) | Memory: 39.9 MB (52.67%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,14 +1,22 @@
-//dp - tabulation
-
 class Solution {
     public int climbStairs(int n) {
-        int[] dp=new int[n+1];
-        dp[0]=1;
-        dp[1]=1;
-        
-        for (int i=2;i<=n;i++)
-            dp[i]=dp[i-1]+dp[i-2];
+        int[] dp=new int[n];
+        Arrays.fill(dp,-1);
+        return solve(n-1,dp);
+    }
 
-        return dp[n];
+    private int solve(int n,int[] dp) {
+        if (n<0)
+            return 1;
+
+        if (dp[n]!=-1)
+            return dp[n];
+
+        int oneStep=solve(n-1,dp);
+        int twoStep=0;
+        if (n>=1)
+            twoStep=solve(n-2,dp);
+
+        return dp[n]=oneStep+twoStep;
     }
 }
