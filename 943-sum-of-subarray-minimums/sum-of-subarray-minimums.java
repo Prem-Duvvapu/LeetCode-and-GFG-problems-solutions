20267b5fdc071c3a1fef9984b91968c5790b54ca
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2024-01-20 16:47:29 +0530

    Time: 66 ms (26.87%) | Memory: 52.8 MB (5.69%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -3,26 +3,39 @@ class Solution {
         long mod=(long)1e9+7;
         int n=arr.length;
         long res=0L;
+        int[] left=new int[n];
+        int[] right=new int[n];
+        Stack<Integer> stack=new Stack<>();
+
+        Arrays.fill(left,-1);
+        Arrays.fill(right,n);
+
+        //before smaller element
+        for (int i=0;i<n;i++) {
+            while (!stack.isEmpty() && arr[stack.peek()]>=arr[i])
+                stack.pop();
+
+            if (!stack.isEmpty())
+                left[i]=stack.peek();
+
+            stack.push(i);
+        }
+
+        stack.clear();
+
+        //next smaller element
+        for (int i=n-1;i>=0;i--) {
+            while (!stack.isEmpty() && arr[stack.peek()]>arr[i])
+                stack.pop();
+
+            if (!stack.isEmpty())
+                right[i]=stack.peek();
+
+            stack.push(i);
+        }
 
         for (int i=0;i<n;i++) {
-            int left=i;
-            int right=i;
-
-            while (left-1>=0) {
-                if (arr[left-1]>arr[i])
-                    left--;
-                else
-                    break;
-            }
-
-            while (right+1<n) {
-                if (arr[right+1]>=arr[i])
-                    right++;
-                else
-                    break;
-            }
-
-            long curr=(long)(i-left+1)*(right-i+1)*arr[i];
+            long curr=(long)(i-left[i])*(right[i]-i)*arr[i];
             res=(res+curr)%mod;
         }
 
