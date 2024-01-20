f3b19143585452831115e8c4e24f02ca816290ad
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2024-01-20 16:24:26 +0530

    Time: 772 ms (5.09%) | Memory: 49.8 MB (36.06%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        long mod=(long)1e9+7;
+        int n=arr.length;
+        long res=0L;
+
+        for (int i=0;i<n;i++) {
+            int left=i;
+            int right=i;
+
+            while (left-1>=0) {
+                if (arr[left-1]>arr[i])
+                    left--;
+                else
+                    break;
+            }
+
+            while (right+1<n) {
+                if (arr[right+1]>=arr[i])
+                    right++;
+                else
+                    break;
+            }
+
+            long curr=(long)(i-left+1)*(long)(right-i+1)*(long)arr[i];
+            res=(res+curr)%mod;
+        }
+
+        return (int)res;
+    }
+}
