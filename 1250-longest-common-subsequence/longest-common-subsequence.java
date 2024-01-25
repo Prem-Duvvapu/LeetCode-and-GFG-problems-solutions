9dc50f58d5ba848f72c64479cd567a48838ae753
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2024-01-25 09:25:40 +0530

    Time: 24 ms (42.72%) | Memory: 50.4 MB (59.10%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,31 +1,26 @@
-//Memoization
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int n=text1.length();
         int m=text2.length();
         int[][] dp=new int[n][m];
-
-        for (int i=0;i<n;i++)
-            for (int j=0;j<m;j++)
-                dp[i][j]=-1;
-
-        return solve(n-1,m-1,text1,text2,dp);
+        for (int[] a: dp)
+            Arrays.fill(a,-1);
+        return solve(text1,text2,n-1,m-1,dp);
     }
 
-    private int solve(int i,int j,String s1,String s2,int[][] dp)
-    {
-        if (i<0 || j<0)
+    private int solve(String s1,String s2,int n,int m,int[][] dp) {
+        if (n<0 || m<0)
             return 0;
 
-        if (dp[i][j]!=-1)
-            return dp[i][j];
+        if (dp[n][m]!=-1)
+            return dp[n][m];
 
-        if (s1.charAt(i)==s2.charAt(j))
-            return dp[i][j]=1+solve(i-1,j-1,s1,s2,dp);
+        if (s1.charAt(n)==s2.charAt(m))
+            return dp[n][m]=1+solve(s1,s2,n-1,m-1,dp);
 
-        int move_i=solve(i-1,j,s1,s2,dp);
-        int move_j=solve(i,j-1,s1,s2,dp);
+        int x=solve(s1,s2,n-1,m,dp);
+        int y=solve(s1,s2,n,m-1,dp);
 
-        return dp[i][j]=Math.max(move_i,move_j);
+        return dp[n][m]=Math.max(x,y);
     }
 }
