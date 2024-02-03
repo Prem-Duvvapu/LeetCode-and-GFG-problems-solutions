4af91cfb05023b652e94566752c7ed03a4991e48
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2024-02-03 23:35:37 +0530

    Time: 7 ms (58.48%) | Memory: 42 MB (47.96%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -1,25 +1,31 @@
-//Tabulation
+//Memoization
 class Solution {
     public int maxSumAfterPartitioning(int[] arr, int k) {
         int n=arr.length;
-        int[] dp=new int[n+1];
-        
-        for (int pos=n-1;pos>=0;pos--)
+        int[] dp=new int[n];
+        Arrays.fill(dp,-1);
+        return solve(0,arr,k,n,dp);
+    }
+
+    private int solve(int pos,int[] arr,int k,int n,int[] dp)
+    {
+        if (pos==n)
+            return 0;
+
+        if (dp[pos]!=-1)
+            return dp[pos];
+
+        int maxSum=Integer.MIN_VALUE;
+        int maxVal=Integer.MIN_VALUE;
+
+        for (int i=pos;i<Math.min(pos+k,n);i++)
         {
-            int maxSum=Integer.MIN_VALUE;
-            int maxVal=Integer.MIN_VALUE;
-
-            for (int i=pos;i<Math.min(pos+k,n);i++)
-            {
-                maxVal=Math.max(maxVal,arr[i]);
-                int len=i-pos+1;
-                int currSum=len*maxVal+dp[i+1];
-                maxSum=Math.max(maxSum,currSum);
-            }
-
-            dp[pos]=maxSum;
+            maxVal=Math.max(maxVal,arr[i]);
+            int len=i-pos+1;
+            int currSum=len*maxVal+solve(i+1,arr,k,n,dp);
+            maxSum=Math.max(maxSum,currSum);
         }
 
-        return dp[0];
+        return dp[pos]=maxSum;
     }
 }
