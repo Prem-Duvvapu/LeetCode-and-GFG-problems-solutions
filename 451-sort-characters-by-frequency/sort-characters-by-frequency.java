20940e85da584f380d88dbfe09f190d53041b893
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2024-02-07 23:16:54 +0530

    Time: 16 ms (56.80%) | Memory: 45.9 MB (24.30%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,24 +1,18 @@
 class Solution {
     public String frequencySort(String s) {
-        int n=s.length();
-        int[][] a=new int[256][2];
+        char[] arr=s.toCharArray();
+        Map<Character,Integer> freq=new HashMap<>();
         StringBuilder res=new StringBuilder();
 
-        for (int i=0;i<256;i++)
-            a[i][0]=i;
+        for (char ch: arr)
+            freq.put(ch,freq.getOrDefault(ch,0)+1);
 
-        for (char ch: s.toCharArray())
-            a[(int)ch][1]++;
-        
-        Arrays.sort(a,(x,y)->(y[1]-x[1]));
+        PriorityQueue<Map.Entry<Character,Integer>> pq=new PriorityQueue<>((x,y)->(y.getValue()-x.getValue()));
+        pq.addAll(freq.entrySet());
 
-        for (int i=0;i<256;i++)
-        {
-            if (a[i][1]==0)
-                break;
-
-            for (int j=0;j<a[i][1];j++)
-                res.append((char)a[i][0]);
+        while (!pq.isEmpty()) {
+            Map.Entry<Character,Integer> entry=pq.poll();
+            res.append(String.valueOf(entry.getKey()).repeat(entry.getValue()));
         }
 
         return res.toString();
