46884d9986a0eff4a8fcb737c486ec114295f93b
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2024-02-11 19:39:31 +0530

    Time: 26 ms (58.94%) | Memory: 45.6 MB (73.29%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -1,56 +1,42 @@
-//Space Optimization(3D -> 2D)
-
 class Solution {
     public int cherryPickup(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-        int[][] next=new int[n][n];
+        int[][][] dp=new int[m][n][n];
 
-        for (int j1=0;j1<n;j1++)
-        {
-            for (int j2=0;j2<n;j2++)
-            {
-                if (j1==j2)
-                    next[j1][j2]=grid[m-1][j1];
+        for (int i=0;i<m;i++)
+            for (int j=0;j<n;j++)
+                for (int k=0;k<n;k++)
+                    dp[i][j][k]=-1;
+
+        return solve(0,0,n-1,grid,m,n,dp);
+    }
+
+    private int solve(int r,int c1,int c2,int[][] grid,int m,int n,int[][][] dp) {
+        if (c1<0 || c2<0 || c1==n || c2==n)
+            return -1;
+
+        if (r==m-1) {
+            if (c1==c2)
+                return grid[r][c1];
+            else
+                return grid[r][c1]+grid[r][c2];
+        }
+
+        if (dp[r][c1][c2]!=-1)
+            return dp[r][c1][c2];
+
+        int maxVal=0;
+
+        for (int i=-1;i<=1;i++) {
+            for (int j=-1;j<=1;j++) {
+                if (c1==c2)
+                    maxVal=Math.max(maxVal,grid[r][c1]+solve(r+1,c1+i,c2+j,grid,m,n,dp));
                 else
-                    next[j1][j2]=grid[m-1][j1]+grid[m-1][j2];
+                    maxVal=Math.max(maxVal,grid[r][c1]+grid[r][c2]+solve(r+1,c1+i,c2+j,grid,m,n,dp));
             }
         }
 
-        for (int i=m-2;i>=0;i--)
-        {
-            int[][] curr=new int[n][n];
-            for (int j1=0;j1<n;j1++)
-            {
-                for (int j2=0;j2<n;j2++)
-                {
-                    int maxi=(int)(-1e8);
-                    for (int dj1=-1;dj1<=1;dj1++)
-                    {
-                        for (int dj2=-1;dj2<=1;dj2++)
-                        {
-                            int value=0;
-                            if (j1==j2)
-                                value=grid[i][j1];
-                            else
-                                value=grid[i][j1]+grid[i][j2];
-
-                            if (j1+dj1>=0 && j1+dj1<n && j2+dj2>=0 && j2+dj2<n)
-                                value+=next[j1+dj1][j2+dj2];
-                            else
-                                value+=(int)(-1e8);
-                            maxi=Math.max(maxi,value);
-                        }
-                    }
-                    curr[j1][j2]=maxi;
-                }
-            }
-
-            for (int x=0;x<n;x++)
-                for (int y=0;y<n;y++)
-                    next[x][y]=curr[x][y];
-        }
-
-        return next[0][n-1];
+        return dp[r][c1][c2]=maxVal;
     }
 }
