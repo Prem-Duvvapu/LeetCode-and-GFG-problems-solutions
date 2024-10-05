89c56fb5b9f12c7a30a8392c750d978085080816
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2024-10-05 12:45:40 +0530

    Time: 141 ms (21.43%) | Memory: 211.4 MB (92.86%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.rb
@@ -0,0 +1,36 @@
+# @param {String} s1
+# @param {String} s2
+# @return {Boolean}
+def check_inclusion(s1, s2)
+    left = 0
+    right = 0
+    map = {}
+    curr_res = {}
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    alphabet.each_char do |ch|
+        map[ch] = 0
+        curr_res[ch] = 0
+    end
+
+    s1.each_char do |ch|
+        map[ch] += 1
+    end
+
+    while right < s2.length
+        if right >= s1.length
+            if s2[left] != s2[right]
+                curr_res[s2[left]] -= 1
+                curr_res[s2[right]] += 1
+            end
+            left += 1
+        else
+            curr_res[s2[right]] += 1
+        end
+
+        right += 1
+        return true if curr_res == map
+    end
+
+    return false
+end
