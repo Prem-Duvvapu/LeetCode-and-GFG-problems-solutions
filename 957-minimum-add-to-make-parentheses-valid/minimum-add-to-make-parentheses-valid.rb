173805b3c4eff807d41e1b5a26dcaf382a0a73ab
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2024-10-09 19:53:32 +0530

    Time: 77 ms (8.33%) | Memory: 211.4 MB (83.33%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.rb
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.rb
@@ -4,14 +4,10 @@ def min_add_to_make_valid(s)
     stack = []
 
     s.each_char do |ch|
-        if ch == '('
-            stack.push(ch)
+        if ch == ')' && stack.last == '('
+            stack.pop
         else
-            if stack.last == '('
-                stack.pop
-            else
-                stack.push(ch)
-            end
+            stack.push(ch)
         end
     end
 
