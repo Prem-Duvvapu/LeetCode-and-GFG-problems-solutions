519ef13345bc0896e48bef58212a1601e37258e8
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2024-10-11 00:35:49 +0530

    Time: 139 ms (50.00%) | Memory: 216.9 MB (50.00%) - LeetSync

---
+++ 1002-maximum-width-ramp/maximum-width-ramp.rb
@@ -0,0 +1,21 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def max_width_ramp(nums)
+    stack = [0]
+    max_width = 0
+
+    nums.each_with_index do |num, index|
+        if num < nums[stack.last]
+            stack << index
+        end
+    end
+
+    (nums.length - 1).downto(0) do |index|
+        while ((stack.any?) && (nums[index] >= nums[stack.last]))
+            max_width = [max_width, index - stack.last].max
+            stack.pop
+        end
+    end
+
+    max_width
+end
