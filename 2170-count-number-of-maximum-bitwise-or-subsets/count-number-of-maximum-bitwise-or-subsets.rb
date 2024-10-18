ece043e133f7b599eeb5bfb2047f1d6670b9bf05
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2024-10-18 14:48:33 +0530

    Time: 207 ms (100.00%) | Memory: 224.3 MB (50.00%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.rb
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.rb
@@ -1,15 +1,18 @@
 # @param {Integer[]} nums
 # @return {Integer}
 $max_val = 0
-puts "maxval #{$max_val}"
+$list = []
+
 def count_max_or_subsets(nums)
     $max_val = 0
+    $list = []
     solve(nums,0,0)
-    return count(nums,0,0)
+    return $list.count($max_val)
 end
 
 def solve(nums, curr, index)
     if index >= nums.length
+        $list << curr
         $max_val = [$max_val, curr].max
         return
     end
@@ -20,23 +23,4 @@ def solve(nums, curr, index)
     #pick
     curr |= nums[index]
     solve(nums,curr,index+1)
-end
-
-def count(nums, curr, index)
-    if index >= nums.length
-        if curr == $max_val
-            return 1
-        else
-            return 0
-        end
-    end
-
-    #notpick
-    not_pick = count(nums,curr,index+1)
-
-    #pick
-    curr |= nums[index]
-    pick = count(nums,curr,index+1)
-
-    return not_pick + pick
 end
