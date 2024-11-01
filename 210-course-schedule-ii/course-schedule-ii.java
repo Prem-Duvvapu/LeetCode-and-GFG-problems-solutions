cd919363a3e1775d4e5df6a1093b10bcb3f5c8eb
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2024-11-01 22:29:38 +0530

    Time: 5 ms (77.97%) | Memory: 45.7 MB (60.57%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,42 +1,37 @@
-//Topological sort
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int v=numCourses;
-        int n=prerequisites.length;
-        List<Integer>[] graph=new ArrayList[v];
-        for (int i=0;i<v;i++)
-            graph[i]=new ArrayList<>();
+        int n = prerequisites.length;
+        int[] indegree = new int[numCourses];
+        List<List<Integer>> adj = new ArrayList<>();
+        Queue<Integer> q = new ArrayDeque<>();
+        int[] res=new int[numCourses];
+        int pos = 0;
 
-        for (int[] i: prerequisites)
-            graph[i[1]].add(i[0]);
+        for (int i=0; i<numCourses; i++)
+            adj.add(new ArrayList<>());
 
-        //topo sort
-        int[] indegree=new int[v];
-        Queue<Integer> q=new LinkedList<>();
-        int[] res=new int[v];
-        int pos=0;
+        for (int i=0; i<n; i++) {
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+            indegree[prerequisites[i][0]]++;
+        }
 
-        for (int i=0;i<v;i++)
-            for (int node: graph[i])
-                indegree[node]++;
-
-        for (int i=0;i<v;i++)
+        for (int i=0; i<numCourses; i++)
             if (indegree[i]==0)
                 q.add(i);
 
         while (!q.isEmpty()) {
-            int curr=q.poll();
-            res[pos++]=curr;
+            int curr = q.poll();
+            res[pos++] = curr;
 
-            for (int neighbour: graph[curr]) {
+            for (int neighbour: adj.get(curr)) {
                 indegree[neighbour]--;
                 if (indegree[neighbour]==0)
                     q.add(neighbour);
             }
         }
 
-        for (int i=0;i<v;i++)
-            if (indegree[i]>0)
+        for (int i=0; i<numCourses; i++)
+            if (indegree[i]!=0)
                 return new int[]{};
 
         return res;
