0de667e39f63dd022226874af4b34ab41e01580e
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2024-11-24 01:01:57 +0530

    Time: 32 ms (7.56%) | Memory: 45.9 MB (7.32%) - LeetSync

---
+++ 126-word-ladder-ii/word-ladder-ii.java
@@ -0,0 +1,84 @@
+class Solution {
+    Queue<String> q;
+    Map<String,Integer> map;
+    List<List<String>> res;
+    int level;
+    int minSteps;
+    
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        q = new ArrayDeque<>();
+        map = new HashMap<>();
+        Set<String> set = new HashSet<>();
+        res = new ArrayList<>();
+
+        for (String word: wordList)
+            set.add(word);
+
+
+        q.add(beginWord);
+        level = 1;
+        minSteps = 0;
+        map.put(beginWord,level);
+        set.remove(beginWord);
+
+        while (!q.isEmpty()) {
+            String curr = q.poll();
+            if (curr.equals(endWord)) {
+                minSteps = map.get(curr);
+                break;
+            }
+            
+            level = map.get(curr)+1;
+            for (int i=0;i<curr.length();i++) {
+                StringBuilder temp = new StringBuilder(curr);
+                for (char ch='a';ch<='z';ch++) {
+                    temp.setCharAt(i,ch);
+                    if (set.contains(temp.toString())) {
+                        q.add(temp.toString());
+                        map.put(temp.toString(),level);
+                        minSteps = level;
+                        set.remove(temp.toString());
+                    }
+                }
+            }
+        }
+
+        System.out.println(map);
+        System.out.println(level);
+        System.out.println(minSteps);
+
+        List<String> temp = new ArrayList<>();
+        temp.add(endWord);
+        solve(endWord, temp);
+
+        System.out.println("final res"+res);
+        return res;
+    }
+
+    public void solve(String word, List<String> currList) {
+        if (currList.size()==minSteps) {
+            List<String> newList = new ArrayList<>(currList);
+            System.out.println(currList);
+            Collections.reverse(newList);
+            System.out.println(newList);
+            res.add(newList);
+            System.out.println(res);
+            return;
+        }
+        System.out.println("word="+word+", currList"+currList);
+        String curr = word;
+        for (int i=0;i<curr.length();i++) {
+            StringBuilder temp = new StringBuilder(curr);
+            for (char ch='a';ch<='z';ch++) {
+                temp.setCharAt(i,ch);        
+                if (map.containsKey(temp.toString()) && map.get(temp.toString()) < map.getOrDefault(curr,-1)) {
+                    System.out.println(curr+" "+temp.toString());
+                    currList.add(temp.toString());
+                    solve(temp.toString(),currList);
+                    currList.remove(currList.size()-1);
+                    System.out.println("currList after removal"+currList);
+                }
+            }
+        }
+    }
+}
