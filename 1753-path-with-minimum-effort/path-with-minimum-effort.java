bcae2723eb0708b124cb8ca5b3e5d3044bbaa98d
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2024-12-01 22:37:02 +0530

    Time: 512 ms (5.02%) | Memory: 46.3 MB (5.31%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -1,53 +1,56 @@
 class Node {
-    int minEffort;
+    int effort;
     int row;
-    int col;
+    int column;
 
-    Node(int _minEffort, int _row, int _col) {
-        minEffort = _minEffort;
-        row = _row;
-        col = _col;
+    Node(int effort,int row,int column) {
+        this.effort = effort;
+        this.row = row;
+        this.column = column;
     }
 }
 
 class Solution {
     public int minimumEffortPath(int[][] heights) {
-        int rows = heights.length;
-        int columns = heights[0].length;
-        int[][] minEffortToNode = new int[rows][columns];
-        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.minEffort, b.minEffort));
+        int noOfRows=heights.length;
+        int noOfColumns=heights[0].length;
+        int[][] minEffort=new int[noOfRows][noOfColumns];
 
-        for (int i=0; i<rows; i++)
-            Arrays.fill(minEffortToNode[i], (int)1e7);
+        for (int i=0;i<noOfRows;i++)
+            for (int j=0;j<noOfColumns;j++)
+                minEffort[i][j]=(int)1e7;
 
-        minEffortToNode[0][0] = 0;
+        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> Integer.compare(a.effort, b.effort));
         pq.add(new Node(0,0,0));
+        minEffort[0][0]=0;
+
+        int[] deltaRow = {0,-1,0,1};
+        int[] deltaColumn = {-1,0,1,0};
 
         while (!pq.isEmpty()) {
-            Node curr = pq.poll();
+            Node curr=pq.poll();
 
             int currRow = curr.row;
-            int currCol = curr.col;
+            int currColumn = curr.column;
 
-            int[] deltaRow = {-1, 0, 1, 0};
-            int[] deltaCol = {0, 1, 0, -1};
-
-            for (int i=0; i<4; i++) {
+            for (int i=0;i<4;i++) {
                 int newRow = currRow + deltaRow[i];
-                int newCol = currCol + deltaCol[i];
+                int newColumn = currColumn + deltaColumn[i];
+                
 
-                if (newRow>=0 && newRow<rows && newCol>=0 && newCol<columns) {
-                    int currDiff = Math.abs(heights[currRow][currCol] - heights[newRow][newCol]);
-                    if (minEffortToNode[newRow][newCol] > Math.max(currDiff, minEffortToNode[currRow][currCol])) {
-                        minEffortToNode[newRow][newCol] = Math.max(currDiff, minEffortToNode[currRow][currCol]);
-                        pq.add(new Node(currDiff, newRow, newCol));
-                        System.out.println(minEffortToNode[newRow][newCol]+ " "+newRow + " "+ newCol);
+                if (newRow>=0 && newRow<noOfRows && newColumn>=0 && newColumn<noOfColumns) {
+                    int currDiff = Math.abs(heights[currRow][currColumn]-heights[newRow][newColumn]);
+                    int currMinEffort = Math.max(minEffort[currRow][currColumn],currDiff);
 
+                    if (minEffort[newRow][newColumn] > currMinEffort) {
+                        minEffort[newRow][newColumn]=currMinEffort;
+                        System.out.println(currDiff + " "+newRow+ " "+newColumn);
+                        pq.add(new Node(currDiff,newRow,newColumn));
                     }
                 }
             }
         }
 
-        return minEffortToNode[rows-1][columns-1];
+        return minEffort[noOfRows-1][noOfColumns-1];
     }
 }
