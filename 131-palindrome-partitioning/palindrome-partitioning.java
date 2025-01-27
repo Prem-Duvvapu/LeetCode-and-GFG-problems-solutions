ba9c69ab909bbae97c023cd5dad51fce33e5bbfa
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-01-27 13:09:20 +0530

    Time: 8 ms (57.71%) | Memory: 57 MB (43.49%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,38 +1,33 @@
 class Solution {
-    private boolean isPalindrome(String s,int left,int right)
-    {
-        while (left<right)
-        {
-            if (s.charAt(left)!=s.charAt(right))
-                return false;
-            left++;
-            right--;
-        }
-        return true;
-    }
-
-    private void solve(int pos,String s,List<String> curr,List<List<String>> res)
-    {
-        if (pos==s.length())
-        {
-            res.add(new ArrayList<>(curr));
-            return;
-        }
-
-        for (int i=pos;i<s.length();i++)
-        {
-            if (isPalindrome(s,pos,i))
-            {
-                curr.add(s.substring(pos,i+1));
-                solve(i+1,s,curr,res);
-                curr.remove(curr.size()-1);
-            }
-        }
-    }
-
     public List<List<String>> partition(String s) {
         List<List<String>> res=new ArrayList<>();
         solve(0,s,new ArrayList<>(),res);
         return res;
+    }
+
+    private void solve(int pos,String s,List<String> currList,List<List<String>> res) {
+        if (pos==s.length()) {
+            res.add(new ArrayList<>(currList));
+            return;
+        }
+
+        for (int i=pos;i<s.length();i++) {
+            if (isPalindrome(pos,i,s)) {
+                currList.add(s.substring(pos,i+1));
+                solve(i+1,s,currList,res);
+                currList.remove(currList.size()-1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(int i,int j,String s) {
+        while (i<j) {
+            if (s.charAt(i)!=s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+
+        return true;
     }
 }
