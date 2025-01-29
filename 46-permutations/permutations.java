1074bd2ff5dbd52c62eabfd1011f4fae2eb91339
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-01-29 18:01:09 +0530

    Time: 1 ms (93.15%) | Memory: 44.3 MB (96.27%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -3,11 +3,11 @@ class Solution {
         int n=nums.length;
         List<List<Integer>> res=new ArrayList<>();
         boolean[] visited=new boolean[n];
-        solve(0,nums,visited,res,new ArrayList<>());
+        solve(nums,visited,res,new ArrayList<>());
         return res;
     }
 
-    private void solve(int pos,int[] nums,boolean[] visited,List<List<Integer>> res,List<Integer> currList) {
+    private void solve(int[] nums,boolean[] visited,List<List<Integer>> res,List<Integer> currList) {
         if (currList.size()==nums.length) {
             res.add(new ArrayList<>(currList));
             return;
@@ -17,7 +17,7 @@ class Solution {
             if (!visited[i]) {
                 visited[i]=true;
                 currList.add(nums[i]);
-                solve(i+1,nums,visited,res,currList);
+                solve(nums,visited,res,currList);
                 visited[i]=false;
                 currList.remove(currList.size()-1);
             }
