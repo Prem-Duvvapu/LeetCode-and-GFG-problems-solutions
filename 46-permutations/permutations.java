d2d33c81d103665a4ca7cc3ab7ecf4edb1789177
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-01-29 17:58:34 +0530

    Time: 1 ms (93.15%) | Memory: 44.4 MB (81.08%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,32 +1,26 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
+        int n=nums.length;
         List<List<Integer>> res=new ArrayList<>();
-        solve(0,new ArrayList<>(),nums,res);
+        boolean[] visited=new boolean[n];
+        solve(0,nums,visited,res,new ArrayList<>());
         return res;
     }
 
-    private void solve(int pos,List<Integer> currList,int[] nums,List<List<Integer>> res)
-    {
-        if (currList.size()==nums.length)
-        {
+    private void solve(int pos,int[] nums,boolean[] visited,List<List<Integer>> res,List<Integer> currList) {
+        if (currList.size()==nums.length) {
             res.add(new ArrayList<>(currList));
             return;
         }
 
-        for (int i=pos;i<nums.length;i++)
-        {
-            swap(nums,pos,i);
-            currList.add(nums[pos]);
-            solve(pos+1,currList,nums,res);
-            currList.remove(currList.size()-1);
-            swap(nums,pos,i);
+        for (int i=0;i<nums.length;i++) {
+            if (!visited[i]) {
+                visited[i]=true;
+                currList.add(nums[i]);
+                solve(i+1,nums,visited,res,currList);
+                visited[i]=false;
+                currList.remove(currList.size()-1);
+            }
         }
-    }
-
-    private void swap(int[] nums,int i,int j)
-    {
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
     }
 }
