252255737a8eae45c5fab8680c8eb7f7c7bb425a
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-01-30 12:55:34 +0530

    Time: 2 ms (48.27%) | Memory: 41.1 MB (63.31%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.java
+++ 60-permutation-sequence/permutation-sequence.java
@@ -5,25 +5,24 @@ class Solution {
         List<Character> l=new ArrayList<>();
         for (int i=0;i<n;i++)
             l.add((char)((i+1)+'0'));
-
-        solve(n,k-1,l);
+        int fact=factorial(n);
+        solve(n,k-1,l,fact);
         return res;
     }
 
-    private void solve(int n,int k,List<Character> l) {
+    private void solve(int n,int k,List<Character> l,int fact) {
         if (n==1) {
             res+=l.get(0);
             return;
         }
-
-        int fact=factorial(n);
+        
         int x=fact/n; //each subset size
         int y=k/x; //index pos
         int z=k%x; //next k
 
         res+=l.get(y);
         l.remove(y);
-        solve(n-1,z,l);
+        solve(n-1,z,l,fact/n);
     }
 
     private int factorial(int n) {
