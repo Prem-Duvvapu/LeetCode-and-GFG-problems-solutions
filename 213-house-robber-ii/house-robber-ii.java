c1f0ac1c64c32a154b32bb807cec762dd81134ec
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-01-30 15:21:29 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (49.47%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -5,31 +5,21 @@ class Solution {
             return nums[0];
         int[] dp=new int[n];
         Arrays.fill(dp,-1);
-        int last=solve(n-1,nums,n-1,dp);
+        int fromFirst=solve(0,n-2,nums,dp);
         Arrays.fill(dp,-1);
-        int first=solve(n-2,nums,n-2,dp);
-
-        return Math.max(first,last);
+        int fromSecond=solve(1,n-1,nums,dp);
+        return Math.max(fromFirst,fromSecond);
     }
 
-    private int solve(int pos,int[] nums,int n,int[] dp)
-    {
-        if (pos==0)
-        {
-            if (n==nums.length-1)
-                return 0;
-            else
-                return nums[0];
-        }
-
-        if (pos<0)
+    private int solve(int pos,int last,int[] nums,int[] dp) {
+        if (pos>last)
             return 0;
 
-        if (dp[pos]!=-1)    
+        if (dp[pos]!=-1)
             return dp[pos];
 
-        int pick=nums[pos]+solve(pos-2,nums,n,dp);
-        int notPick=solve(pos-1,nums,n,dp);
+        int pick=nums[pos]+solve(pos+2,last,nums,dp);
+        int notPick=solve(pos+1,last,nums,dp);
 
         return dp[pos]=Math.max(pick,notPick);
     }
