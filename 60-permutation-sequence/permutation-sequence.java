799df102cf792e1bb4506e5b0f004a65414988dd
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-01-30 12:54:02 +0530

    Time: 2 ms (48.27%) | Memory: 40.9 MB (87.96%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.java
+++ 60-permutation-sequence/permutation-sequence.java
@@ -1,22 +1,35 @@
 class Solution {
+    String res="";
+
     public String getPermutation(int n, int k) {
-        List<Character> list=new ArrayList<>();
-        for (int i=1;i<=n;i++)
-            list.add((char)(i+'0'));
-        int fact=1;
-        for (int i=1;i<n;i++)
-            fact*=i;
-        StringBuilder sb=new StringBuilder();
-        k=k-1;
-        for (int i=n-1;i>0;i--)
-        {
-            int index=k/fact;
-            sb.append(list.get(index));
-            list.remove(index);
-            k=k%fact;
-            fact=fact/i;
+        List<Character> l=new ArrayList<>();
+        for (int i=0;i<n;i++)
+            l.add((char)((i+1)+'0'));
+
+        solve(n,k-1,l);
+        return res;
+    }
+
+    private void solve(int n,int k,List<Character> l) {
+        if (n==1) {
+            res+=l.get(0);
+            return;
         }
-        sb.append(list.get(0));
-        return sb.toString();
+
+        int fact=factorial(n);
+        int x=fact/n; //each subset size
+        int y=k/x; //index pos
+        int z=k%x; //next k
+
+        res+=l.get(y);
+        l.remove(y);
+        solve(n-1,z,l);
+    }
+
+    private int factorial(int n) {
+        if (n==1)
+            return 1;
+
+        return n * factorial(n-1);
     }
 }
