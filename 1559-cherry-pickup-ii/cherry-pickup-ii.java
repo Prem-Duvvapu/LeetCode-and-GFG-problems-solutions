c7c8645d4f531efdc39401e2bd3c718527dabd77
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-01-30 22:52:06 +0530

    Time: 27 ms (46.58%) | Memory: 46.1 MB (50.68%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -1,42 +1,38 @@
 class Solution {
     public int cherryPickup(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        int[][][] dp=new int[m][n][n];
+        int n=grid.length;
+        int m=grid[0].length;
+        int[][][] dp=new int[n][m][m];
 
-        for (int i=0;i<m;i++)
-            for (int j=0;j<n;j++)
-                for (int k=0;k<n;k++)
+        for (int i=0;i<n;i++)
+            for (int j=0;j<m;j++)
+                for (int k=0;k<m;k++)
                     dp[i][j][k]=-1;
 
-        return solve(0,0,n-1,grid,m,n,dp);
+        return solve(0,0,m-1,grid,n,m,dp);
     }
 
-    private int solve(int r,int c1,int c2,int[][] grid,int m,int n,int[][][] dp) {
-        if (c1<0 || c2<0 || c1==n || c2==n)
-            return -1;
+    private int solve(int r,int c1,int c2,int[][] grid,int n,int m,int[][][] dp) {
+        if (c1<0 || c2<0 || c1==m || c2==m)
+            return 0;
 
-        if (r==m-1) {
-            if (c1==c2)
-                return grid[r][c1];
-            else
-                return grid[r][c1]+grid[r][c2];
-        }
+        if (r==n)
+            return 0;
 
         if (dp[r][c1][c2]!=-1)
             return dp[r][c1][c2];
 
-        int maxVal=0;
-
+        int res=0;
         for (int i=-1;i<=1;i++) {
             for (int j=-1;j<=1;j++) {
+                int curr=grid[r][c1]+grid[r][c2];
                 if (c1==c2)
-                    maxVal=Math.max(maxVal,grid[r][c1]+solve(r+1,c1+i,c2+j,grid,m,n,dp));
-                else
-                    maxVal=Math.max(maxVal,grid[r][c1]+grid[r][c2]+solve(r+1,c1+i,c2+j,grid,m,n,dp));
+                    curr=grid[r][c1];
+                int currMax=curr+solve(r+1,c1+i,c2+j,grid,n,m,dp);
+                res=Math.max(res,currMax);
             }
         }
 
-        return dp[r][c1][c2]=maxVal;
+        return dp[r][c1][c2]=res;
     }
 }
