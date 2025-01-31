2d2001f37ab6181cc5a3edd75710d1d4a7a6023a
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-01-31 19:23:18 +0530

    Time: 21 ms (56.71%) | Memory: 51.2 MB (30.50%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -4,23 +4,22 @@ class Solution {
         int m=text2.length();
         int[][] dp=new int[n+1][m+1];
         
+        //base case
+        for (int j=0;j<=m;j++)
+            dp[0][j]=0;
+
         for (int i=0;i<=n;i++)
-            for (int j=0;j<=m;j++)
-                dp[i][j]=-1;
+            dp[i][0]=0;
 
-        return solve(n,m,text1,text2,dp);
-    }
+        for (int i=1;i<=n;i++) {
+            for (int j=1;j<=m;j++) {
+                if (text1.charAt(i-1)==text2.charAt(j-1))
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
 
-    private int solve(int n,int m,String text1,String text2,int[][] dp) {
-        if (n==0 || m==0)
-            return 0;
-
-        if (dp[n][m]!=-1)
-            return dp[n][m];
-
-        if (text1.charAt(n-1)==text2.charAt(m-1))
-            return 1+solve(n-1,m-1,text1,text2,dp);
-
-        return dp[n][m]=Math.max(solve(n-1,m,text1,text2,dp),solve(n,m-1,text1,text2,dp));
+        return dp[n][m];
     }
 }
