62567b1ff469c80a609bef1ea9dae4c7a2c0d7fa
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-01-31 13:27:48 +0530

    Time: 620 ms (40.27%) | Memory: 41.6 MB (58.97%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,28 +1,21 @@
 class Solution {
-    private int solve(int pos,int[] nums,int target,int[][] dp)
-    {
-        if (pos==nums.length)
-        {
-            if (target==0)
-                return 1;
-            else
-                return 0;
-        }
-
-        if (dp[pos][target+1000]!=-1)
-            return dp[pos][target+1000];
-
-        int plus=solve(pos+1,nums,target-nums[pos],dp);
-        int minus=solve(pos+1,nums,target+nums[pos],dp);
-        
-        return dp[pos][target+1000]=plus+minus;
-    }
-
     public int findTargetSumWays(int[] nums, int target) {
         int n=nums.length;
-        int[][] dp=new int[n][3001];
-        for (int[] arr: dp)
-            Arrays.fill(arr,-1);
-        return solve(0,nums,target,dp);
+        return solve(n-1,nums,target);
+    }
+
+    private int solve(int pos,int[] nums,int target) {
+        if (pos<0) {
+            if (target==0)
+                return 1;
+            return 0;
+        }
+
+        int add=nums[pos]+solve(pos-1,nums,target-nums[pos]);
+        int sub=-1*nums[pos]+solve(pos-1,nums,target+nums[pos]);
+
+        int res=add+sub;
+
+        return res;
     }
 }
