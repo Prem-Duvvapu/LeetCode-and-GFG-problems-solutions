1095cd632242306ac676e05d569e2fb2c64e88ff
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-01-31 15:12:16 +0530

    Time: 21 ms (56.71%) | Memory: 51.5 MB (8.05%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -8,19 +8,43 @@ class Solution {
             for (int j=0;j<m;j++)
                 dp[i][j]=-1;
 
-        return solve(n-1,m-1,text1,text2,dp);
+        for (int i=0;i<n;i++)
+            if (text1.charAt(i)==text2.charAt(0))
+                dp[i][0]=1;
+
+        for (int j=1;j<m;j++)
+            if (text1.charAt(0)==text2.charAt(j))
+                dp[0][j]=1;
+
+        for (int i=0;i<n;i++) {
+            for (int j=0;j<m;j++) {
+                if (text1.charAt(i)==text2.charAt(j) && i>0 && j>0)
+                    dp[i][j]=1+dp[i-1][j-1];
+                else {
+                    int l=0;
+                    if (i>0)
+                        l=dp[i-1][j];
+                    int r=0;
+                    if (j>0)
+                        r=dp[i][j-1];
+                    dp[i][j]=Math.max(dp[i][j],Math.max(l,r));
+                }
+            }
+        }
+
+        return dp[n-1][m-1];
     }
 
-    private int solve(int i,int j,String text1,String text2,int[][] dp) {
-        if (i<0 || j<0)
-            return 0;
+    // private int solve(int i,int j,String text1,String text2,int[][] dp) {
+    //     if (i<0 || j<0)
+    //         return 0;
 
-        if (dp[i][j]!=-1)
-            return dp[i][j];
+    //     if (dp[i][j]!=-1)
+    //         return dp[i][j];
 
-        if (text1.charAt(i)==text2.charAt(j))
-            return dp[i][j]=1+solve(i-1,j-1,text1,text2,dp);
+    //     if (text1.charAt(i)==text2.charAt(j))
+    //         return dp[i][j]=1+solve(i-1,j-1,text1,text2,dp);
 
-        return dp[i][j]=Math.max(solve(i-1,j,text1,text2,dp), solve(i,j-1,text1,text2,dp));
-    }
+    //     return dp[i][j]=Math.max(solve(i-1,j,text1,text2,dp), solve(i,j-1,text1,text2,dp));
+    // }
 }
