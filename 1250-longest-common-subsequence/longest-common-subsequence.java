462833d7150516b8209c3042a820b77b62d6df33
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-01-31 19:23:27 +0530

    Time: 20 ms (76.62%) | Memory: 51.4 MB (14.23%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -5,11 +5,11 @@ class Solution {
         int[][] dp=new int[n+1][m+1];
         
         //base case
-        for (int j=0;j<=m;j++)
-            dp[0][j]=0;
+        // for (int j=0;j<=m;j++)
+        //     dp[0][j]=0;
 
-        for (int i=0;i<=n;i++)
-            dp[i][0]=0;
+        // for (int i=0;i<=n;i++)
+        //     dp[i][0]=0;
 
         for (int i=1;i<=n;i++) {
             for (int j=1;j<=m;j++) {
