9652ad7f160d795dde841c26619ed7098f1e5867
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-01-31 19:18:41 +0530

    Time: 26 ms (25.49%) | Memory: 51 MB (55.58%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -2,49 +2,25 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int n=text1.length();
         int m=text2.length();
-        int[][] dp=new int[n][m];
-
-        for (int i=0;i<n;i++)
-            for (int j=0;j<m;j++)
+        int[][] dp=new int[n+1][m+1];
+        
+        for (int i=0;i<=n;i++)
+            for (int j=0;j<=m;j++)
                 dp[i][j]=-1;
 
-        for (int i=0;i<n;i++)
-            if (text1.charAt(i)==text2.charAt(0))
-                dp[i][0]=1;
-
-        for (int j=1;j<m;j++)
-            if (text1.charAt(0)==text2.charAt(j))
-                dp[0][j]=1;
-
-        for (int i=0;i<n;i++) {
-            for (int j=0;j<m;j++) {
-                if (text1.charAt(i)==text2.charAt(j) && i>0 && j>0)
-                    dp[i][j]=1+dp[i-1][j-1];
-                else {
-                    int l=0;
-                    if (i>0)
-                        l=dp[i-1][j];
-                    int r=0;
-                    if (j>0)
-                        r=dp[i][j-1];
-                    dp[i][j]=Math.max(dp[i][j],Math.max(l,r));
-                }
-            }
-        }
-
-        return dp[n-1][m-1];
+        return solve(n,m,text1,text2,dp);
     }
 
-    // private int solve(int i,int j,String text1,String text2,int[][] dp) {
-    //     if (i<0 || j<0)
-    //         return 0;
+    private int solve(int n,int m,String text1,String text2,int[][] dp) {
+        if (n==0 || m==0)
+            return 0;
 
-    //     if (dp[i][j]!=-1)
-    //         return dp[i][j];
+        if (dp[n][m]!=-1)
+            return dp[n][m];
 
-    //     if (text1.charAt(i)==text2.charAt(j))
-    //         return dp[i][j]=1+solve(i-1,j-1,text1,text2,dp);
+        if (text1.charAt(n-1)==text2.charAt(m-1))
+            return 1+solve(n-1,m-1,text1,text2,dp);
 
-    //     return dp[i][j]=Math.max(solve(i-1,j,text1,text2,dp), solve(i,j-1,text1,text2,dp));
-    // }
+        return dp[n][m]=Math.max(solve(n-1,m,text1,text2,dp),solve(n,m-1,text1,text2,dp));
+    }
 }
