54444b873411d9101d86e70ebcb6afff7d4fa135
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-01-31 19:33:46 +0530

    Time: 25 ms (64.81%) | Memory: 45.5 MB (51.98%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,36 +1,27 @@
-//Find string length - longest palindromic subsequence of string
-//To find longest palindromic subsequence of a string, take the string and the reverse of that string and find their longest common subsequence
-//Space Optimization
-
-
 class Solution {
     public int minInsertions(String s) {
-        return (s.length()-longestPalindromicSubsequence(s));
-    }
+        String text1=s;
+        String text2=new StringBuilder(text1).reverse().toString();
+        int n=text1.length();
+        int m=text2.length();
+        int[][] dp=new int[n+1][m+1];
+        
+        //base case
+        // for (int j=0;j<=m;j++)
+        //     dp[0][j]=0;
 
-    private int longestPalindromicSubsequence(String s)
-    {
-        int n=s.length();
-        StringBuilder sb=new StringBuilder(s);
-        String t=sb.reverse().toString();
+        // for (int i=0;i<=n;i++)
+        //     dp[i][0]=0;
 
-        int[] prev=new int[n+1];
-        int[] curr=new int[n+1];
-
-        for (int i=1;i<=n;i++)
-        {
-            for (int j=1;j<=n;j++)
-            {
-                if (s.charAt(i-1)==t.charAt(j-1))
-                    curr[j]=1+prev[j-1];
+        for (int i=1;i<=n;i++) {
+            for (int j=1;j<=m;j++) {
+                if (text1.charAt(i-1)==text2.charAt(j-1))
+                    dp[i][j]=1+dp[i-1][j-1];
                 else
-                    curr[j]=Math.max(prev[j],curr[j-1]);
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
             }
-
-            for (int k=1;k<=n;k++)
-                prev[k]=curr[k];
         }
 
-        return prev[n];
+        return (n-dp[n][m]);
     }
 }
