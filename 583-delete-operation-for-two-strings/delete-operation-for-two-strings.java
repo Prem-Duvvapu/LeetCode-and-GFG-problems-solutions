e56210ae77bdab04a681950eb9f067853c19b10a
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-01-31 19:36:25 +0530

    Time: 6 ms (94.21%) | Memory: 45.2 MB (13.36%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,26 +1,25 @@
 class Solution {
-    public int minDistance(String word1, String word2) {
-        int n=word1.length();
-        int m=word2.length();
-        int lcs=longestCommonSubsequence(word1,word2,n,m);
-        return ((n-lcs)+(m-lcs));
-    }
-
-    private int longestCommonSubsequence(String s,String t,int n,int m)
-    {
+    public int minDistance(String text1, String text2) {
+        int n=text1.length();
+        int m=text2.length();
         int[][] dp=new int[n+1][m+1];
+        
+        //base case
+        // for (int j=0;j<=m;j++)
+        //     dp[0][j]=0;
 
-        for (int i=1;i<=n;i++)
-        {
-            for (int j=1;j<=m;j++)
-            {
-                if (s.charAt(i-1)==t.charAt(j-1))
+        // for (int i=0;i<=n;i++)
+        //     dp[i][0]=0;
+
+        for (int i=1;i<=n;i++) {
+            for (int j=1;j<=m;j++) {
+                if (text1.charAt(i-1)==text2.charAt(j-1))
                     dp[i][j]=1+dp[i-1][j-1];
                 else
                     dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
             }
         }
 
-        return dp[n][m];
+        return (n+m-2*dp[n][m]);
     }
 }
