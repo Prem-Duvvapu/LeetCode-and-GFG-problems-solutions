8e4b0976d02902a16d4b2254ee4fd0ee27283506
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-02-01 13:11:38 +0530

    Time: 23 ms (38.09%) | Memory: 48.5 MB (29.09%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,45 +1,98 @@
-//Tabulation (right shift by 1)
-
 class Solution {
     public boolean isMatch(String s, String p) {
-        int n=p.length();
-        int m=s.length();
-        Boolean[][] dp=new Boolean[n+1][m+1];
+        int n=s.length();
+        int m=p.length();
+        int[][] dp=new int[n][m];
 
-        dp[0][0]=true;
+        for (int i=0;i<n;i++)
+            for (int j=0;j<m;j++)
+                dp[i][j]=-1;
 
-        for (int j=1;j<=m;j++)
-            dp[0][j]=false;
+        return solve(n-1,m-1,s,p,dp);
+    }
 
-        if (n>0)
-        {
-            if (p.charAt(0)=='*')
-                dp[1][0]=true;
-            else
-                dp[1][0]=false;
+    private boolean solve(int i,int j,String s,String p,int[][] dp) {
+        if (i<0 && j<0)
+            return true;
+
+        if (j<0 && i>=0)
+            return false;
+
+        if (i<0 && j>=0) {
+            for (int k=j;k>=0;k--)
+                if (p.charAt(k)!='*')
+                    return false;
+
+            return true;
+        }
+        
+        if (i<0) {
+            if (j<0)
+                return true;
+            
+            if (j==0 && p.charAt(j)=='*')
+                return true;
+            
+            int k=j;
+            for (;k>=0;k--)
+                if (p.charAt(j)!='*')
+                    return false;
+
+            if (k<0)
+                return true;
+
+            return false;
+        }
+            
+        if (j<0)
+            return false;
+
+        if (dp[i][j]!=-1) {
+            if (dp[i][j]==1)
+                return true;
+            return false;
         }
 
-        for (int i=2;i<=n;i++)
-        {
-            if (p.charAt(i-1)=='*')
-                dp[i][0]=dp[i-1][0];
-            else
-                dp[i][0]=false;
-        }
+        boolean res=false;
 
-        for (int i=1;i<=n;i++)
-        {
-            for (int j=1;j<=m;j++)
-            {
-                if (p.charAt(i-1)=='?' || p.charAt(i-1)==s.charAt(j-1))
-                    dp[i][j]=dp[i-1][j-1];
-                else if (p.charAt(i-1)=='*')
-                    dp[i][j]=(dp[i-1][j] || dp[i][j-1]);
+        if (p.charAt(j)!='*' && p.charAt(j)!='?') {
+            if (p.charAt(j)==s.charAt(i)) {
+                res=solve(i-1,j-1,s,p,dp);
+                if (res)
+                    dp[i][j]=1;
                 else
-                    dp[i][j]=false;
+                    dp[i][j]=0;
+                return res;
+            } else {
+                res=false;
+                if (res)
+                    dp[i][j]=1;
+                else
+                    dp[i][j]=0;
+                return res;
             }
         }
 
-        return dp[n][m];
+        if (p.charAt(j)=='?') {
+            res=solve(i-1,j-1,s,p,dp);
+            if (res)
+                dp[i][j]=1;
+            else
+                dp[i][j]=0;
+            return res;
+        }
+
+        //*
+        boolean x=solve(i,j-1,s,p,dp);
+        boolean y=solve(i-1,j-1,s,p,dp);
+        boolean z=solve(i-1,j,s,p,dp);
+
+        res=(x || y|| z);
+        if (res)
+            dp[i][j]=1;
+        else
+            dp[i][j]=0;
+
+        return res;
     }
 }
