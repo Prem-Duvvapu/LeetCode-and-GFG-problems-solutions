477c2596c38dcf499df0c9c83372e25cca2b93f7
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-02-02 14:00:48 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (17.83%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,36 +1,16 @@
+//There should be only one peak value when there are distinct numbers in the array
+//TC: O(n)
+//SC: O(1)
+
 class Solution {
     public boolean check(int[] nums) {
         int n=nums.length;
-        int[] temp=new int[n];
+        int peakCount=0;
+
         for (int i=0;i<n;i++)
-            temp[i]=nums[i];
+            if (nums[i]>nums[(i+1)%n])
+                peakCount++;
 
-        Arrays.sort(temp);
-        // System.out.println(Arrays.toString(nums));
-        // System.out.println(Arrays.toString(temp));
-        for (int i=0;i<n;i++) {
-            for (int j=0;j<n-1;j++) {
-                int y=temp[j];
-                temp[j]=temp[(j+1)%n];
-                temp[(j+1)%n]=y;
-            }
-            // System.out.println(Arrays.toString(temp));
-            
-            boolean flag=true;
-            for (int k=0;k<n;k++) {
-                if (temp[k]!=nums[k]) {
-                    flag=false;
-                    break;
-                }
-            }
-
-            // System.out.println(Arrays.toString(temp));
-            // System.out.println(Arrays.toString(nums));
-
-            if (flag)
-                return true;
-        }
-
-        return false;
+        return (peakCount<=1);
     }
 }
