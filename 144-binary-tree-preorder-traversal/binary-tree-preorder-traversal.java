31d6632e368f1dd0a188b78056116ab1ddd35e73
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-02-03 21:14:52 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (63.41%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -14,38 +14,25 @@
  * }
  */
 
- //Morris Traversal
- //Root Left Right
+ //preorder - node,left,right
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res=new ArrayList<>();
-        TreeNode curr=root;
+        if (root==null)
+            return res;
 
-        while (curr!=null)
-        {
-            if (curr.left==null)
-            {
-                res.add(curr.val);
-                curr=curr.right;
-            }
-            else
-            {
-                TreeNode prev=curr.left;
-                while (prev.right!=null && prev.right!=curr)
-                    prev=prev.right;
+        Stack<TreeNode> stack=new Stack<>();
+        stack.add(root);
 
-                if (prev.right==null)
-                {
-                    prev.right=curr;
-                    res.add(curr.val);
-                    curr=curr.left;
-                }
-                else
-                {
-                    prev.right=null;
-                    curr=curr.right;
-                }
-            }
+        while (!stack.isEmpty()) {
+            TreeNode curr=stack.pop();
+            res.add(curr.val);
+
+            if (curr.right!=null)
+                stack.add(curr.right);
+
+            if (curr.left!=null)
+                stack.add(curr.left);
         }
 
         return res;
