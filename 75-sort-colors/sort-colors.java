26d6ab30ba4cf4d65b0304eddb03acb9b9b775be
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-02-08 00:08:57 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (45.84%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,10 +1,24 @@
-//Brute force
-//T.C.: O(nlogn)
-//S.C.: O(n) (extra space used during sorting)
+//Better
+//T.C.: O(2*n)
+//S.C.: O(1)
 
 class Solution {
     public void sortColors(int[] nums) {
         int n=nums.length;
-        Arrays.sort(nums);
+        int[] cnt=new int[3];
+
+        for (int i=0;i<n;i++)
+            cnt[nums[i]]++;
+
+        int pos=0;
+        
+        while (cnt[0]-- > 0)
+            nums[pos++]=0;
+
+        while (cnt[1]-- > 0)
+            nums[pos++]=1;
+
+        while (cnt[2]-- > 0)
+            nums[pos++]=2;
     }
 }
