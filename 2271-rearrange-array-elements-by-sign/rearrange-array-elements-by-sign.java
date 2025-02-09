4402a8550cbb2895c87a7514b07354cb72261558
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-02-09 23:06:11 +0530

    Time: 7 ms (23.52%) | Memory: 76.8 MB (82.61%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -2,21 +2,31 @@ class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n=nums.length;
         int[] res=new int[n];
-        int pos=0;
+        int positiveIndex=0;
+        int negativeIndex=0;
 
-        for (int i=0;i<n;i++) {
-            if (nums[i]>0) {
-                res[pos]=nums[i];
-                pos+=2;
-            }
+        while (nums[positiveIndex]<0)
+            positiveIndex++;
+
+        while (nums[negativeIndex]>0)
+            negativeIndex++;
+
+        int pos=0;
+        while (pos<n && positiveIndex<n) {
+            res[pos]=nums[positiveIndex];
+            positiveIndex++;
+            pos+=2;
+            while (positiveIndex<n && nums[positiveIndex]<0)
+                positiveIndex++;
         }
 
         pos=1;
-        for (int i=0;i<n;i++) {
-            if (nums[i]<0) {
-                res[pos]=nums[i];
-                pos+=2;
-            }
+        while (pos<n && negativeIndex<n) {
+            res[pos]=nums[negativeIndex];
+            negativeIndex++;
+            pos+=2;
+            while (negativeIndex<n && nums[negativeIndex]>0)
+                negativeIndex++;
         }
 
         return res;
