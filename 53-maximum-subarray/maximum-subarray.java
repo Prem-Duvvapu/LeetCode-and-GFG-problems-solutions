1a6c316d4f9aa34777f971e6ce2e043873d9b2f5
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-02-09 22:37:18 +0530

    Time: 414 ms (3.13%) | Memory: 56.5 MB (96.91%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -4,14 +4,25 @@ class Solution {
         int n=nums.length;
         int maxSum=(int)-1e5;
         int currSum=0;
+        int startIndex=0;
+        int endIndex=-1;
 
         for (int i=0;i<n;i++) {
-            if (currSum<0)
+            if (currSum<0) {
                 currSum=0;
+                startIndex=i;
+            }
 
             currSum+=nums[i];
-            maxSum=Math.max(maxSum,currSum);
+
+            if (currSum>maxSum) {
+                maxSum=currSum;
+                endIndex=i;
+            }
         }
+
+        for (int i=startIndex;i<=endIndex;i++)
+            System.out.print(nums[i]+" ");
 
         return maxSum;
     }
