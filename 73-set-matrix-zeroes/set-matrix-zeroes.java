4f50762260c74c34d1ed09726b2204739fd9f164
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-02-11 13:31:17 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (65.76%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -5,21 +5,49 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m=matrix.length;
         int n=matrix[0].length;
-        int[] rowsMark=new int[m];
-        int[] colsMark=new int[n];
+        boolean firstRowCheck=false;
+        boolean firstColCheck=false;
 
+        //check first row
+        for (int j=0;j<n;j++) {
+            if (matrix[0][j]==0) {
+                firstRowCheck=true;
+                break;
+            }
+        }
+
+        //check first col
         for (int i=0;i<m;i++) {
-            for (int j=0;j<n;j++) {
+            if (matrix[i][0]==0) {
+                firstColCheck=true;
+                break;
+            }
+        }
+
+        for (int i=1;i<m;i++) {
+            for (int j=1;j<n;j++) {
                 if (matrix[i][j]==0) {
-                    rowsMark[i]=1;
-                    colsMark[j]=1;
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
                 }
             }
         }
 
-        for (int i=0;i<m;i++)
-            for (int j=0;j<n;j++)
-                if (rowsMark[i]==1 || colsMark[j]==1)
+        for (int i=1;i<m;i++)
+            for (int j=1;j<n;j++)
+                if (matrix[0][j]==0 || matrix[i][0]==0)
                     matrix[i][j]=0;
+
+        if (firstRowCheck) {
+            for (int j=0;j<n;j++)
+                matrix[0][j]=0;
+        }
+
+        if (firstColCheck) {
+            for (int i=0;i<m;i++)
+                matrix[i][0]=0;
+        }
+
+
     }
 }
