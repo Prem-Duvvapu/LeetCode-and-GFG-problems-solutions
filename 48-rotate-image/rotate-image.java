cecce48d9a417998f1d9f34e58378271b493f43a
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-02-11 16:08:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (75.62%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,17 +1,38 @@
-//Brute
+//Optimal
 //T.C. : O(n*n)
-//S.C. : O(n*n)
+//S.C. : O(1)
+
+/*
+Steps
+1. transpose the matrix
+2. reverse each row after transposing the matrix
+*/
+
 class Solution {
     public void rotate(int[][] matrix) {
         int n=matrix.length;
-        int[][] temp=new int[n][n];
-
-        for (int i=0;i<n;i++)
-            for (int j=0;j<n;j++)
-                temp[j][n-1-i]=matrix[i][j];
             
         for (int i=0;i<n;i++)
-            for (int j=0;j<n;j++)
-                matrix[i][j]=temp[i][j];
+            for (int j=i;j<n;j++)
+                swap(matrix,i,j,j,i);
+
+        for (int i=0;i<n;i++)
+            reverse(matrix,i,0,n-1);
+    }
+
+    public void swap(int[][] matrix,int r1,int c1,int r2,int c2) {
+        int temp=matrix[r1][c1];
+        matrix[r1][c1]=matrix[r2][c2];
+        matrix[r2][c2]=temp;
+    }
+
+    public void reverse(int[][] matrix,int row,int colStart,int colEnd) {
+        while (colStart<colEnd) {
+            int temp=matrix[row][colStart];
+            matrix[row][colStart]=matrix[row][colEnd];
+            matrix[row][colEnd]=temp;
+            colStart++;
+            colEnd--;
+        }
     }
 }
