5a92231ec9822439a0a4bb504e754e7c722e198a
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-02-12 18:03:31 +0530

    Time: 29 ms (73.05%) | Memory: 51.8 MB (40.93%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,51 +1,36 @@
+//optimal
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        int n=nums.length;
+        int k=0;
+        List<List<Integer>> resList=new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>> result=new ArrayList<>();
-        solve(3,0,0,new ArrayList<>(),nums,result);
-        return result;
-    }
 
-    private void solve(int k,int start,int target,List<Integer> currList,int[] nums,List<List<Integer>> result)
-    {
-        //base case
-        if (k==2)
-        {
-            int left=start;
-            int right=nums.length-1;
-
-            while (left<right)
-            {
-                if (nums[left]+nums[right]<target)
-                    left++;
-                else if (nums[left]+nums[right]>target)
-                    right--;
-                else
-                {
-                    currList.add(nums[left]);
-                    currList.add(nums[right]);
-                    result.add(new ArrayList<>(currList));
-                    currList.remove(currList.size()-1);
-                    currList.remove(currList.size()-1);
-                    left++;
-                    right--;
-
-                    while (left<right && nums[left]==nums[left-1])
-                        left++;
-                }
-            }
-
-            return;
-        }
-
-        for (int i=start;i<nums.length;i++)
-        {
-            if (i>start && nums[i]==nums[i-1])
+        for (int i=0;i<n;i++) {
+            int left=i+1;
+            int right=n-1;
+            if (i>0 && nums[i]==nums[i-1])
                 continue;
 
-            currList.add(nums[i]);
-            solve(k-1,i+1,target-nums[i],currList,nums,result);
-            currList.remove(currList.size()-1);
+            while (left<right) {
+                if (nums[left]+nums[right]<k-nums[i])
+                    left++;
+                else if (nums[left]+nums[right]>k-nums[i])
+                    right--;
+                else {
+                    List<Integer> currList=new ArrayList<>(Arrays.asList(nums[i],nums[left],nums[right]));
+                    resList.add(currList);
+                    left++;
+                    right--;
+                    while (left<right && nums[left]==nums[left-1])
+                        left++;
+                    
+                    while (right>left && nums[right]==nums[right+1])
+                        right--;
+                }
+            }
         }
+
+        return resList;
     }
 }
