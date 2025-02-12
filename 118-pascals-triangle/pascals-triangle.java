baf0675ea113ede5dc396d313caf5ff0bec940e2
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-02-12 12:35:30 +0530

    Time: 1 ms (84.16%) | Memory: 41.9 MB (89.40%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,21 +1,30 @@
+//brute
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> res=new ArrayList<>();
-        List<Integer> prev=new ArrayList<>();
-        prev.add(1);
-        res.add(prev);
-        for (int i=1;i<numRows;i++)
-        {
-            List<Integer> curr=new ArrayList<>();
-            curr.add(1);
-            for (int j=1;j<i;j++)
-            {
-                curr.add(prev.get(j-1)+prev.get(j));
-            }
-            curr.add(1);
-            res.add(curr);
-            prev=curr;
+        List<List<Integer>> resList=new ArrayList<>();
+
+        for (int i=0;i<numRows;i++) {
+            List<Integer> currList=new ArrayList<>();
+
+            for (int j=0;j<=i;j++)
+                currList.add(findNcR(i,j));
+
+            resList.add(currList);
         }
+
+        return resList;
+    }
+
+    //O(r)
+    public int findNcR(int n,int r) {
+        int res=1;
+        r=Math.min(r,n-r);
+
+        for (int i=0;i<r;i++) {
+            res=(res*(n-i));
+            res=(res/(i+1));
+        }
+
         return res;
     }
 }
