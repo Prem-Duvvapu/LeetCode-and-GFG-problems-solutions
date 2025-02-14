7b17f3699f936146b9683bf34e5dc2f7e2ca66d4
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-02-14 14:39:07 +0530

    Time: 18 ms (23.31%) | Memory: 68.9 MB (53.70%) - LeetSync

---
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.java
@@ -0,0 +1,37 @@
+class ProductOfNumbers {
+    List<Integer> list;
+
+    public ProductOfNumbers() {
+        list=new ArrayList<>();
+    }
+    
+    public void add(int num) {
+        if (num==0)
+            list=new ArrayList<>();
+        else if (list.size()==0)
+            list.add(num);
+        else
+            list.add(list.get(list.size()-1)*num);
+        // System.out.println(list);
+    }
+    
+    public int getProduct(int k) {
+        if (k>list.size())
+            return 0;
+
+        if (k==list.size())
+            return list.get(list.size()-1);
+
+        int res=list.get(list.size()-1);
+        System.out.println(res);
+        res=res/(list.get(list.size()-k-1));
+        return res;
+    }
+}
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers obj = new ProductOfNumbers();
+ * obj.add(num);
+ * int param_2 = obj.getProduct(k);
+ */
