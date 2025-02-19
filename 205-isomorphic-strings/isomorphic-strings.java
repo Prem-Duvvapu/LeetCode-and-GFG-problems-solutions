df78b4e01503c15d1c8dc44a8bab37162d1d8d9f
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-02-19 13:03:09 +0530

    Time: 5 ms (97.57%) | Memory: 42.5 MB (52.22%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int n=s.length();
+
+        int[] sTot=new int[256];
+        int[] tTos=new int[256];
+
+        Arrays.fill(sTot,-1);
+        Arrays.fill(tTos,-1);
+
+        for (int i=0;i<n;i++) {
+            char sChar=s.charAt(i);
+            char tChar=t.charAt(i);
+
+            //s to t checking
+            if (sTot[sChar]!=-1) {
+                if (sTot[sChar]!=tChar)
+                    return false;
+            } else {
+                sTot[sChar]=tChar;
+            }
+
+            //t to s checking
+            if (tTos[tChar]!=-1) {
+                if (tTos[tChar]!=sChar)
+                    return false;
+            } else {
+                tTos[tChar]=sChar;
+            }
+
+        }
+
+        return true;
+    }
+}
