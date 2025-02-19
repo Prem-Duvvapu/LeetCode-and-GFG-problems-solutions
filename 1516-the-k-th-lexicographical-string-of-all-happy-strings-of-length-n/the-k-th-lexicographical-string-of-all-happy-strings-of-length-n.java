6423b71b60e1ea40e71c7df9e0cce29adc3fdcaa
DUVVAPU PREM SAI GANESH BABU <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-02-19 12:14:03 +0530

    Time: 19 ms (34.62%) | Memory: 45.7 MB (9.11%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,5 +1,7 @@
 class Solution {
     int pos=0;
+    String res="";
+
     public String getHappyString(int n, int k) {
         int total=(1<<(n-1))*3;
         if (k>total)
@@ -7,19 +9,21 @@ class Solution {
 
         String[] arr=new String[total];
         
-        solve("",-1,n,arr,total);
-        return arr[k-1];
+        solve("",-1,n,arr,new int[]{k});
+        return res;
     }
 
-    private void solve(String curr,int last,int n,String[] arr,int total) {
+    private void solve(String curr,int last,int n,String[] arr,int[] k) {
         if (curr.length()==n) {
-            arr[pos++]=new String(curr);
+            k[0]--;
+            if (k[0]==0)
+                res=new String(curr);
             return;
         }
 
         for (int i=0;i<3;i++) {
             if (last!=i)
-                solve(curr+(char)(i+'a'),i,n,arr,total);
+                solve(curr+(char)(i+'a'),i,n,arr,k);
         }
     }
 }
