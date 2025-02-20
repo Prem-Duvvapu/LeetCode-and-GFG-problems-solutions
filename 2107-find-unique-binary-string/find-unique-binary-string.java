c7db85cf2b57e674bf7930a18d2b05fe00ee4f19
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-02-20 15:46:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (36.03%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,39 +1,19 @@
+//Cantor's diagonalization
+//Time: O(n)
+//Space: O(1)
+
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
         int n=nums.length;
-        Set<String> set=new HashSet<>();
-        String[] res=new String[1];
+        StringBuilder res=new StringBuilder();
 
-        for (String s: nums)
-            set.add(s);
-
-        solve(new StringBuilder(),n,res,set);
-
-        return res[0];
-    }
-
-    private boolean solve(StringBuilder curr,int n,String[] res,Set<String> set) {
-        if (curr.length()==n) {
-            String s=new String(curr);
-            
-            if (!set.contains(s)) {
-                res[0]=s;
-                return true;
-            }
-
-            return false;
+        for (int i=0;i<n;i++) {
+            if (nums[i].charAt(i)=='0')
+                res.append('1');
+            else if (nums[i].charAt(i)=='1')
+                res.append('0');
         }
 
-        curr.append('0');
-        if (solve(curr,n,res,set))
-            return true;
-        curr.deleteCharAt(curr.length()-1);
-
-        curr.append('1');
-        if (solve(curr,n,res,set))
-            return true;
-        curr.deleteCharAt(curr.length()-1);
-
-        return false;
+        return res.toString();
     }
 }
