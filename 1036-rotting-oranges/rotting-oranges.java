6bb3f45264e5732f51805b8639e16c9fb4b32dce
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-02-21 16:25:21 +0530

    Time: 2 ms (70.94%) | Memory: 42.5 MB (60.91%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,58 +1,68 @@
 class Pair {
-    int row,col;
-    Pair(int _row,int _col) {
-        row = _row;
-        col = _col;
+    int row;
+    int col;
+
+    Pair(int row,int col) {
+        this.row=row;
+        this.col=col;
     }
 }
 
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int freshCnt = 0;
-        int m = grid.length;
-        int n = grid[0].length;
-        Queue<Pair> q = new LinkedList<>();
-        boolean[][] visited = new boolean[m][n];
-        int minutes = 0;
+        int m=grid.length;
+        int n=grid[0].length;
+        boolean[][] visited=new boolean[m][n];
+        int freshCnt=0;
+        int time=0;
+        Queue<Pair> q=new LinkedList<>();
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1)
+        for (int i=0;i<m;i++) {
+            for (int j=0;j<n;j++) {
+                if (grid[i][j]==1) {
                     freshCnt++;
-                else if (grid[i][j] == 2) {
+                } else if (grid[i][j]==2) {
+                    visited[i][j]=true;
                     q.add(new Pair(i,j));
-                    visited[i][j] = true;
                 }
             }
         }
 
-        if (freshCnt == 0)
-            return 0;
+        if (freshCnt==0)
+            return time;
 
-        int[] drow = {-1, 0, 1, 0};
-        int[] dcol = {0, 1, 0, -1};
-
-        //bfs
+        int[] dRow={-1,0,1,0};
+        int[] dCol={0,1,0,-1};
+        
         while (!q.isEmpty()) {
-            int qlen = q.size();
-            minutes++;
+            int qlen=q.size();
+            boolean flag=false;
 
             while (qlen-- > 0) {
-                Pair curr = q.poll();
+                Pair curr=q.poll();
+                int currRow=curr.row;
+                int currCol=curr.col;
 
-                for (int i = 0; i < 4; i++) {
-                    int nrow = curr.row + drow[i];
-                    int ncol = curr.col + dcol[i];
+                for (int i=0;i<4;i++) {
+                    int newRow=currRow+dRow[i];
+                    int newCol=currCol+dCol[i];
 
-                    if (nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol]==1 && !visited[nrow][ncol]) {
+                    if (newRow>=0 && newRow<m && newCol>=0 && newCol<n && grid[newRow][newCol]==1 && !visited[newRow][newCol]) {
+                        visited[newRow][newCol]=true;
                         freshCnt--;
-                        visited[nrow][ncol] = true;
-                        q.add(new Pair(nrow, ncol));
+                        q.add(new Pair(newRow,newCol));
+                        flag=true;
                     }
                 }
             }
+
+            if (flag)
+                time++;
         }
 
-        return (freshCnt > 0) ? -1 : --minutes;
+        if (freshCnt==0)
+            return time;
+
+        return -1;
     }
 }
