714c9b690dac8b48aa6186d09f22d93d8e860b64
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-02-24 17:32:27 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (59.40%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,66 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int n=s.length();
+        int openCnt=0;
+        int closeCnt=0;
+        int symbolCnt=0;
+
+        for (int i=n-1;i>=0;i--) {
+            char ch=s.charAt(i);
+            if (ch=='(') {
+                openCnt++;
+                if (openCnt>closeCnt+symbolCnt)
+                    return false;
+            } else if (ch==')') {
+                closeCnt++;
+            } else {
+                symbolCnt++;
+            }
+        }
+
+        openCnt=0;
+        closeCnt=0;
+        symbolCnt=0;
+
+        for (int i=0;i<n;i++) {
+            char ch=s.charAt(i);
+            if (ch==')') {
+                closeCnt++;
+                if (closeCnt>openCnt+symbolCnt)
+                    return false;
+            } else if (ch=='(') {
+                openCnt++;
+            } else {
+                symbolCnt++;
+            }
+        }
+
+        openCnt=0;
+        closeCnt=0;
+        symbolCnt=0;
+
+        for (int i=0;i<n;i++) {
+            char ch=s.charAt(i);
+            if (ch=='(') {
+                openCnt++;
+            } else if (ch==')') {
+                closeCnt++;
+                if (closeCnt>openCnt+symbolCnt)
+                    return false;
+
+                closeCnt--;
+                if (openCnt>0)
+                    openCnt--;
+                else
+                    symbolCnt--;
+            } else {
+                symbolCnt++;
+            }
+        }
+
+        if (openCnt>symbolCnt)
+            return false;
+
+        return true;
+    }
+}
