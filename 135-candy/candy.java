d342efffe7e87d139af8bf670a80da11dd2b76e4
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-02-26 19:29:23 +0530

    Time: 3 ms (83.63%) | Memory: 45.2 MB (97.74%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -7,29 +7,29 @@ class Solution {
         if (n==1)
             return 1;
 
-        int[] prefix=new int[n];
-        int[] suffix=new int[n];
+        int[] left=new int[n];
+        int[] right=new int[n];
         int res=0;
 
-        prefix[0]=1;
-        suffix[n-1]=1;
+        left[0]=1;
+        right[n-1]=1;
 
         for (int i=1;i<n;i++) {
             if (ratings[i]>ratings[i-1])
-                prefix[i]=1+prefix[i-1];
+                left[i]=1+left[i-1];
             else
-                prefix[i]=1;
+                left[i]=1;
         }
 
         for (int i=n-2;i>=0;i--) {
             if (ratings[i]>ratings[i+1])
-                suffix[i]=1+suffix[i+1];
+                right[i]=1+right[i+1];
             else
-                suffix[i]=1;
+                right[i]=1;
         }
 
         for (int i=0;i<n;i++)
-            res+=Math.max(prefix[i],suffix[i]);
+            res+=Math.max(left[i],right[i]);
 
         return res;
     }
