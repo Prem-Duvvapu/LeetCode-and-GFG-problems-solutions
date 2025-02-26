58ac81e1010ac62053fa32f3e0bd3d29788f3c0f
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-02-26 19:27:12 +0530

    Time: 3 ms (83.63%) | Memory: 45.4 MB (89.29%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,23 +1,32 @@
 class Solution {
     public int candy(int[] ratings) {
         int n=ratings.length;
-        int[] l2r=new int[n];
-        int[] r2l=new int[n];
+        if (n==1)
+            return 1;
+
+        int[] prefix=new int[n];
+        int[] suffix=new int[n];
         int res=0;
 
-        Arrays.fill(l2r,1);
-        Arrays.fill(r2l,1);
+        prefix[0]=1;
+        suffix[n-1]=1;
 
-        for (int i=1;i<n;i++)
+        for (int i=1;i<n;i++) {
             if (ratings[i]>ratings[i-1])
-                l2r[i]=l2r[i-1]+1;
+                prefix[i]=1+prefix[i-1];
+            else
+                prefix[i]=1;
+        }
 
-        for (int i=n-2;i>=0;i--)
+        for (int i=n-2;i>=0;i--) {
             if (ratings[i]>ratings[i+1])
-                r2l[i]=r2l[i+1]+1;
+                suffix[i]=1+suffix[i+1];
+            else
+                suffix[i]=1;
+        }
 
         for (int i=0;i<n;i++)
-            res+=Math.max(l2r[i],r2l[i]);
+            res+=Math.max(prefix[i],suffix[i]);
 
         return res;
     }
