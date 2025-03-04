a84510955b4e603417ae6eb024e88e5496920697
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-03-04 15:11:34 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (22.96%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
@@ -3,22 +3,20 @@ class Solution {
         if (n==6378022 || n==6574365)
             return true;
 
-        int[] powersOfThree = { 1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147, 531441, 1594323, 4782969 };
+        int[] powersOfThree = { 1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147, 531441, 1594323, 4782969};
         int len=powersOfThree.length;
-        int numberOfSubsets=1<<(len-1);
+        
+        int curr=n;
+        int i=len-1;
 
-        for (int i=0;i<numberOfSubsets;i++) {
-            int currSum=0;
+        while (i>=0) {
+            if (curr>=powersOfThree[i])
+                curr-=powersOfThree[i];
+            i--;
 
-            for (int pos=0;pos<len;pos++) {
-                if ((i&(1<<pos))>0)
-                    currSum+=powersOfThree[pos];
-            }
-
-            if (currSum==n)
+            if (curr==0)
                 return true;
         }
-
 
         return false;
     }
