9807e359545dd6a6ca6dc982fdaeabbfeb75710c
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-03-05 10:16:47 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (37.30%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
@@ -1,14 +1,20 @@
-//brute
+//optimized
+
+/*
+1st term: 1
+2nd term: 1+4 = (1+4*1)
+3rd term: 1+4+8 = (1+4*1+4*2)
+.
+.
+.
+.
+nth term: 1+4*1+4*2+.....+4*(n-1) = 1+4*(1+2+3+...+n-1)
+*/
+
+
 class Solution {
     public long coloredCells(int n) {
-        long res=1;
-        long prev=0;
-
-        for (int i=2;i<=n;i++) {
-            prev=prev+4;
-            res+=prev;
-        }
-
+        long res=1+2*((long)n*(n-1));
         return res;
     }
 }
