5ecb9946176e7eb91c2ea7f633e957a86006ab31
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-03-10 00:08:48 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (67.57%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,3 +1,4 @@
+//First approach(optimized for pop() and peek())
 class MyQueue {
     Stack<Integer> s1;
     Stack<Integer> s2;
@@ -8,25 +9,25 @@ class MyQueue {
     }
     
     public void push(int x) {
-        while (!s1.isEmpty())
-            s2.push(s1.pop());
-
-        s1.push(x);
-
         while (!s2.isEmpty())
             s1.push(s2.pop());
+            
+        s1.push(x);
+
+        while (!s1.isEmpty())
+            s2.push(s1.pop());
     }
     
     public int pop() {
-        return s1.pop();
+        return s2.pop();
     }
     
     public int peek() {
-        return s1.peek();
+        return s2.peek();
     }
     
     public boolean empty() {
-        return s1.isEmpty();
+        return s2.isEmpty();
     }
 }
 
