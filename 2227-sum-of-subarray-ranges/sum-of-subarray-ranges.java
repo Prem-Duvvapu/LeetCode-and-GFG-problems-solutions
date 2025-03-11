0cc8e5ef3856ca1d52629d2a45f27a6561d7dbc3
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-03-11 22:15:13 +0530

    Time: 42 ms (24.11%) | Memory: 44.4 MB (66.34%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,23 +1,96 @@
-//brute
-//Time: O(n^2)
-//Space: O(1)
 class Solution {
     public long subArrayRanges(int[] nums) {
         int n=nums.length;
-        long sum=0;
+        long res=0L;
+
+        int[] pse=prevSmallerElement(nums,n);
+        int[] nse=nextSmallerElement(nums,n);
+        int[] pge=prevGreaterElement(nums,n);
+        int[] nge=nextGreaterElement(nums,n);
 
         for (int i=0;i<n;i++) {
-            int minVal=Integer.MAX_VALUE;
-            int maxVal=Integer.MIN_VALUE;
-            
-            for (int j=i;j<n;j++) {
-                minVal=Math.min(minVal,nums[j]);
-                maxVal=Math.max(maxVal,nums[j]);
-
-                sum+=(maxVal-minVal);
-            }
+            long maxValCnt=(i-pge[i])*(nge[i]-i);
+            long minValCnt=(i-pse[i])*(nse[i]-i);
+            long currVal=maxValCnt*(long)nums[i]+minValCnt*-1*(long)nums[i];
+            res+=currVal;
         }
-        
-        return sum;
+
+        return res;
+    }
+
+    public int[] prevSmallerElement(int[] nums,int n) {
+        int[] res=new int[n];
+        Stack<Integer> stack=new Stack<>();
+
+        for (int i=0;i<n;i++) {
+            while (!stack.isEmpty() && nums[stack.peek()]>nums[i])
+                stack.pop();
+
+            if (stack.isEmpty())
+                res[i]=-1;
+            else
+                res[i]=stack.peek();
+
+            stack.push(i);
+        }
+
+        return res;
+    }
+
+    public int[] nextSmallerElement(int[] nums,int n) {
+        int[] res=new int[n];
+        Stack<Integer> stack=new Stack<>();
+
+        for (int i=n-1;i>=0;i--) {
+            while (!stack.isEmpty() && nums[stack.peek()]>=nums[i])
+                stack.pop();
+
+            if (stack.isEmpty())
+                res[i]=n;
+            else
+                res[i]=stack.peek();
+
+            stack.push(i);
+        }
+
+        return res;
+    }
+
+    public int[] prevGreaterElement(int[] nums,int n) {
+        int[] res=new int[n];
+        Stack<Integer> stack=new Stack<>();
+
+        for (int i=0;i<n;i++) {
+            while (!stack.isEmpty() && nums[stack.peek()]<nums[i])
+                stack.pop();
+
+            if (stack.isEmpty())
+                res[i]=-1;
+            else
+                res[i]=stack.peek();
+
+            stack.push(i);
+        }
+
+        return res;
+    }
+
+    public int[] nextGreaterElement(int[] nums,int n) {
+        int[] res=new int[n];
+        Stack<Integer> stack=new Stack<>();
+
+        for (int i=n-1;i>=0;i--) {
+            while (!stack.isEmpty() && nums[stack.peek()]<=nums[i])
+                stack.pop();
+
+            if (stack.isEmpty())
+                res[i]=n;
+            else
+                res[i]=stack.peek();
+
+            stack.push(i);
+        }
+
+        return res;
     }
 }
