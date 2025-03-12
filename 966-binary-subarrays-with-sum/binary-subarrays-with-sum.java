592c817679bc0da3282b1874745809e737265c3e
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-03-12 23:29:38 +0530

    Time: 1 ms (100.00%) | Memory: 47.8 MB (53.63%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,37 +1,37 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int n=nums.length;
-        Map<Integer,Integer> map=new HashMap<>();
-        map.put(0,1);
-        int res=0;
-        int currSum=0;
+        int a=solve(nums,goal);
+        int b=solve(nums,goal-1);
 
-        for (int i=0;i<n;i++) {
-            currSum+=nums[i];
-            int diff=currSum-goal;
-            if (map.containsKey(diff))
-                res+=map.get(diff);
-            map.put(currSum,map.getOrDefault(currSum,0)+1);
+        return (a-b);
+    }
+
+    public int solve(int[] nums,int k) {
+        if (k<0)
+            return 0;
+
+        int n=nums.length;
+        int left=0;
+        int right=0;
+        int currSum=0;
+        int cnt=0;
+
+        while (right<n) {
+            currSum+=nums[right];
+
+            //greater than k
+            while (left<=right && currSum>k) {
+                currSum-=nums[left];
+                left++;
+            }
+
+            //less than or equal to k
+            int currCnt=(right-left)+1;
+            cnt+=currCnt;
+
+            right++;
         }
 
-        return res;
+        return cnt;
     }
 }
-
-/*
-brute - time: O(n^2), space: O(1)
-better - prefix sum time: O(n), space: O(n)
-
-map={0,2},{1,2},{2,2}
-ans=2+2+2
-nums = [0, 1, 0, 1, 0, 1], goal = 2
-sum=    0. 1. 1. 2  2. 3
-
-map={0,1},{1,2},{2,2}
-res=0+1+1+2
- nums = [1,0,1,0,1], goal = 2
-sum =    1 1.2 2 3
-
-{0,1}
-[0,1,1,1,1,1]
-*/
