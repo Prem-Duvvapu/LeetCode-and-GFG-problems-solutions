e66890114b95b8719c3bdb84a04217d80d279afe
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-03-13 00:47:45 +0530

    Time: 28 ms (90.74%) | Memory: 59.7 MB (40.08%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,22 +1,34 @@
+//Consider a double ended stack where you can perform push,pop,peek at front and back(deque in java)
+//and it should be a decreasing monotonic stack
+
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n=nums.length;
         int[] res=new int[n-k+1];
         Deque<Integer> q=new ArrayDeque<>();
+        int right=0;
 
-        for (int i=0;i<n;i++)
-        {
-            while (!q.isEmpty() && q.peekFirst()<(i-k+1))
-                q.pollFirst();
-
-            while (!q.isEmpty() && nums[q.peekLast()]<nums[i])
-                q.pollLast();
-
-            q.offerLast(i);
-
-            if ((i-k+1)>=0)
-                res[i-k+1]=nums[q.peekFirst()];
+        while (right<k) {
+            while (!q.isEmpty() && nums[q.peekLast()]<=nums[right])
+                q.removeLast();
+            q.addLast(right);
+            right++;
         }
+
+        while (right<n) {
+            // System.out.println(right-k+" "+q);
+            res[right-k]=nums[q.peekFirst()];
+            while (!q.isEmpty() && right-q.peekFirst()>=k)
+                q.removeFirst();
+
+            while (!q.isEmpty() && nums[q.peekLast()]<=nums[right])
+                q.removeLast();
+
+            q.add(right);
+            right++;
+        }
+
+        res[right-k]=nums[q.peekFirst()];
 
         return res;
     }
