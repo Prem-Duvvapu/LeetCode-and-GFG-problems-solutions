28073cb48ae2a90a6a26b430ab9ef6d33794acb7
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-03-12 22:37:22 +0530

    Time: 7 ms (1.67%) | Memory: 45.6 MB (8.64%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,16 +1,74 @@
 class Solution {
     public int maximumCount(int[] nums) {
         int n=nums.length;
-        int pos=0;
-        int neg=0;
+        int lb=lowerBound(nums,0);
+        int ub=upperBound(nums,0);
+        System.out.println(lb+" "+ub);
+        int negativeCnt=lb;
+        int positiveCnt=n-ub;
 
-        for (int i=0;i<n;i++) {
-            if (nums[i]>0)
-                pos++;
-            else if (nums[i]<0)
-                neg++;
+        return Math.max(negativeCnt,positiveCnt);
+    }
+
+    public int lowerBound(int[] nums,int findVal) {
+        int n=nums.length;
+        int low=0;
+        int high=n-1;
+        int lowerBoundIndex=n;
+
+        while (low<=high) {
+            int mid=low+(high-low)/2;
+
+            if (nums[mid]>=findVal) {
+                lowerBoundIndex=mid;
+                high=mid-1;
+            } else {
+                low=mid+1;
+            }
         }
 
-        return Math.max(pos,neg);
+        return lowerBoundIndex;
+    }
+
+    public int upperBound(int[] nums,int findVal) {
+        int n=nums.length;
+        int low=0;
+        int high=n-1;
+        int upperBoundIndex=n;
+
+        while (low<=high) {
+            int mid=low+(high-low)/2;
+
+            if (nums[mid]>findVal) {
+                upperBoundIndex=mid;
+                high=mid-1;
+            } else {
+                low=mid+1;
+            }
+        }
+
+        return upperBoundIndex;
     }
 }
+
+/*
+findVal=0
+index=6
+nums = [-2, -1, -1, 0, 0, 0, 1, 2, 3]
+        0.   1.  2. 3. 4. 5. 6. 7. 8
+
+
+lowerBound=3
+upperBound=6;
+
+nums = [-2, -1, -1, 0, 1, 2, 3]
+
+nums = [-2,-1,-1,1,2,3]
+
+
+searchValue=0
+
+lowerBound - smallest Index which has value greater than or equal to SearchValue
+
+upperBound- smallest Index which has value greater than SearchValue
+*/
