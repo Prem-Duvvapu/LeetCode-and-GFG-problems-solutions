83783fb4ef8caebdd880caf73e97d9dd46cd4082
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-03-12 18:03:32 +0530

    Time: 122 ms (7.16%) | Memory: 61.6 MB (5.07%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n=heights.length;
+        int[] pse=new int[n];
+        int[] nse=new int[n];
+        int maxArea=0;
+
+        prevSmallerElement(heights,n,pse);
+        nextSmallerElement(heights,n,nse);
+
+        // System.out.println(Arrays.toString(pse)+" "+Arrays.toString(nse));
+
+        for (int i=0;i<n;i++) {
+            int height=heights[i];
+            int width=(i-pse[i])+(nse[i]-i-1);
+
+            int currArea=width*height;
+            maxArea=Math.max(maxArea,currArea);
+        }
+
+        return maxArea;
+    }
+
+    public void prevSmallerElement(int[] arr,int n,int[] res) {
+        Stack<Integer> stack=new Stack<>();
+
+        for (int i=0;i<n;i++) {
+            while (!stack.isEmpty() && arr[stack.peek()]>=arr[i])
+                stack.pop();
+
+            if (stack.isEmpty())
+                res[i]=-1;
+            else
+                res[i]=stack.peek();
+
+            stack.push(i);
+        }
+    }
+
+    public void nextSmallerElement(int[] arr,int n,int[] res) {
+        Stack<Integer> stack=new Stack<>();
+
+        for (int i=n-1;i>=0;i--) {
+            while (!stack.isEmpty() && arr[stack.peek()]>arr[i])
+                stack.pop();
+
+            if (stack.isEmpty())
+                res[i]=n;
+            else
+                res[i]=stack.peek();
+
+            stack.push(i);
+        }
+    }
+
+}
