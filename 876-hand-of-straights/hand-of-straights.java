2af7d5ea416a649a1f96db42263c72976937dbda
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-03-15 05:20:13 +0530

    Time: 341 ms (5.06%) | Memory: 46 MB (29.51%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        int n=hand.length;
+
+        if (n<groupSize || n%groupSize!=0)
+            return false;
+
+        int numOfGroups=n/groupSize;
+        Stack<Integer>[] groups=new Stack[numOfGroups];
+        int currIndex=0;
+
+        Arrays.sort(hand);
+        System.out.println(Arrays.toString(hand));
+
+        for (int j=0;j<numOfGroups;j++)
+            groups[j]=new Stack<Integer>();
+
+        for (int i=0;i<n;i++) {
+            int j=currIndex;
+            while (j<numOfGroups) {
+                if (groups[j].isEmpty()) {
+                    groups[j].add(hand[i]);
+                    if (groups[j].size()==groupSize)
+                        currIndex=j+1;
+                    break;
+                } else {
+                    if (groups[j].peek()+1==hand[i]) {
+                        groups[j].add(hand[i]);
+
+                        if (groups[j].size()==groupSize)
+                            currIndex=j+1;
+                        break;
+                    }
+                }
+
+                j++;
+            }
+            // System.out.println(i+" "+j);
+
+            if (j==numOfGroups)
+                return false;
+        }
+
+        return true;
+    }
+}
