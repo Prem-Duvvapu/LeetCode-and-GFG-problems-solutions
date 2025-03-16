8fbd2f9a91b35e3b91043644c9422b5e13ab7f8b
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-03-16 13:15:07 +0530

    Time: 56 ms (47.31%) | Memory: 56.4 MB (43.82%) - LeetSync

---
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.java
@@ -0,0 +1,50 @@
+class Solution {
+    public long repairCars(int[] ranks, int cars) {
+        int n=ranks.length;
+        long res=0L;
+        long low=1;
+        long high=1;
+
+        Arrays.sort(ranks);
+
+        high=ranks[n-1];
+
+        high=high*cars*cars;
+        res=high;
+
+        while (low<=high) {
+            long mid=low+(high-low)/2;
+
+            if (isPossible(ranks,cars,mid)) {
+                res=mid;
+                high=mid-1;
+            } else {
+                low=mid+1;
+            }
+        }
+
+        return res;
+    }
+
+    public boolean isPossible(int[] ranks,int cars,long maxTime) {
+        long cnt=0;
+
+        for (int i=0;i<ranks.length;i++) {
+            if (maxTime<ranks[i])
+                continue;
+            long curr=maxTime/ranks[i];
+            curr=(long)Math.sqrt(curr);
+            cnt+=curr;
+        }
+
+        return (cnt>=cars);
+    }
+
+    public long squareRoot(long num) {
+        long res=1L;
+        for (long i=1;i*i<=num;i++) {
+            res=i;
+        }
+        return res;
+    }
+}
