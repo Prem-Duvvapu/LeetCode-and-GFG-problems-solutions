4ad96c7ae165cf1f48e74e7b83645bfa618ccdd8
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-03-18 23:59:36 +0530

    Time: 42 ms (92.80%) | Memory: 113.1 MB (71.47%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,88 +1,93 @@
 class Node {
-    Node prev;
     int key;
     int value;
+    Node prev;
     Node next;
 
-    Node() {
-    }
-    
     Node(int key,int value) {
-        prev=null;
-        next=null;
         this.key=key;
         this.value=value;
+        prev=null;
+        next=null;
+    }
+}
+
+class DLL {
+    Node head;
+    Node tail;
+
+    DLL() {
+        head=new Node(-1,-1);
+        tail=new Node(-1,-1);
+        head.next=tail;
+        tail.prev=head;
+    }
+
+    void insertAfterHead(Node node) {
+        Node afterHeadNode=head.next;
+
+        head.next=node;
+        node.prev=head;
+
+        node.next=afterHeadNode;
+        afterHeadNode.prev=node;
+    }
+
+    void removeNode(Node node) {
+        Node beforeNode=node.prev;
+        Node afterNode=node.next;
+
+        beforeNode.next=afterNode;
+        afterNode.prev=beforeNode;
     }
 }
 
 class LRUCache {
-    Node head;
-    Node tail;
-    Map<Integer,Node> map;
+    Map<Integer,Node> cache;
     int capacity;
+    DLL dll;
+    int currSize;
 
     public LRUCache(int capacity) {
-        head=new Node();
-        tail=new Node();
-        map=new HashMap<>();
         this.capacity=capacity;
-
-        head.next=tail;
-        tail.prev=head;
+        cache=new HashMap<>();
+        dll=new DLL();
+        currSize=0;
     }
     
     public int get(int key) {
-        if (!map.containsKey(key))
-            return -1;
+        if (cache.containsKey(key)) {
+            Node node=cache.get(key);
+            dll.removeNode(node);
+            dll.insertAfterHead(node);
 
-        Node currNode=map.get(key);
-        currNode.prev.next=currNode.next;
-        currNode.next.prev=currNode.prev;
+            return node.value;
+        }
 
-        currNode.next=head.next;
-        head.next.prev=currNode;
-        head.next=currNode;
-        currNode.prev=head;
-
-        return map.get(key).value;
+        return -1;
     }
     
     public void put(int key, int value) {
-        if (map.containsKey(key)) {
-            map.get(key).value=value;
-
-            Node currNode=map.get(key);
-            currNode.prev.next=currNode.next;
-            currNode.next.prev=currNode.prev;
-
-            currNode.next=head.next;
-            head.next.prev=currNode;
-            head.next=currNode;
-            currNode.prev=head;
-
-        } else {
-            Node newNode=new Node(key,value);
-            if (map.size()<capacity) {
-                map.put(key,newNode);
-                
-                newNode.next=head.next;
-                head.next.prev=newNode;
-                head.next=newNode;
-                newNode.prev=head;
-            } else {
-                Node lastNode=tail.prev;
-
-                map.remove(lastNode.key);
-                lastNode.prev.next=tail;
-                tail.prev=lastNode.prev;
-
-                map.put(key,newNode);
-                newNode.next=head.next;
-                head.next.prev=newNode;
-                head.next=newNode;
-                newNode.prev=head;
-            }
+        //updating existing key with new value
+        if (cache.containsKey(key)) {
+            Node node=cache.get(key);
+            node.value=value;
+            dll.removeNode(node);
+            dll.insertAfterHead(node);
+            return;
         }
+
+        if (currSize==capacity) {
+            Node node=dll.tail.prev;
+            cache.remove(node.key);
+            dll.removeNode(node);
+            currSize--;
+        }
+
+        Node newNode=new Node(key,value);
+        cache.put(key,newNode);
+        dll.insertAfterHead(newNode);
+        currSize++;
     }
 }
 
