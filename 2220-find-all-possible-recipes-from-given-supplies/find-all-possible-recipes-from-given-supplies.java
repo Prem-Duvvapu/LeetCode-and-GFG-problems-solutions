2f3227481fcf75506be73caea76b130c0ffc7cfb
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-03-21 22:37:17 +0530

    Time: 32 ms (93.82%) | Memory: 46.3 MB (96.25%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.java
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.java
@@ -1,35 +1,44 @@
-//brute
 class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
         int n=recipes.length;
         List<String> res=new ArrayList<>();
-        boolean[] canCreate=new boolean[n];
-        
         Set<String> suppliesSet=new HashSet<>();
+        Map<String,List<Integer>> map=new HashMap<>();
+        Queue<Integer> q=new LinkedList<>();
+        int[] indegree=new int[n];
+
         for (String supply: supplies)
             suppliesSet.add(supply);
 
         for (int i=0;i<n;i++) {
-            for (int j=0;j<n;j++) {
-                if (!canCreate[j]) {
-                    boolean flag=true;
-                    for (String ingredient: ingredients.get(j)) {
-                        if (!suppliesSet.contains(ingredient)) {
-                            flag=false;
-                            break;
-                        }
-                    }
+            boolean flag=true;
+            for (String ingredient: ingredients.get(i)) {
+                if (!suppliesSet.contains(ingredient)) {
+                    flag=false;
+                    List<Integer> list=map.getOrDefault(ingredient,new ArrayList<>());
+                    list.add(i);
+                    map.put(ingredient,list);
+                    indegree[i]++;
+                }
+            }
 
-                    if (flag) {
-                        canCreate[j]=true;
-                        suppliesSet.add(recipes[j]);
-                    }
+            if (flag)
+                q.add(i);
+        }
+
+        while (!q.isEmpty()) {
+            int curr=q.poll();
+            if (map.containsKey(recipes[curr])) {
+                for (int index: map.get(recipes[curr])) {
+                    indegree[index]--;
+                    if (indegree[index]==0)
+                        q.add(index);
                 }
             }
         }
 
         for (int i=0;i<n;i++)
-            if (canCreate[i])
+            if (indegree[i]==0)
                 res.add(recipes[i]);
 
         return res;
