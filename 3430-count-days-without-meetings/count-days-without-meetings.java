043336aa73e19d288bf499bd7477a470134241da
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-03-24 16:26:03 +0530

    Time: 48 ms (26.94%) | Memory: 91 MB (99.33%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.java
+++ 3430-count-days-without-meetings/count-days-without-meetings.java
@@ -1,19 +1,22 @@
 class Solution {
     public int countDays(int days, int[][] meetings) {
         int n=meetings.length;
+        int[][] sortedMeetings=new int[n][2];
         int res=0;
 
-        Arrays.sort(meetings, (a,b) -> Integer.compare(a[0],b[0]));
+        for (int i=0;i<n;i++) {
+            sortedMeetings[i][0]=meetings[i][0];
+            sortedMeetings[i][1]=meetings[i][1];
+        }
+
+        Arrays.sort(sortedMeetings, (a,b) -> Integer.compare(a[0],b[0]));
         int prevEnd=0;
 
-        // for (int i=0;i<n;i++)
-        //     System.out.println(Arrays.toString(meetings[i]));
-
         for (int i=0;i<n;i++) {
-            if (meetings[i][0]>prevEnd+1)
-                res+=(meetings[i][0]-prevEnd-1);
+            if (sortedMeetings[i][0]>prevEnd+1)
+                res+=(sortedMeetings[i][0]-prevEnd-1);
 
-            prevEnd=Math.max(prevEnd,meetings[i][1]);
+            prevEnd=Math.max(prevEnd,sortedMeetings[i][1]);
         }
 
         res+=(days-prevEnd);
