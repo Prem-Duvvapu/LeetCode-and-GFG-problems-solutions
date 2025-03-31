3e2dcee0dd7527c6e5e4ee1e0dbb7bfe37611a00
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-04-01 00:43:25 +0530

    Time: 591 ms (88.86%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,5 +1,5 @@
 select
-    round(count(distinct player_id)/(select count(distinct player_id) from Activity),2) as fraction
+    round(count(player_id)/(select count(distinct player_id) from Activity),2) as fraction
 from Activity
 where (player_id,date_sub(event_date, interval 1 day))
 in
