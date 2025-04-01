62fc5a249f4f0020b2ce6cd72413ec1009bf1c81
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-04-01 22:13:57 +0530

    Time: 1 ms (47.13%) | Memory: 45.1 MB (48.32%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,35 +1,47 @@
 class Solution {
-    int[] dx={-1,0,1,0};
-    int[] dy={0,1,0,-1};
+    int[] dRow={-1,0,1,0}; //up,right,down,left
+    int[] dCol={0,1,0,-1}; //up,right,down,left
 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int m=image.length;
         int n=image[0].length;
+
+        if (image[sr][sc]==color)
+            return image;
+
         int[][] res=new int[m][n];
-        boolean[][] visited=new boolean[m][n];
 
         for (int i=0;i<m;i++)
             for (int j=0;j<n;j++)
                 res[i][j]=image[i][j];
 
-        dfs(sr,sc,res[sr][sc],color,visited,res,m,n);
+        int initialColor=image[sr][sc];
+
+        dfs(sr,sc,res,initialColor,color,m,n);
 
         return res;
     }
 
-    private void dfs(int r,int c,int prev,int color,boolean[][] visited,int[][] res,int m,int n) {
-        res[r][c]=color;
-        visited[r][c]=true;
+    public void dfs(int r,int c,int[][] res,int initialColor,int finalColor,int m,int n) {
+        res[r][c]=finalColor;
 
-        for (int i=0;i<4;i++)
-        {
-            int newRow=r+dx[i];
-            int newCol=c+dy[i];
-            if (newRow>=0 && newRow<m && newCol>=0 && newCol<n && res[newRow][newCol]==prev && !visited[newRow][newCol]) {
-                res[newRow][newCol]=color;
-                visited[newRow][newCol]=true;
-                dfs(newRow,newCol,prev,color,visited,res,m,n);
-            }
+        for (int i=0;i<4;i++) {
+            int newRow=r+dRow[i];
+            int newCol=c+dCol[i];
+
+            if (newRow>=0 && newRow<m && newCol>=0 && newCol<n && res[newRow][newCol]==initialColor)
+                dfs(newRow,newCol,res,initialColor,finalColor,m,n);
         }
     }
 }
+
+
+/*
+
+1 1 1   1 1 1  1 2 1  2 2 1  2 2 1  2 2 1  2 2 2
+1 1 0 ->1 2 0->1 2 0->1 2 0->2 2 0->2 2 0->2 2 0
+1 0 1   1 0 1  1 0 1  1 0 1  1 0 1  2 0 1  2 0 1
+
+up,down,left,right
+
+*/
