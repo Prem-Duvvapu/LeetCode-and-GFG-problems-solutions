635a00770aea78fbec10bc59b6d3711647e88186
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-04-01 18:38:42 +0530

    Time: 1 ms (88.80%) | Memory: 48.1 MB (37.26%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,64 +1,26 @@
-//Using Disjoint Set
-class DisjointSet {
-    int[] parent;
-    int[] size;
-
-    DisjointSet(int n) {
-        parent=new int[n];
-        size=new int[n];
-
-        for (int i=0;i<n;i++) {
-            parent[i]=i;
-            size[i]=1;
-        }
-    }
-
-    public int ultimateParent(int node) {
-        if (parent[node] == node)
-            return node;
-        
-        return parent[node]=ultimateParent(parent[node]);
-    }
-
-    public void unionBySize(int u,int v) {
-        int uPu = ultimateParent(u);
-        int uPv = ultimateParent(v);
-
-        if (uPu == uPv) {
-            return;
-        }
-
-        if (size[uPu] > size[uPv]) {
-            parent[uPv] = uPu;
-            size[uPu] += size[uPv];
-        } else {
-            parent[uPu] = uPv;
-            size[uPv] += uPu;
-        }
-    }
-
-}
-
-
+//dfs
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        int provinces = 0;
-
-        DisjointSet uds = new DisjointSet(n);
+        int n=isConnected.length;
+        boolean[] visited=new boolean[n];
+        int numOfProvinces=0;
 
         for (int i=0;i<n;i++) {
-            for (int j=0;j<n;j++) {
-                if (isConnected[i][j]==1) {
-                    uds.unionBySize(i,j);
-                }
+            if (!visited[i]) {
+                dfs(i,visited,isConnected,n);
+                numOfProvinces++;
             }
         }
 
-        for (int i=0;i<n;i++)
-            if (uds.ultimateParent(i) == i)
-                provinces++;
+        return numOfProvinces;
+    }
 
-        return provinces;
+    public void dfs(int currNode,boolean[] visited,int[][] isConnected,int n) {
+        visited[currNode]=true;
+
+        for (int j=0;j<n;j++) {
+            if (isConnected[currNode][j]==1 && !visited[j])
+                dfs(j,visited,isConnected,n);
+        }
     }
 }
