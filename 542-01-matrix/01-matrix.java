d0ccec2445a52c719be447e9eecb4f1edda99c90
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-04-03 00:25:15 +0530

    Time: 18 ms (18.12%) | Memory: 47.2 MB (72.34%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,9 +1,10 @@
 class Pair {
-    int row,col;
+    int row;
+    int col;
 
-    Pair(int _row,int _col) {
-        row=_row;
-        col=_col;
+    Pair(int row,int col) {
+        this.row=row;
+        this.col=col;
     }
 }
 
@@ -12,38 +13,59 @@ class Solution {
         int m=mat.length;
         int n=mat[0].length;
         int[][] res=new int[m][n];
-
         Queue<Pair> q=new LinkedList<>();
+
         for (int i=0;i<m;i++) {
             for (int j=0;j<n;j++) {
-                res[i][j]=-1;
                 if (mat[i][j]==0) {
                     q.add(new Pair(i,j));
-                    res[i][j]=0;
+                } else {
+                    res[i][j]=-1;
                 }
             }
         }
 
-        int cnt=1;
-        int[] drow={-1,0,1,0};
-        int[] dcol={0,1,0,-1};
+        bfs(res,mat,q,m,n);
+        
+        return res;
+    }
+
+    public void bfs(int[][] res,int[][] mat,Queue<Pair> q,int m,int n) {
+        int steps=1;
+        int[] dRow={-1,0,1,0};
+        int[] dCol={0,1,0,-1};
 
         while (!q.isEmpty()) {
             int qlen=q.size();
+
             while (qlen-- > 0) {
                 Pair curr=q.poll();
+                int currRow=curr.row;
+                int currCol=curr.col;
+
                 for (int i=0;i<4;i++) {
-                    int nrow=curr.row+drow[i];
-                    int ncol=curr.col+dcol[i];
-                    if (nrow>=0 && nrow<m && ncol>=0 && ncol<n && mat[nrow][ncol]==1 && res[nrow][ncol]==-1) {
-                        res[nrow][ncol]=cnt;
-                        q.add(new Pair(nrow,ncol));
+                    int newRow=currRow+dRow[i];
+                    int newCol=currCol+dCol[i];
+
+                    if (newRow>=0 && newRow<m && newCol>=0 && newCol<n && mat[newRow][newCol]==1 && res[newRow][newCol]==-1) {
+                        res[newRow][newCol]=steps;
+                        q.add(new Pair(newRow,newCol));
                     }
                 }
             }
-            cnt++;
-        }
 
-        return res;
+            steps++;
+        }
     }
 }
+
+/*
+
+0  0  0
+0  1  0
+1  2  1
+
+
+steps=3
+
+*/
