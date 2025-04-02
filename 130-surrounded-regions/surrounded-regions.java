e6c1ed902d175aacd5e4d7311cddf09a2b4b4e7a
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-04-03 00:57:31 +0530

    Time: 3 ms (33.21%) | Memory: 45.3 MB (85.05%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,32 +1,35 @@
 class Solution {
+    int[] dRow={-1,0,1,0};
+    int[] dCol={0,1,0,-1};
+
     public void solve(char[][] board) {
         int m=board.length;
         int n=board[0].length;
         boolean[][] visited=new boolean[m][n];
 
-        for (int i=0;i<m;i++)
-            for (int j=0;j<n;j++)
-                if ((i==0 || i==m-1 || j==0 || j==n-1) && board[i][j]=='O' && !visited[i][j])
+        for (int i=0;i<m;i++) {
+            for (int j=0;j<n;j++) {
+                if ((i==0 || j==0 || i==m-1 || j==n-1) && (board[i][j]=='O'))
                     dfs(i,j,visited,board,m,n);
-        
-        for (int i=0;i<m;i++)
-            for (int j=0;j<n;j++)
+            }
+        }
+
+        for (int i=1;i<m-1;i++)
+            for (int j=1;j<n-1;j++)
                 if (board[i][j]=='O' && !visited[i][j])
                     board[i][j]='X';
     }
 
-    private void dfs(int r,int c,boolean[][] visited,char[][] board,int m,int n) {
+    public void dfs(int r,int c,boolean[][] visited,char[][] board,int m,int n) {
         visited[r][c]=true;
 
-        int[] drow={-1,0,1,0};
-        int[] dcol={0,1,0,-1};
-
         for (int i=0;i<4;i++) {
-            int nrow=r+drow[i];
-            int ncol=c+dcol[i];
+            int newRow=r+dRow[i];
+            int newCol=c+dCol[i];
 
-            if (nrow>=0 && nrow<m && ncol>=0 && ncol<n && board[nrow][ncol]=='O' && !visited[nrow][ncol])
-                dfs(nrow,ncol,visited,board,m,n);
+            if (newRow>=0 && newRow<m && newCol>=0 && newCol<n && board[newRow][newCol]=='O' && !visited[newRow][newCol])
+                dfs(newRow,newCol,visited,board,m,n);
         }
     }
 }
+
