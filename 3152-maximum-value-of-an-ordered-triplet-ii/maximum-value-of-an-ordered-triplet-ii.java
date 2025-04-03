6eb9941f9c633f4ca352403fc0d29ede23d68823
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-04-03 14:28:16 +0530

    Time: 2 ms (97.85%) | Memory: 57.3 MB (81.72%) - LeetSync

--- 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.java
+++ 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.java
@@ -7,9 +7,14 @@ class Solution {
 
         for (int k=2;k<n;k++) {
             long currVal=maxDiff*nums[k];
-            maxTripletVal=Math.max(maxTripletVal,currVal);
 
+            //current value can be in kth postion in the final triplet
+            maxTripletVal=Math.max(maxTripletVal,currVal); 
+
+            //current value can be in jth postion in the final triplet
             maxDiff=Math.max(maxDiff,maxi-nums[k]);
+
+            //current value can be in ith postion in the final triplet
             maxi=Math.max(maxi,nums[k]);
         }
 
