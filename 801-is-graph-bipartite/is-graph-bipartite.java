382e34c7eb7d8ad183ce8faf7c4d47326fa59e9a
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-04-04 20:01:39 +0530

    Time: 1 ms (62.63%) | Memory: 45.5 MB (12.02%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -6,24 +6,26 @@ class Solution {
 
         for (int i=0;i<n;i++)
             if (color[i]==-1)
-                if (!bfs(i,color,graph))
+                if (!bfs(i,color,graph,n))
                     return false;
 
         return true;
     }
 
-    public boolean bfs(int start,int[] color,int[][] graph) {
-        color[start]=0;
+    public boolean bfs(int node,int[] color,int[][] graph,int n) {
+        color[node]=0;
         Queue<Integer> q=new LinkedList<>();
-        q.add(start);
+        q.add(node);
 
         while (!q.isEmpty()) {
-            int curr=q.poll();
-            for (int neighbour: graph[curr]) {
-                if (color[neighbour]==-1) {
-                    color[neighbour]=1-color[curr];
-                    q.add(neighbour);
-                } else if (color[neighbour]==color[curr]) {
+            int currNode=q.poll();
+            int currColor=color[currNode];
+
+            for (int ngbr: graph[currNode]) {
+                if (color[ngbr]==-1) {
+                    color[ngbr]=1-currColor;
+                    q.add(ngbr);
+                } else if (color[ngbr]==currColor) {
                     return false;
                 }
             }
@@ -31,19 +33,32 @@ class Solution {
 
         return true;
     }
-
-    public boolean dfs(int curr,int currColor,int[] color,int[][] graph) {
-        color[curr]=currColor;
-
-        for (int neighbour: graph[curr]) {
-            if (color[neighbour]==-1) {
-                if (!dfs(neighbour,1-currColor,color,graph))
-                    return false;
-            } else if (color[neighbour]==currColor) {
-                return false;
-            }
-        }
-
-        return true;
-    }
 }
+
+
+/*
+
+
+
+0   1
+0 - 1
+|   \
+|    4 0
+|    /
+2 - 3
+1   0
+
+//a linear graph is always bipartitie
+0-1-2-3
+
+//a graph with even length cycle is also bipartite
+
+//a graph with odd length cycle is not bipartite
+
+color = [0,  1, 1, 0, 0]
+          0  1  2  3  4
+
+colors = {0,1}
+
+q=[ 4,3]
+*/
