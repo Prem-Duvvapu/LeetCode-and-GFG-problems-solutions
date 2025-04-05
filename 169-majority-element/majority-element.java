d870890e92ed2bfb92c28883da2596fa4aa5aeef
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-04-05 23:37:53 +0530

    Time: 1 ms (99.88%) | Memory: 53.7 MB (8.79%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,19 +1,16 @@
-//Optimal - Boyer Moore Majority Voting Alogrithm
 class Solution {
     public int majorityElement(int[] nums) {
         int n=nums.length;
         int res=nums[0];
         int cnt=1;
 
-        for (int i=1;i<n;i++)  {
-            if (cnt==0) {
-                res=nums[i];
-                cnt=1;
-            } else if (nums[i]==res) {
+        for (int i=1;i<n;i++) {
+            if (nums[i]==res)
                 cnt++;
-            } else {
+            else if (cnt==0)
+                res=nums[i];
+            else
                 cnt--;
-            }
         }
 
         return res;
