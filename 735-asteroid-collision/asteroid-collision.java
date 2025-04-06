38cd13f862f9707e44b6c615db4b649447499db2
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-04-06 23:43:18 +0530

    Time: 6 ms (67.73%) | Memory: 45.7 MB (11.34%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -2,30 +2,81 @@ class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         int n=asteroids.length;
         Stack<Integer> stack=new Stack<>();
-        int[] res;
 
         for (int i=0;i<n;i++) {
+            //if current is positive
             if (asteroids[i]>0) {
                 stack.push(asteroids[i]);
                 continue;
             }
 
-            while (!stack.isEmpty() && stack.peek()>0 && stack.peek()<Math.abs(asteroids[i]))
-                stack.pop();
-
-            if (!stack.isEmpty() && stack.peek()==Math.abs(asteroids[i])) {
-                stack.pop();
+            //current one is negative
+            //1. stack is empty
+            //2. top is negative
+            //3. top is positive
+            if (stack.isEmpty() || stack.peek()<0) {
+                stack.push(asteroids[i]);
                 continue;
             }
 
-            if (stack.isEmpty() || stack.peek()<0)
+            //top one is positive
+
+            //positive is bigger one
+            if (stack.peek()>Math.abs(asteroids[i]))
+                continue;
+
+            //negative is bigger one
+            while (!stack.isEmpty() && stack.peek()>0 && stack.peek()<Math.abs(asteroids[i]))
+                stack.pop();
+
+            //1. stack is empty
+            //2. top is negative
+            if (stack.isEmpty() || stack.peek()<0) {
                 stack.push(asteroids[i]);
+                continue;
+            }
+
+            //top is positive
+
+            //1. top is equal in size
+            //2. top is bigger in size
+            if (stack.peek()+asteroids[i]==0) {
+                stack.pop();
+                continue;
+            }
         }
-        
-        res=new int[stack.size()];
-        for (int i=res.length-1;i>=0;i--)
-            res[i]=stack.pop();
+
+        int[] res=new int[stack.size()];
+        int pos=res.length-1;
+        while (!stack.isEmpty())
+            res[pos--]=stack.pop();
 
         return res;
     }
 }
+
+
+/*
+
+tc1
+[1,2,3]
+exp: [1,2,3]
+
+tc2
+[-1,-2,-3]
+exp: [-1,-2,-3]
+
+tc3
+[5,-2]
+exp: [5]
+
+tc4
+[-3,5,4,-6]
+exp: [-3,-6]
+
+tc5
+[8,-8]
+exp: []
+
+*/
+
