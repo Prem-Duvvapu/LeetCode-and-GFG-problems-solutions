6884a8bfea6ead345c9d87c0ecc34eabb192e424
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-04-13 23:43:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (99.26%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -2,18 +2,44 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         int n=nums.length;
         List<List<Integer>> res=new ArrayList<>();
-
-        int numOfSubsets=(1<<n);
-
-        for (int i=0;i<numOfSubsets;i++) {
-            List<Integer> subset=new ArrayList<>();
-            for (int pos=0;pos<n;pos++) {
-                if ((i&(1<<pos))>0)
-                    subset.add(nums[pos]);
-            }
-            res.add(subset);
-        }
-
+        solve(0,new ArrayList<>(),res,nums,n);
         return res;
     }
+
+    public void solve(int pos,List<Integer> currList,List<List<Integer>> res,int[] nums,int n) {
+        if (pos==n) {
+            res.add(new ArrayList<>(currList));
+            return;
+        }
+
+        //not pick
+        solve(pos+1,currList,res,nums,n);
+
+        //pick
+        currList.add(nums[pos]);
+        solve(pos+1,currList,res,nums,n);
+        currList.remove(currList.size()-1);
+    }
 }
+
+/*
+1. subarrays - should be consecutive indexes
+[1][2][3][1,2][2,3][1,2,3]
+
+2. subsequences - can be consecutive or increasing order indexes
+[][1][2][3][1,2][1,3][2,3][1,2,3]
+
+3. subsets - any order
+[][1][2][3][1,2][1,3][3,2][3,2,1]
+(1,3) 
+
+
+1. return all subsequences
+2. return subsequences with sum k
+3. return cnt of subsequences with sum k
+
+[1, 2, 3]
+
+pick/not pick
+
+*/
