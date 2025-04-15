2a40eceb6ebdabdd33045db89001f0c8682a6cbb
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-04-15 23:32:58 +0530

    Time: 5 ms (19.62%) | Memory: 43.9 MB (6.06%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,30 +1,110 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates);
+        int n=candidates.length;
         List<List<Integer>> res=new ArrayList<>();
-        solve(0,new ArrayList<>(),target,candidates,res);
+        Arrays.sort(candidates);
+        solve(0,new ArrayList<>(),target,res,candidates,n);
         return res;
     }
 
-    private void solve(int pos,List<Integer> curr,int target,int[] candidates,List<List<Integer>> res)
-    {
-        if (target==0)
-        {
-            res.add(new ArrayList<>(curr));
+    public void solve(int pos,List<Integer> currList,int req,List<List<Integer>> res,int[] candidates,int n) {
+        if (req==0) {
+            res.add(new ArrayList<>(currList));
             return;
         }
 
-        for (int i=pos;i<candidates.length;i++)
-        {
+        if (req<0)
+            return;
+
+        if (pos==n)
+            return;
+
+        for (int i=pos;i<n;i++) {
             if (i>pos && candidates[i]==candidates[i-1])
                 continue;
 
-            if (candidates[i]>target)
-                return;
-
-            curr.add(candidates[i]);
-            solve(i+1,curr,target-candidates[i],candidates,res);
-            curr.remove(curr.size()-1);
+            currList.add(candidates[i]);
+            solve(i+1,currList,req-candidates[i],res,candidates,n);
+            currList.remove(currList.size()-1);
         }
     }
 }
+
+/*
+
+0 th index  1
+1 st index  2, 2, 2, 5 
+2 nd index  2, 2 5
+
+
+solve(1,[1],4,res,...)
+i=1,2,3,4
+[1,2]
+
+solve(2,[1,2],2,res,...)
+
+i=2,3,4
+[1,2,2]
+ 0 1 3
+
+i=3 value=2
+
+i=3
+// solve(3,[1,2,2],0,res,....)
+            0 1 2
+
+
+
+Avoiding same element value at same index in the combination
+
+candidates = [2,5,2,1,2], target = 5
+
+[1, 2, 2, 2, 5]
+ 0  1  2  3  4
+
+
+[1, 2, 2]
+ 0  1  2
+
+[1, 2, 2]
+ 0  1  3
+
+0 index - picked 1
+1 index - not picked
+2 index - curr=2 prev=2 [1,2]
+
+
+
+res=[ [1,2,2] ]
+       0 1 2
+
+req=4  1
+req=2  1+2
+
+req=2 I'm index at 3 if I pick [1,2,2]
+                                0 1 3
+
+
+
+target=3
+[1,2]
+ 0 1
+
+[1,2]
+ 0 2
+
+[1,2]
+ 0 3
+
+combination = [0, 1, 2, 3, .....]
+
+
+2,2,1 -> 1,2,2
+
+2,1,2 -> 1,2,2
+
+2,1,2 -> 1,2,2
+
+
+
+*/
