87a8b79c25b5022f7d84e683a271ae69011e6148
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-04-17 21:12:30 +0530

    Time: 1 ms (93.16%) | Memory: 44.4 MB (81.80%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -2,28 +2,29 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         int n=nums.length;
         List<List<Integer>> res=new ArrayList<>();
-        solve(0,nums,res,new ArrayList<>());
+        solve(0,new ArrayList<>(),res,nums,n);
+
         return res;
     }
 
-    private void solve(int pos,int[] nums,List<List<Integer>> res,List<Integer> currList) {
-        if (pos==nums.length) {
+    public void solve(int pos,List<Integer> currList,List<List<Integer>> res,int[] nums,int n) {
+        if (pos==n) {
             res.add(new ArrayList<>(currList));
             return;
         }
-        
-        for (int i=pos;i<nums.length;i++) {
-            swap(nums,pos,i);
+
+        for (int i=pos;i<n;i++) {
+            swap(nums,i,pos);
             currList.add(nums[pos]);
-            solve(pos+1,nums,res,currList);
+            solve(pos+1,currList,res,nums,n);
             currList.remove(currList.size()-1);
-            swap(nums,pos,i);
+            swap(nums,i,pos);
         }
     }
 
-    private void swap(int[] nums,int i,int j) {
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+    public void swap(int[] arr,int i,int j) {
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
     }
 }
