44c96bae25955d633236f93b1d0bf942cdf1a230
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-04-18 23:39:09 +0530

    Time: 328 ms (43.43%) | Memory: 41 MB (73.31%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,46 +1,46 @@
 class Solution {
-    char[] nums = {'1','2','3','4','5','6','7','8','9'};
     public void solveSudoku(char[][] board) {
-        solve(board,0,0);
+        solve(0,0,board);
     }
 
-    private boolean solve(char[][] board,int r,int c) {
+    public boolean solve(int r,int c,char[][] board) {
+        if (r==8 && c==9)
+            return true;
+
         if (c==9) {
             r+=1;
             c=0;
         }
 
-        for (int i=0;i<9;i++) {
-            for (int j=0;j<9;j++) {
-                if (board[i][j]=='.') {
-                    for (int k=0;k<9;k++) {
-                        if (isValid(board,i,j,nums[k])) {
-                            board[i][j]=nums[k];
-                            if (solve(board,r,c+1))
-                                return true;
-                            board[i][j]='.';
-                        }
-                    }
-                    return false;
+        if (board[r][c]=='.') {
+            for (char ch='1';ch<='9';ch++) {
+                if (isSafe(r,c,ch,board)) {
+                    board[r][c]=ch;
+
+                    if (solve(r,c+1,board))
+                        return true;
+                        
+                    board[r][c]='.';
                 }
             }
+            return false;
         }
-
-        return true;
+        
+        return solve(r,c+1,board);
     }
 
-    private boolean isValid(char[][] board,int r,int c,char ch) {
+    public boolean isSafe(int r,int c,char ch,char[][] board) {
         for (int i=0;i<9;i++) {
             //row check
-            if (board[i][c]==ch)
-                return false;
-
-            //column check
             if (board[r][i]==ch)
                 return false;
 
-            //subbox check
-            int newRow = 3 * (r/3) + i/3;
+            //col check
+            if (board[i][c]==ch)
+                return false;
+
+            //sub-box
+            int newRow = 3 * (r/3) + i/3; 
             int newCol = 3 * (c/3) + i%3;
 
             if (board[newRow][newCol]==ch)
@@ -50,3 +50,52 @@ class Solution {
         return true;
     }
 }
+
+/*
+(0,3) (0,4) (0,5)
+(1,3) (1,4) (1,5)
+(2,3) (2,4) (2,5)
+
+gridNum=1
+
+i
+0 - (0,3)
+1 - (0,4)
+2 - (0,5)
+3 - (1,3)
+.
+.
+.
+8 - (2,5)
+
+r, c, i
+
+r -> 0 to 2
+c -> 3 to 5
+
+r=1 c=5 
+
+newRow=i/3
+newCol=3+i%3
+
+i   r  c
+0   0  3
+1   0  4
+2   0  5
+
+3   1  3
+4   1  4
+5   1  5
+
+6   2  3
+7   2  4
+8   2  5
+
+(r/3)*3+c/3
+
+0 1 2
+3 
+
+
+
+*/
