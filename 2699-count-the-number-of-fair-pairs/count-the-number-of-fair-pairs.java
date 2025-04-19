29d7c72607ae77e48ffbe7f8830bda430ed6d54e
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-04-19 13:20:07 +0530

    Time: 62 ms (53.70%) | Memory: 57.8 MB (13.25%) - LeetSync

---
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
@@ -0,0 +1,52 @@
+class Solution {
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        int n=nums.length;
+        long res=0;
+        Arrays.sort(nums);
+        // System.out.println(Arrays.toString(nums));
+
+        for (int i=0;i<n-1;i++) {
+            int minPossibleJ=-1;
+            int maxPossibleJ=-1;
+
+            int low=i+1;
+            int high=n-1;
+
+            while (low<=high) {
+                int mid=low+(high-low)/2;
+
+                if (lower<=nums[i]+nums[mid] && nums[i]+nums[mid]<=upper) {
+                    minPossibleJ=mid;
+                    high=mid-1;
+                } else if (lower>nums[i]+nums[mid]) {
+                    low=mid+1;
+                } else {
+                    high=mid-1;
+                }
+            }
+
+            if (minPossibleJ==-1)
+                continue;
+
+            low=minPossibleJ;
+            high=n-1;
+            while (low<=high) {
+                int mid=low+(high-low)/2;
+
+                if (lower<=nums[i]+nums[mid] && nums[i]+nums[mid]<=upper) {
+                    maxPossibleJ=mid;
+                    low=mid+1;
+                } else if (lower>nums[i]+nums[mid]) {
+                    low=mid+1;
+                } else {
+                    high=mid-1;
+                }
+            }
+
+            res+=(maxPossibleJ-minPossibleJ+1);
+            // System.out.println("i="+i+" minPossibleJ="+minPossibleJ+" maxPossibleJ="+maxPossibleJ+" res="+res);
+        }
+
+        return res;
+    }
+}
