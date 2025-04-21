495f3ef8cf8d662430a87b6653242295b7d6edf6
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-04-21 23:28:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (34.94%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,18 +1,48 @@
+//memoization
 class Solution {
     public int rob(int[] nums) {
         int n=nums.length;
-        int prev1=nums[n-1];
-        int prev2=0;
+        //pos is changing [0,n-1]
+        int[] dp=new int[n];
+        Arrays.fill(dp,-1);
 
-        for (int i=n-2;i>=0;i--) {
-            int pick=nums[i]+prev2;
-            int notPick=prev1;
+        return solve(0,dp,nums,n);
+    }
 
-            int curr=Math.max(pick,notPick);
-            prev2=prev1;
-            prev1=curr;
-        }
+    public int solve(int pos,int[] dp,int[] nums,int n) {
+        if (pos==n-1)
+            return nums[pos];
 
-        return prev1;
+        if (pos>=n)
+            return 0;
+
+        if (dp[pos]!=-1)
+            return dp[pos];
+
+        int pick=nums[pos]+solve(pos+2,dp,nums,n);
+        int notPick=0+solve(pos+1,dp,nums,n);
+
+        return dp[pos]=Math.max(pick,notPick);
     }
 }
+
+/*
+Dynamic programming(DP) - storing states/(remembering the past)
+1. write a recursive solution
+2. write memoization
+3. write tabulation
+4. write space optimization
+
+Sub-topics
+1. Simple DP / 1-D Dp -> 1
+2. Dp on grids -> 1
+3. Dp on subsequences -> 2
+4. Dp on strings -> 1
+5. Dp on stocks -> 1
+6. Dp on Longest Increasing subsequence
+7. Partion Dp(hard)
+*/
+
+
+
+
