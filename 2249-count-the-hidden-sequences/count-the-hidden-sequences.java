5c555b017dc87b980f90665ad3a64d150f0c939f
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-04-21 14:40:17 +0530

    Time: 7 ms (15.87%) | Memory: 61.9 MB (25.40%) - LeetSync

---
+++ 2249-count-the-hidden-sequences/count-the-hidden-sequences.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numberOfArrays(int[] differences, int lower, int upper) {
+        int n=differences.length;
+        int cnt=0;
+        int[] currInterval={lower,upper};
+        int res=0;
+
+        for (int i=0;i<n;i++) {
+            currInterval[0]=currInterval[0]-differences[i];
+            currInterval[1]=currInterval[1]-differences[i];
+
+            currInterval[0]=Math.max(currInterval[0],lower);
+            currInterval[1]=Math.min(currInterval[1],upper);
+
+            // System.out.println(i+" "+Arrays.toString(currInterval));
+
+            if (currInterval[0]>currInterval[1])
+                return 0;
+        }
+        
+        res=(currInterval[1]-currInterval[0]+1);
+        return res;
+    }
+}
