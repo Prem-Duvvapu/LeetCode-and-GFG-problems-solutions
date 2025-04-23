ff6deb6daa8fbc8f65d4b2420e9ba6e6046f93c9
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-04-23 13:51:53 +0530

    Time: 4 ms (77.06%) | Memory: 41.1 MB (48.39%) - LeetSync

--- 1500-count-largest-group/count-largest-group.java
+++ 1500-count-largest-group/count-largest-group.java
@@ -1,31 +1,39 @@
 class Solution {
     public int countLargestGroup(int n) {
-        Map<Integer,Integer> map=new HashMap<>();
+        //we will get maxSum when num=9999, sum=9+9+9+9=36
+        int[] groupSize=new int[37];
         int maxGroupSize=0;
         int groupsCnt=0;
 
         for (int num=1;num<=n;num++) {
-            int temp=num;
-            int sumOfDigits=0;
-
-            while (temp>0) {
-                int lastDigit=temp%10;
-                sumOfDigits+=lastDigit;
-                temp/=10;
-            }
-
-            map.put(sumOfDigits,map.getOrDefault(sumOfDigits,0)+1);
+            int digitsSum=sumOfDigits(num);
+            groupSize[digitsSum]++;
         }
 
-        for (int freq: map.values()) {
-            if (freq>maxGroupSize) {
-                maxGroupSize=freq;
+        // System.out.println(Arrays.toString(groupSize));
+
+        for (int sum=1;sum<groupSize.length;sum++) {
+            if (groupSize[sum]>maxGroupSize) {
+                maxGroupSize=groupSize[sum];
                 groupsCnt=1;
-            } else if (freq==maxGroupSize) {
+            } else if (groupSize[sum]==maxGroupSize) {
                 groupsCnt++;
             }
         }
 
         return groupsCnt;
+    }
+
+    public int sumOfDigits(int num) {
+        int temp=num;
+        int totalSum=0;
+
+        while (temp>0) {
+            int lastDigit=temp%10;
+            totalSum+=lastDigit;
+            temp/=10;
+        }
+
+        return totalSum;
     }
 }
