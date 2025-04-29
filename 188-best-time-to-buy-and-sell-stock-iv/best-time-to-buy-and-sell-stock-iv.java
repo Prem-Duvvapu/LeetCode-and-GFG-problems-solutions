36d43031720542c5d345f89d4370e641bdbfaad1
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-04-29 23:45:00 +0530

    Time: 9 ms (21.59%) | Memory: 44.8 MB (36.00%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,36 +1,64 @@
 class Solution {
-    public int maxProfit(int k,int[] prices) {
+    public int maxProfit(int k, int[] prices) {
         int n=prices.length;
         int[][][] dp=new int[n][2][k+1];
 
         for (int i=0;i<n;i++)
             for (int j=0;j<2;j++)
-                for (int l=0;l<=k;l++)
+                for (int l=0;l<k+1;l++)
                     dp[i][j][l]=-1;
 
-        return solve(0,prices,1,k,dp);
+        return solve(0,1,k,dp,prices);
     }
 
-    private int solve(int pos,int[] prices,int canBuy,int count,int[][][] dp) {
-        if (pos==prices.length || count==0)
+    public int solve(int pos,int canBuy,int k,int[][][] dp,int[] prices) {
+        if (k==0)
             return 0;
 
-        if (dp[pos][canBuy][count]!=-1)
-            return dp[pos][canBuy][count];
+        if (pos==prices.length)
+            return 0;
 
-        int res=0;
-        int b=0;
-        int s=0;
+        if (dp[pos][canBuy][k]!=-1)
+            return dp[pos][canBuy][k];
+
+        int maxRes=0;
+
         if (canBuy==1) {
-            int bought=-1*prices[pos]+solve(pos+1,prices,0,count,dp);
-            int notBought=0+solve(pos+1,prices,canBuy,count,dp);
-            b=Math.max(bought,notBought);
-        } else {
-            int sell=prices[pos]+solve(pos+1,prices,1,count-1,dp);
-            int notSell=0+solve(pos+1,prices,canBuy,count,dp);
-            s=Math.max(sell,notSell);
-        }
+            //buy
+            int buy= -1*prices[pos] + solve(pos+1,0,k,dp,prices);
 
-        return dp[pos][canBuy][count]=Math.max(b,s);
+            //buy later
+            int buyLater=solve(pos+1,1,k,dp,prices);
+
+            maxRes=Math.max(buy,buyLater);
+        } else {
+            //sell
+            int sell=prices[pos] + solve(pos+1,1,k-1,dp,prices);
+
+            //sell later
+            int sellLater=solve(pos+1,0,k,dp,prices);
+
+            maxRes=Math.max(sell,sellLater);
+        }
+        
+        return dp[pos][canBuy][k]=maxRes;
     }
 }
+
+/*
+
+pos  -> 0 to n-1
+k -> k to 0
+canBuy -> 0/1
+
+dp[pos][k][canBuy]
+    n   k+1  2
+
+1. do at max k transactions  -> give me max profit
+2. do at max k transactions with cooldown period -> give me max profit
+3. do at max k transactions with trasaction fee -> give me max profit
+
+buy and sell -> 1 transaction
+at max transactionsl
+
+*/
