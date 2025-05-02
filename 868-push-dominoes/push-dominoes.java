1b7364ea6ea2f35bba8dda8f4be5ddf1d03c14e5
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-05-02 16:42:53 +0530

    Time: 28 ms (30.94%) | Memory: 46.1 MB (29.43%) - LeetSync

---
+++ 868-push-dominoes/push-dominoes.java
@@ -0,0 +1,54 @@
+class Solution {
+    public String pushDominoes(String dominoes) {
+        int n=dominoes.length();
+        char[] res=dominoes.toCharArray();
+
+        int[] Ldist=new int[n];
+        int[] Rdist=new int[n];
+
+        Arrays.fill(Ldist,Integer.MAX_VALUE);
+        Arrays.fill(Rdist,Integer.MAX_VALUE);
+
+        int Lpos=Integer.MAX_VALUE;
+        int Rpos=Integer.MAX_VALUE;
+
+        for (int i=n-1;i>=0;i--) {
+            // System.out.println(i+" "+Lpos+" "+Rpos);
+            if (dominoes.charAt(i) == 'L')
+                Lpos = i;
+            else if (dominoes.charAt(i) == 'R')
+                Rpos = i;
+            else if (dominoes.charAt(i)=='.') {
+                if (Lpos!=Integer.MAX_VALUE && Lpos<Rpos)
+                    Ldist[i]=Lpos-i;
+            }
+        }
+
+        Lpos=-1;
+        Rpos=Integer.MAX_VALUE;
+        for (int i=0;i<n;i++) {
+            if (dominoes.charAt(i) == 'L')
+                Lpos = i;
+            else if (dominoes.charAt(i) == 'R')
+                Rpos = i;
+            else if (dominoes.charAt(i)=='.') {
+                if (Rpos!=Integer.MAX_VALUE && Rpos>Lpos)
+                    Rdist[i]=i-Rpos;
+            }
+        }
+
+        for (int i=0;i<n;i++) {
+            if (res[i]=='.') {
+                if (Ldist[i]<Rdist[i])
+                    res[i]='L';
+                else if (Rdist[i]<Ldist[i])
+                    res[i]='R';
+            }
+        }
+
+        // System.out.println(Arrays.toString(Ldist));
+        // System.out.println(Arrays.toString(Rdist));
+
+        return new String(res);
+    }
+}
