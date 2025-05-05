b55cda1c178338197a29af7544f4c422bc5d0ca4
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-05-05 21:09:08 +0530

    Time: 14 ms (59.84%) | Memory: 48.6 MB (60.97%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,30 +1,20 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int n=nums.length;
         int[] res=new int[k];
-        Map<Integer,Integer> freqMap=new HashMap<>();
-        List<Integer>[] freqList=new ArrayList[n+1];
-        
+        Map<Integer,Integer> map=new HashMap<>();
+
         for (int val: nums)
-            freqMap.put(val,freqMap.getOrDefault(val,0)+1);
+            map.put(val,map.getOrDefault(val,0)+1);
 
-        for (Map.Entry<Integer,Integer> m: freqMap.entrySet()) {
-            int element=m.getKey();
-            int freq=m.getValue();
+        PriorityQueue<Integer> pq=new PriorityQueue<>((x,y) -> Integer.compare(map.get(y),map.get(x)));
+        for (int val: map.keySet())
+            pq.add(val);
 
-            if (freqList[freq]==null)
-                freqList[freq]=new ArrayList<>();
-
-            freqList[freq].add(element);
+        int pos=0;
+        while (pos<k) {
+            res[pos]=pq.poll();
+            pos++;
         }
-
-        List<Integer> resList=new ArrayList<>();
-        for (int freq=n;freq>=1;freq--)
-            if (freqList[freq]!=null)
-                resList.addAll(freqList[freq]);
-        
-        for (int i=0;i<k;i++)
-            res[i]=resList.get(i);
 
         return res;
     }
