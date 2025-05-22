7f95cf5bf6a92c03dbbfc35cbece241343d3d7a0
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-05-22 20:06:23 +0530

    Time: 10 ms (45.31%) | Memory: 57.4 MB (11.89%) - LeetSync

---
+++ 148-sort-list/sort-list.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        return mergeSort(head);
+    }
+
+    private ListNode mergeSort(ListNode head) {
+        if (head==null || head.next==null)
+            return head;
+
+        ListNode middleNode=findMiddleNode(head);
+        ListNode temp=middleNode.next;
+        middleNode.next=null;
+
+        ListNode leftHead=mergeSort(head);
+        ListNode rightHead=mergeSort(temp);
+
+        return merge(leftHead,rightHead);
+    }
+
+    private ListNode merge(ListNode leftHead,ListNode rightHead) {
+        ListNode tempHead=new ListNode(-1);
+        ListNode temp=tempHead;
+
+        while (leftHead!=null && rightHead!=null) {
+            if (leftHead.val<=rightHead.val) {
+                temp.next=leftHead;
+                leftHead=leftHead.next;
+                temp=temp.next;
+            } else {
+                temp.next=rightHead;
+                rightHead=rightHead.next;
+                temp=temp.next;
+            }
+        }
+
+        if (leftHead!=null)
+            temp.next=leftHead;
+
+        if (rightHead!=null)
+            temp.next=rightHead;
+
+        return tempHead.next;
+    }
+
+    private ListNode findMiddleNode(ListNode head) {
+        if (head==null || head.next==null)
+            return head;
+
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while (fast!=null && fast.next!=null && fast.next.next!=null) {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+
+        return slow;
+    }
+}
