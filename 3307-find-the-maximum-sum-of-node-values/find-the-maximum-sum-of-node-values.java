23768df12a1a7d872a81f2424a40a54278f9e573
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-05-23 21:18:46 +0530

    Time: 2 ms (76.32%) | Memory: 54.5 MB (76.32%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.java
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.java
@@ -2,28 +2,23 @@ class Solution {
     public long maximumValueSum(int[] nums, int k, int[][] edges) {
         long res=0;
         int cnt=0;
+        int minLoss=Integer.MAX_VALUE;
 
         for (int val: nums) {
-            if ((val^k)>val)
+            if ((val^k)>val) {
                 cnt++;
+                res+=(val^k);
+            } else {
+                res+=val;
+            }
 
-            res+=Math.max(val,val^k);
+            minLoss=Math.min(minLoss,Math.abs(val-(val^k)));
         }
 
         if ((cnt&1)==0)//even
             return res;
 
-        int minLoss=Integer.MAX_VALUE;
-        int minProfit=Integer.MAX_VALUE;
-
-        for (int val: nums) {
-            if ((val^k)>val)
-                minProfit=Math.min(minProfit,(val^k)-val);
-            else if (val>(val^k))
-                minLoss=Math.min(minLoss,val-(val^k));
-        }
-        
-        res=Math.max(res-minLoss,res-minProfit);
+        res-=minLoss;
         return res;
     }
 }
