d0a4a65daeaf7b1a78250e2001f58e7d640040d9
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-05-27 17:39:45 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (58.84%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -35,7 +35,6 @@ class Solution {
             last.next=null;
             newFirst=reverseLL(first);
             newLast=first;
-            newLast.next=null;
 
             prev.next=newFirst;
             prev=newLast;
