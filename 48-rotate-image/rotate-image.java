decd95c0e9b70546eae4d1c2ac1fd1e29b630b2b
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-07-03 12:02:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (25.56%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,38 +1,37 @@
-//Optimal
-//T.C. : O(n*n)
-//S.C. : O(1)
-
-/*
-Steps
-1. transpose the matrix
-2. reverse each row after transposing the matrix
-*/
-
 class Solution {
     public void rotate(int[][] matrix) {
-        int n=matrix.length;
-            
-        for (int i=0;i<n;i++)
-            for (int j=i+1;j<n;j++)
+        int n = matrix.length;
+
+        for (int i=0;i<n;i++) {
+            for (int j=i+1;j<n;j++) {
                 swap(matrix,i,j,j,i);
+            }
+        }
 
         for (int i=0;i<n;i++)
-            reverse(matrix,i,0,n-1);
+            reverse(matrix[i]);
     }
 
-    public void swap(int[][] matrix,int r1,int c1,int r2,int c2) {
-        int temp=matrix[r1][c1];
-        matrix[r1][c1]=matrix[r2][c2];
-        matrix[r2][c2]=temp;
+    private void swap(int[][] a,int i,int j,int x,int y) {
+        int temp = a[i][j];
+        a[i][j] = a[x][y];
+        a[x][y] = temp;
     }
 
-    public void reverse(int[][] matrix,int row,int colStart,int colEnd) {
-        while (colStart<colEnd) {
-            int temp=matrix[row][colStart];
-            matrix[row][colStart]=matrix[row][colEnd];
-            matrix[row][colEnd]=temp;
-            colStart++;
-            colEnd--;
+    private void swap(int[] a,int i,int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    private void reverse(int[] a) {
+        int low = 0;
+        int high = a.length-1;
+
+        while (low<high) {
+            swap(a,low,high);
+            low++;
+            high--;
         }
     }
 }
