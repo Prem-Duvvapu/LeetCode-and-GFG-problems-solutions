7dd79fccab27adb72e5af33793b8bdcfe8a57f61
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-07-09 12:00:47 +0530

    Time: 2 ms (99.71%) | Memory: 50.5 MB (41.91%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -2,14 +2,43 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
         List<Integer> res = new ArrayList<>();
-        Map<Integer,Integer> map = new HashMap<>();
+        int majority1 = Integer.MIN_VALUE;
+        int cnt1 = 0;
+        int majority2 = Integer.MAX_VALUE;
+        int cnt2 = 0;
 
-        for (int val: nums)
-            map.put(val,map.getOrDefault(val,0)+1);
-        
-        for (Map.Entry<Integer,Integer> m: map.entrySet())
-            if (m.getValue() > n/3)
-                res.add(m.getKey());
+        for (int val: nums) {
+            if (val==majority1) {
+                cnt1++;
+            } else if (val==majority2) {
+                cnt2++;
+            } else if (cnt1==0) {
+                majority1 = val;
+                cnt1++;
+            } else if (cnt2==0) {
+                majority2 = val;
+                cnt2++;
+            } else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        int cntCheck1 = 0;
+        int cntCheck2 = 0;
+
+        for (int val: nums) {
+            if (val==majority1)
+                cntCheck1++;
+            else if (val==majority2)
+                cntCheck2++;
+        }
+
+        if (cntCheck1 > n/3)
+            res.add(majority1);
+
+        if (cntCheck2 > n/3)
+            res.add(majority2);
 
         return res;
     }
