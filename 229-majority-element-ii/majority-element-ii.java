f10c4b4a5f7fb5148affe3e7babcb8e39e6ce3b9
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-07-09 11:51:19 +0530

    Time: 14 ms (39.20%) | Memory: 50 MB (81.34%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,43 +1,15 @@
-//optimal
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n=nums.length;
-        List<Integer> res=new ArrayList<>();
-        int mE1=0;
-        int mE1Cnt=0;
-        int mE2=0;
-        int mE2Cnt=0;
+        int n = nums.length;
+        List<Integer> res = new ArrayList<>();
+        Map<Integer,Integer> map = new HashMap<>();
 
-        for (int i=0;i<n;i++) {
-            if (mE1Cnt==0 && nums[i]!=mE2) {
-                mE1=nums[i];
-                mE1Cnt++;
-            } else if (mE2Cnt==0 && nums[i]!=mE1) {
-                mE2=nums[i];
-                mE2Cnt++;
-            } else if (nums[i]==mE1) {
-                mE1Cnt++;
-            } else if (nums[i]==mE2) {
-                mE2Cnt++;
-            } else {
-                mE1Cnt--;
-                mE2Cnt--;
-            }
-        }
-
-        mE1Cnt=mE2Cnt=0;
-        for (int i=0;i<n;i++) {
-            if (nums[i]==mE1)
-                mE1Cnt++;
-            else if (nums[i]==mE2)
-                mE2Cnt++;
-        }
-
-        if (mE1Cnt > n/3)
-            res.add(mE1);
-
-        if (mE2Cnt > n/3)
-            res.add(mE2);
+        for (int val: nums)
+            map.put(val,map.getOrDefault(val,0)+1);
+        
+        for (Map.Entry<Integer,Integer> m: map.entrySet())
+            if (m.getValue() > n/3)
+                res.add(m.getKey());
 
         return res;
     }
