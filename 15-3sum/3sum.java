a8358d5a65aa8f53736c23ec5ed892865e09c91b
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-07-10 12:49:09 +0530

    Time: 29 ms (74.09%) | Memory: 51.2 MB (96.87%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,55 +1,53 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int n=nums.length;
-        List<List<Integer>> res=new ArrayList<>();
+        int n = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
 
         Arrays.sort(nums);
-        solve(0,new ArrayList<>(),res,0,nums,n);
+        solve(res,0,nums,n);
 
         return res;
     }
 
-    public void solve(int pos,List<Integer> currList,List<List<Integer>> res,int target,int[] nums,int n) {
-        if (currList.size()==0) {
-            for (int i=0;i<n;i++) {
-                if (i>0 && nums[i]==nums[i-1])
-                    continue;
+    private void solve(List<List<Integer>> res,int target,int[] nums,int n) {
+        List<Integer> currList = new ArrayList<>();
 
-                currList.add(nums[i]);
-                solve(i+1,currList,res,target-nums[i],nums,n);
-                currList.remove(currList.size()-1);
-            }
+        for (int i=0;i<n;i++) {
+            if (i>0 && nums[i]==nums[i-1])
+                continue;
 
-            return;
+            currList.add(nums[i]);
+            helper(i+1,currList,res,target-nums[i],nums,n);
+            currList.remove(currList.size()-1);
         }
+    }
 
-        int left=pos;
-        int right=n-1;
+    private void helper(int pos,List<Integer> currList,List<List<Integer>> res,int target,int[] nums,int n) {
+        int low = pos;
+        int high = n-1;
 
-        while (left<right) {
-            int leftVal=nums[left];
-            int rightVal=nums[right];
+        while (low<high) {
+            int currSum = nums[low]+nums[high];
+            if (currSum<target) {
+                low++;
+            } else if (currSum>target) {
+                high--;
+            } else {
+                currList.add(nums[low]);
+                currList.add(nums[high]);
 
-            if (leftVal+rightVal<target)
-                left++;
-            else if (leftVal+rightVal>target)
-                right--;
-            else {
-                currList.add(nums[left]);
-                currList.add(nums[right]);
                 res.add(new ArrayList<>(currList));
 
                 currList.remove(currList.size()-1);
                 currList.remove(currList.size()-1);
 
-                left++;
-                right--;
+                low++;
+                high--;
+                while (low<high && nums[low]==nums[low-1])
+                    low++;
 
-                while (left<right && nums[left]==nums[left-1])
-                    left++;
-
-                while (right>left && nums[right]==nums[right+1])
-                    right--;
+                while (high>low && nums[high]==nums[high+1])
+                    high--;
             }
         }
     }
