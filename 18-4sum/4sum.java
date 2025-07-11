daa352222bfa9744dceb59e508d3fb12bd94f66d
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-07-11 12:14:50 +0530

    Time: 15 ms (88.05%) | Memory: 44.6 MB (22.97%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,48 +1,63 @@
-//optimal
-//Time: O(nlogn)+O(n^3)
-//Space: O(1)+O(no. of unique triplets)
-
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n=nums.length;
-        List<List<Integer>> resList=new ArrayList<>();
+        int n = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+
         Arrays.sort(nums);
+        solve(res,target,nums,n);
+
+        return res;
+    }
+
+    private void solve(List<List<Integer>> res,int target,int[] nums,int n) {
+        List<Integer> currList = new ArrayList<>();
 
         for (int i=0;i<n;i++) {
             if (i>0 && nums[i]==nums[i-1])
                 continue;
+            
+            currList.add(nums[i]);
 
             for (int j=i+1;j<n;j++) {
                 if (j>i+1 && nums[j]==nums[j-1])
-                    continue;
+                continue;
+            
+                currList.add(nums[j]);
+                helper(j+1,currList,res,(long)target-(nums[i]+nums[j]),nums,n);
+                currList.remove(currList.size()-1);
+            }
 
-                int left=j+1;
-                int right=n-1;
+            currList.remove(currList.size()-1);
+        }
+    }
 
-                while (left<right) {
-                    long sum=nums[i]+nums[j];
-                    sum+=(nums[left]+nums[right]);
-                    if (sum<(long)target) {
-                        left++;
-                    } else if (sum>(long)target) {
-                        right--;
-                    } else {
-                        List<Integer> res=new ArrayList<>(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
-                        resList.add(res);
+    private void helper(int pos,List<Integer> currList,List<List<Integer>> res,long target,int[] nums,int n) {
+        int low= pos;
+        int high = n-1;
 
-                        left++;
-                        right--;
+        while (low<high) {
+            long currSum = nums[low]+nums[high];
 
-                        while (left<right && nums[left]==nums[left-1])
-                            left++;
+            if (currSum<target) {
+                low++;
+            } else if (currSum>target) {
+                high--;
+            } else {
+                currList.add(nums[low]);
+                currList.add(nums[high]);
+                res.add(new ArrayList<>(currList));
+                currList.remove(currList.size()-1);
+                currList.remove(currList.size()-1);
 
-                        while (left<right && nums[right]==nums[right+1])
-                            right--;
-                    }
-                }
+                low++;
+                high--;
+
+                while (low<high && nums[low]==nums[low-1])
+                    low++;
+
+                while (low<high && nums[high]==nums[high+1])
+                    high--;
             }
         }
-
-        return resList;
     }
 }
