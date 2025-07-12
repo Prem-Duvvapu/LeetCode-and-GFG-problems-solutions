eacdcd60e41818738ada3b9983c4dafa6fb0ee82
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-07-12 15:31:13 +0530

    Time: 677 ms (5.10%) | Memory: 41.3 MB (86.29%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,38 +1,19 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int n=nums.length;
-        int[][] dp=new int[n][3001];
-        for (int i=0;i<n;i++)
-            for (int j=0;j<3001;j++)
-                dp[i][j]=-1;
-
-        Arrays.sort(nums); // Sort in ascending order
-        
-        // Reverse the sorted numsay
-        for (int i = 0; i < nums.length / 2; i++) {
-            int temp = nums[i];
-            nums[i] = nums[nums.length - 1 - i];
-            nums[nums.length - 1 - i] = temp;
-        }
-
-        return solve(n-1,nums,target,dp);
+        int n = nums.length;
+        return solve(0,target,nums);
     }
 
-    private int solve(int pos,int[] nums,int target,int[][] dp) {
-        if (pos<0) {
-            if (target==0)
-                return 1;
+    private int solve(int pos,int target,int[] nums) {
+        if (pos==nums.length && target==0)
+            return 1;
+
+        if (pos>=nums.length)
             return 0;
-        }
 
-        if (dp[pos][target+1000]!=-1)
-            return dp[pos][target+1000];
+        int add = solve(pos+1,target-nums[pos],nums);
+        int subtract = solve(pos+1,target+nums[pos],nums);
 
-        int add=nums[pos]+solve(pos-1,nums,target-nums[pos],dp);
-        int sub=-1*nums[pos]+solve(pos-1,nums,target+nums[pos],dp);
-
-        int res=add+sub;
-
-        return dp[pos][target+1000]=res;
+        return add + subtract;
     }
 }
