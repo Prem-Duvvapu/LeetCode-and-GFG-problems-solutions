5589e974bd2983dfe6977606859ca37815d2eb81
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-07-12 19:42:55 +0530

    Time: 8 ms (35.01%) | Memory: 45.1 MB (27.71%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,46 +1,32 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
-        int n=prices.length;
-        int[][][] dp=new int[n][2][k+1];
+        int n = prices.length;
+        int[][][] dp = new int[n][2][k+1];
 
         for (int i=0;i<n;i++)
             for (int j=0;j<2;j++)
-                for (int l=0;l<k+1;l++)
-                    dp[i][j][l]=-1;
+                Arrays.fill(dp[i][j],-1);
 
-        return solve(0,1,k,dp,prices);
+        return solve(0,1,k,prices,dp);
     }
 
-    public int solve(int pos,int canBuy,int k,int[][][] dp,int[] prices) {
-        if (k==0)
-            return 0;
-
-        if (pos==prices.length)
+    private int solve(int pos,int canBuy,int k,int[] prices,int[][][] dp) {
+        if (k==0 || pos==prices.length)
             return 0;
 
         if (dp[pos][canBuy][k]!=-1)
             return dp[pos][canBuy][k];
 
-        int maxRes=0;
-
         if (canBuy==1) {
-            //buy
-            int buy= -1*prices[pos] + solve(pos+1,0,k,dp,prices);
+            int buy = -prices[pos] + solve(pos+1,1^canBuy,k,prices,dp);
+            int notBuy = solve(pos+1,canBuy,k,prices,dp);
 
-            //buy later
-            int buyLater=solve(pos+1,1,k,dp,prices);
-
-            maxRes=Math.max(buy,buyLater);
+            return dp[pos][canBuy][k] = Math.max(buy,notBuy);
         } else {
-            //sell
-            int sell=prices[pos] + solve(pos+1,1,k-1,dp,prices);
+            int sell = prices[pos] + solve(pos+1,1^canBuy,k-1,prices,dp);
+            int notSell = solve(pos+1,canBuy,k,prices,dp);
 
-            //sell later
-            int sellLater=solve(pos+1,0,k,dp,prices);
-
-            maxRes=Math.max(sell,sellLater);
+            return dp[pos][canBuy][k] = Math.max(sell,notSell);
         }
-        
-        return dp[pos][canBuy][k]=maxRes;
     }
 }
