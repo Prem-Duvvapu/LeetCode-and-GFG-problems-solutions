ef92c505406ec31e51901446da5963e3fb9930d5
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-07-12 16:11:07 +0530

    Time: 20 ms (74.02%) | Memory: 51.1 MB (42.60%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -2,36 +2,29 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int n = text1.length();
         int m = text2.length();
-        int[][] dp = new int[n+1][m+1];
+        int[][] dp = new int[n][m];
 
-        for (int i=0;i<n;i++) {
-            for (int j=0;j<m;j++) {
+        if (text1.charAt(0) == text2.charAt(0))
+            dp[0][0] = 1;
+
+        for (int j=1;j<m;j++)
+            dp[0][j] = (text1.charAt(0)==text2.charAt(j)) ? 1 : dp[0][j-1];
+
+        for (int i=1;i<n;i++)
+            dp[i][0] = (text1.charAt(i)==text2.charAt(0)) ? 1 : dp[i-1][0];
+
+        for (int i=1;i<n;i++) {
+            for (int j=1;j<m;j++) {
                 if (text1.charAt(i) == text2.charAt(j)) {
-                    dp[i+1][j+1] = 1 + dp[i][j];
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 } else {
-                    int moveI = dp[i][j+1];
-                    int moveJ = dp[i+1][j];
-                    dp[i+1][j+1] = Math.max(moveI,moveJ);
+                    int moveI = dp[i-1][j];
+                    int moveJ = dp[i][j-1];
+                    dp[i][j] = Math.max(moveI,moveJ);
                 }
             }
         }
 
-        return dp[n][m];
-    }
-
-    private int solve(int i,int j,String text1,String text2,int[][] dp) {
-        if (i<0 || j<0)
-            return 0;
-
-        if (dp[i][j]!=-1)
-            return dp[i][j];
-
-        if (text1.charAt(i) == text2.charAt(j))
-            return 1+solve(i-1,j-1,text1,text2,dp);
-
-        int moveI = solve(i-1,j,text1,text2,dp);
-        int moveJ = solve(i,j-1,text1,text2,dp);
-
-        return dp[i][j] = Math.max(moveI,moveJ);
+        return dp[n-1][m-1];
     }
 }
