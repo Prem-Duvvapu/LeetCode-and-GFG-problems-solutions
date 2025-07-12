59e9f5c49ec2a380ecbff42b0adec7c47627e770
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-07-12 19:39:44 +0530

    Time: 3 ms (12.07%) | Memory: 49.2 MB (6.06%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,35 +1,31 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n=prices.length;
-        int[][] dp=new int[n][2];
+        int n = prices.length;
+        int[][] dp = new int[n][2];
 
         for (int i=0;i<n;i++)
-            for (int j=0;j<2;j++)
-                dp[i][j]=-1;
+            Arrays.fill(dp[i],-1);
 
-        return solve(0,prices,1,dp);
+        return solve(0,1,prices,dp);
     }
 
-    private int solve(int pos,int[] prices,int canBuy,int[][] dp) {
+    private int solve(int pos,int canBuy,int[] prices,int[][] dp) {
         if (pos==prices.length)
             return 0;
 
         if (dp[pos][canBuy]!=-1)
             return dp[pos][canBuy];
 
-        int res=0;
-        int b=0;
-        int s=0;
         if (canBuy==1) {
-            int bought=-1*prices[pos]+solve(pos+1,prices,0,dp);
-            int notBought=0+solve(pos+1,prices,canBuy,dp);
-            b=Math.max(bought,notBought);
-        } else {
-            int sell=prices[pos]+solve(pos+1,prices,1,dp);
-            int notSell=0+solve(pos+1,prices,canBuy,dp);
-            s=Math.max(sell,notSell);
-        }
+            int buy = -prices[pos] + solve(pos+1,1^canBuy,prices,dp);
+            int notBuy = solve(pos+1,canBuy,prices,dp);
 
-        return dp[pos][canBuy]=Math.max(b,s);
+            return dp[pos][canBuy] = Math.max(buy,notBuy);
+        } else {
+            int sell = prices[pos] + solve(pos+1,1^canBuy,prices,dp);
+            int notSell = solve(pos+1,canBuy,prices,dp);
+
+            return dp[pos][canBuy] = Math.max(sell,notSell);
+        }
     }
 }
