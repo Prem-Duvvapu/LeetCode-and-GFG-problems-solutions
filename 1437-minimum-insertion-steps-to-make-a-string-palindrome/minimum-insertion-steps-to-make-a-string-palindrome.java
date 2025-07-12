3728f04998601befd68d50b3efdb565d77044622
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-07-12 16:19:05 +0530

    Time: 29 ms (31.10%) | Memory: 46 MB (10.13%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,27 +1,33 @@
 class Solution {
     public int minInsertions(String s) {
-        String text1=s;
-        String text2=new StringBuilder(text1).reverse().toString();
-        int n=text1.length();
-        int m=text2.length();
-        int[][] dp=new int[n+1][m+1];
-        
-        //base case
-        // for (int j=0;j<=m;j++)
-        //     dp[0][j]=0;
+        String text1 = s;
+        String text2 = new StringBuilder(text1).reverse().toString();
+        int n = text1.length();
+        int m = text2.length();
+        int[][] dp = new int[n][m];
 
-        // for (int i=0;i<=n;i++)
-        //     dp[i][0]=0;
+        if (text1.charAt(0) == text2.charAt(0))
+            dp[0][0] = 1;
 
-        for (int i=1;i<=n;i++) {
-            for (int j=1;j<=m;j++) {
-                if (text1.charAt(i-1)==text2.charAt(j-1))
-                    dp[i][j]=1+dp[i-1][j-1];
-                else
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+        for (int j=1;j<m;j++)
+            dp[0][j] = (text1.charAt(0)==text2.charAt(j)) ? 1 : dp[0][j-1];
+
+        for (int i=1;i<n;i++)
+            dp[i][0] = (text1.charAt(i)==text2.charAt(0)) ? 1 : dp[i-1][0];
+
+        for (int i=1;i<n;i++) {
+            for (int j=1;j<m;j++) {
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    int moveI = dp[i-1][j];
+                    int moveJ = dp[i][j-1];
+                    dp[i][j] = Math.max(moveI,moveJ);
+                }
             }
         }
 
-        return (n-dp[n][m]);
+        int lps = dp[n-1][m-1];
+        return (n-lps);
     }
 }
