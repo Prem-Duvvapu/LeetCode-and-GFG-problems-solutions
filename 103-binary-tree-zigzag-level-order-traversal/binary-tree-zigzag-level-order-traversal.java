8ae61bf7840e391ea6a146a68ac2861d1fa3d17a
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-07-22 19:46:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (7.30%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -15,32 +15,34 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> res=new ArrayList<>();
-        if (root==null)
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
             return res;
-        Queue<TreeNode> q=new LinkedList<>();
-        q.add(root);
-        boolean rightToLeft=false;
 
-        while (!q.isEmpty())
-        {
-            int qlen=q.size();
-            List<Integer> currLevel=new ArrayList<>();
-            while (qlen-- > 0)
-            {
-                TreeNode t=q.poll();
-                if (rightToLeft)
-                    currLevel.add(0,t.val);
-                else
-                    currLevel.add(t.val);
-                if (t.left!=null)
-                    q.add(t.left);
-                if (t.right!=null)
-                    q.add(t.right);
+        boolean isEven = true;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int qlen = q.size();
+            List<Integer> list = new ArrayList<>();
+
+            while (qlen-- > 0) {
+                TreeNode curr = q.poll();
+                list.add(curr.val);
+
+                if (curr.left!=null)
+                    q.add(curr.left);
+
+                if (curr.right!=null)
+                    q.add(curr.right);
             }
-            
-            rightToLeft=!rightToLeft;
-            res.add(currLevel);
+
+            if (!isEven)
+                Collections.reverse(list);
+
+            res.add(list);
+            isEven = !isEven;
         }
 
         return res;
