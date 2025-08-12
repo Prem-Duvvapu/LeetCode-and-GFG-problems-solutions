76377b09ff406bfde1968047127407a78146a33d
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-08-12 11:17:53 +0530

    Time: 103 ms (48.19%) | Memory: 88 MB (5.50%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.java
+++ 854-making-a-large-island/making-a-large-island.java
@@ -1,97 +1,97 @@
 class DisjointSet {
-    int[] parent;
-    int[] size;
+    public int[] parent;
+    public int[] size;
 
-    DisjointSet(int n) {
-        parent=new int[n];
-        size=new int[n];
+    public DisjointSet(int n) {
+        parent = new int[n];
+        size = new int[n];
 
         for (int i=0;i<n;i++) {
-            parent[i]=i;
-            size[i]=1;
+            parent[i] = i;
+            size[i] = 1;
         }
     }
 
-    public int uPar(int node) {
-        if (parent[node]==node)
+    public int getUltimateParent(int node) {
+        if (parent[node] == node)
             return node;
 
-        return parent[node]=uPar(parent[node]);
+        return parent[node] = getUltimateParent(parent[node]);
     }
 
-    public void unionBySize(int u,int v) {
-        int uParU=uPar(u);
-        int uParV=uPar(v);
+    public void unionBySize(int u, int v) {
+        int ultParU = getUltimateParent(u);
+        int ultParV = getUltimateParent(v);
 
-        if (uParU==uParV)
+        if (ultParU == ultParV)
             return;
 
-        if (size[uParU]>=size[uParV]) {
-            parent[uParV]=uParU;
-            size[uParU]+=size[uParV];
+        if (size[ultParU] >= size[ultParV]) {
+            size[ultParU] += size[ultParV];
+            parent[ultParV] = ultParU;
         } else {
-            parent[uParU]=uParV;
-            size[uParV]+=size[uParU];
+            size[ultParV] += size[ultParU];
+            parent[ultParU] = ultParV;
         }
     }
 }
 
 class Solution {
+    public int[] dRow = {-1,0,1,0};
+    public int[] dCol = {0,1,0,-1};
+
     public int largestIsland(int[][] grid) {
-        int n=grid.length;
-        DisjointSet ds=new DisjointSet(n*n);
-        int[] dRow={-1,0,1,0};
-        int[] dCol={0,1,0,-1};
+        int n = grid.length;
+        DisjointSet ds = new DisjointSet(n*n);
+        int maxSize = 0;
 
         for (int i=0;i<n;i++) {
             for (int j=0;j<n;j++) {
-                if (grid[i][j]==0)
+                if (grid[i][j] == 0)
                     continue;
 
-                int node=i*n+j;
+                int node = n*i + j;
                 for (int k=0;k<4;k++) {
-                    int newRow=i+dRow[k];
-                    int newCol=j+dCol[k];
-                    int newNode=n*newRow+newCol;
+                    int newRow = i + dRow[k];
+                    int newCol = j + dCol[k];
+                    int newNode = n*newRow + newCol;
 
-                    if (newRow>=0 && newRow<n && newCol>=0 && newCol<n && grid[newRow][newCol]==1) {
-                        ds.unionBySize(node,newNode);
-                        // System.out.println(""+i+" "+j+" "+newRow+" "+newCol+" "+Arrays.toString(ds.size));
-                    }
+                    if (newRow>=0 && newRow<n && newCol>=0 && newCol<n && grid[newRow][newCol]==1)
+                        ds.unionBySize(node, newNode);
                 }
             }
         }
 
-        int finalRes=1;
+        int oneCnt = 0;
         for (int i=0;i<n;i++) {
             for (int j=0;j<n;j++) {
-                if (grid[i][j]==1)
+                if (grid[i][j] == 1) {
+                    oneCnt++;
                     continue;
+                }
 
-                int node=i*n+j;
-                Set<Integer> set=new HashSet<>();
+                Set<Integer> set = new HashSet<>();
                 for (int k=0;k<4;k++) {
-                    int newRow=i+dRow[k];
-                    int newCol=j+dCol[k];
-                    int newNode=n*newRow+newCol;
+                    int newRow = i + dRow[k];
+                    int newCol = j+ dCol[k];
+                    int newNode = n*newRow + newCol;
 
                     if (newRow>=0 && newRow<n && newCol>=0 && newCol<n && grid[newRow][newCol]==1) {
-                        set.add(ds.uPar(newNode));
+                        set.add(ds.getUltimateParent(newNode));
                     }
                 }
 
-                int currRes=1;
-                for (int uniqueParent: set) {
-                    currRes+=ds.size[uniqueParent];
-                }
-                finalRes=Math.max(finalRes,currRes);
+                int currSize = 1;
+                for (int uniqueParent: set)
+                    currSize += ds.size[uniqueParent];
+
+                maxSize = Math.max(maxSize, currSize);
             }
         }
 
-        // System.out.println(Arrays.toString(ds.size));
-        if (ds.size[0]==n*n)
+        if (oneCnt == n*n)
             return n*n;
 
-        return finalRes;
+        return maxSize;
     }
 }
