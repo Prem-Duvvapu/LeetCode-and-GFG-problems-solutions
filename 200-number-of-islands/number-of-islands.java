41566734951672c4e8c13599166894989d4620cd
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-08-18 16:18:30 +0530

    Time: 4 ms (49.33%) | Memory: 49.6 MB (54.01%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,60 +1,34 @@
-class Pair {
-    int row;
-    int col;
-
-    Pair(int row,int col) {
-        this.row=row;
-        this.col=col;
-    }
-}
-
 class Solution {
-    int[] dRow={-1,0,1,0};
-    int[] dCol={0,1,0,-1};
+    public static int[] dRow = {-1,0,1,0};
+    public static int[] dCol = {0,1,0,-1};
 
     public int numIslands(char[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        boolean[][] visited=new boolean[m][n];
-        int numOfIslands=0;
+        int cnt = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
 
         for (int i=0;i<m;i++) {
             for (int j=0;j<n;j++) {
-                if (grid[i][j]=='1' && !visited[i][j]) {
-                    bfs(i,j,visited,grid,m,n);
-                    numOfIslands++;
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    cnt++;
+                    dfs(i,j,visited,grid,m,n);
                 }
             }
         }
 
-        return numOfIslands;
+        return cnt;
     }
 
-    public void bfs(int r,int c,boolean[][] visited,char[][] grid,int m,int n) {
-        Queue<Pair> q=new LinkedList<>();
-        visited[r][c]=true;
-        q.add(new Pair(r,c));
+    private void dfs(int r,int c,boolean[][] visited,char[][] grid,int m,int n) {
+        visited[r][c] = true;
 
-        while (!q.isEmpty()) {
-            Pair curr=q.poll();
-            int currRow=curr.row;
-            int currCol=curr.col;
+        for (int i=0;i<4;i++) {
+            int newRow = r + dRow[i];
+            int newCol = c + dCol[i];
 
-            for (int i=0;i<4;i++) {
-                int newRow=currRow+dRow[i];
-                int newCol=currCol+dCol[i];
-
-                if (newRow>=0 && newRow<m && newCol>=0 && newCol<n && grid[newRow][newCol]=='1' && !visited[newRow][newCol]) {
-                    q.add(new Pair(newRow,newCol));
-                    visited[newRow][newCol]=true;
-                }
-            }
+            if (newRow >=0 && newRow < m && newCol >= 0 && newCol < n && grid[newRow][newCol] == '1' && !visited[newRow][newCol])
+                dfs(newRow,newCol,visited,grid,m,n);
         }
     }
 }
-
-/*
-        r-1,c
-r,c-1    r,c    r,c+1
-        r+1,c
-*/
