981a905bd36c6fb7727f2602fed00eb03fedc57b
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-08-18 16:09:38 +0530

    Time: 1 ms (86.74%) | Memory: 48.2 MB (28.54%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,58 +1,27 @@
-class DisjointSet {
-    int[] parent;
-    int[] size;
-
-    public DisjointSet(int n) {
-        parent = new int[n];
-        size = new int[n];
-
-        for (int i=0;i<n;i++) {
-            parent[i] = i;
-            size[i] = 1;
-        }
-    }
-
-    public int getParent(int node) {
-        if (parent[node] == node)
-            return node;
-
-        return parent[node] = getParent(parent[node]);
-    }
-
-    public void unionBySize(int u, int v) {
-        int uParent = getParent(u);
-        int vParent = getParent(v);
-
-        if (uParent == vParent)
-            return;
-
-        if (size[uParent] >= size[vParent]) {
-            size[uParent] += size[vParent];
-            parent[vParent] = uParent;
-        } else {
-            size[vParent] += size[uParent];
-            parent[uParent] = vParent;
-        }
-    }
-}
-
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int res = 0;
+        int cnt = 0;
         int n = isConnected.length;
-        DisjointSet ds = new DisjointSet(n);
+        boolean[] visited = new boolean[n];
 
         for (int i=0;i<n;i++) {
-            for (int j=0;j<n;j++) {
-                if (isConnected[i][j] == 1)
-                    ds.unionBySize(i,j);
-            }
+            if (!visited[i]) {
+                cnt++;
+                dfs(i,visited,isConnected,n);
+            }     
         }
 
-        for (int i=0;i<n;i++)
-            if (ds.parent[i] == i)
-                res++;
+        return cnt;
+    }
 
-        return res;
+    private void dfs(int curr,boolean[] visited,int[][] adjMatrix,int n) {
+        visited[curr] = true;
+
+        for (int j=0;j<n;j++) {
+            if (curr == j || adjMatrix[curr][j] == 0 || visited[j])
+                continue;
+
+            dfs(j,visited,adjMatrix,n);
+        }
     }
 }
