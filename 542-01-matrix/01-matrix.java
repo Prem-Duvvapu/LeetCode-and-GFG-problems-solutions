0de7c7efca762b0a9f0a0bee22a358b2ff63f2c9
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-08-20 15:43:06 +0530

    Time: 18 ms (14.55%) | Memory: 47.3 MB (56.27%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -3,69 +3,55 @@ class Pair {
     int col;
 
     Pair(int row,int col) {
-        this.row=row;
-        this.col=col;
+        this.row = row;
+        this.col = col;
     }
 }
 
 class Solution {
+    public static int[] dRow = {-1,0,1,0};
+    public static int[] dCol = {0,1,0,-1};
+
     public int[][] updateMatrix(int[][] mat) {
-        int m=mat.length;
-        int n=mat[0].length;
-        int[][] res=new int[m][n];
-        Queue<Pair> q=new LinkedList<>();
+        int m = mat.length;
+        int n = mat[0].length;
+        Queue<Pair> q = new LinkedList<>();
+        int[][] res = new int[m][n];
 
         for (int i=0;i<m;i++) {
             for (int j=0;j<n;j++) {
-                if (mat[i][j]==0) {
+                if (mat[i][j] == 0)
                     q.add(new Pair(i,j));
-                } else {
-                    res[i][j]=-1;
-                }
             }
         }
 
-        bfs(res,mat,q,m,n);
-        
+        bfs(q,res,mat,m,n);
         return res;
     }
 
-    public void bfs(int[][] res,int[][] mat,Queue<Pair> q,int m,int n) {
-        int steps=1;
-        int[] dRow={-1,0,1,0};
-        int[] dCol={0,1,0,-1};
+    public void bfs(Queue<Pair> q, int[][] res,int[][] matrix,int m,int n) {
+        int dist = 1;
 
         while (!q.isEmpty()) {
-            int qlen=q.size();
+            int qlen = q.size();
 
             while (qlen-- > 0) {
-                Pair curr=q.poll();
-                int currRow=curr.row;
-                int currCol=curr.col;
-
+                Pair curr = q.poll();
+                int currRow = curr.row;
+                int currCol = curr.col;
+                
                 for (int i=0;i<4;i++) {
-                    int newRow=currRow+dRow[i];
-                    int newCol=currCol+dCol[i];
+                    int newRow = currRow + dRow[i];
+                    int newCol = currCol + dCol[i];
 
-                    if (newRow>=0 && newRow<m && newCol>=0 && newCol<n && mat[newRow][newCol]==1 && res[newRow][newCol]==-1) {
-                        res[newRow][newCol]=steps;
-                        q.add(new Pair(newRow,newCol));
+                    if (newRow>=0 && newRow<m && newCol>=0 && newCol<n && matrix[newRow][newCol]==1 && res[newRow][newCol]==0) {
+                        res[newRow][newCol] = dist;
+                        q.add(new Pair(newRow, newCol));
                     }
                 }
             }
 
-            steps++;
+            dist++;
         }
     }
 }
-
-/*
-
-0  0  0
-0  1  0
-1  2  1
-
-
-steps=3
-
-*/
