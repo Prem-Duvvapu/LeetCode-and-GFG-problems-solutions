ecf1e49c35b417812b3df44f3aad893e437957b9
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-08-21 11:41:42 +0530

    Time: 6 ms (72.89%) | Memory: 45.5 MB (46.45%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,87 +1,40 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int n=numCourses;
-        List<List<Integer>> adjList=new ArrayList<>();
-
-        for (int i=0;i<n;i++)
+        int V = numCourses;
+        List<List<Integer>> adjList = new ArrayList<>();
+        int[] indegree = new int[V];
+        Queue<Integer> q = new LinkedList<>();
+        
+        for (int i=0;i<V;i++)
             adjList.add(new ArrayList<>());
-
-        for (int[] e: prerequisites) {
-            int u=e[0];
-            int v=e[1];
-            //v->u
-            adjList.get(v).add(u);
+        
+        for (int[] edge: prerequisites) {
+            int a = edge[0];
+            int b = edge[1];
+            
+            adjList.get(b).add(a);
+            indegree[a]++;
         }
-
-        if (isCycle(adjList,n))
-            return false;
-
-        return true;
-    }
-
-    public boolean isCycle(List<List<Integer>> adjList,int n) {
-        boolean[] visited=new boolean[n];
-        boolean[] path=new boolean[n];
-
-        for (int i=0;i<n;i++)
-            if (!visited[i])
-                if (dfs(i,visited,path,adjList,n))
-                    return true;
-
-        return false;
-    }
-
-    public boolean dfs(int node,boolean[] visited,boolean[] path,List<List<Integer>> adjList,int n) {
-        visited[node]=true;
-        path[node]=true;
-
-        for (int ngbr: adjList.get(node)) {
-            if (!visited[ngbr]) {
-                if (dfs(ngbr,visited,path,adjList,n))
-                    return true;
-            } else if (path[ngbr]) {
-                return true;
+        
+        
+        for (int i=0;i<V;i++)
+            if (indegree[i] == 0)
+                q.add(i);
+                
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            for (int ngbr: adjList.get(curr)) {
+                indegree[ngbr]--;
+                
+                if (indegree[ngbr] == 0)
+                    q.add(ngbr);
             }
         }
-
-        path[node]=false;
-        return false;
+        
+        for (int i=0;i<V;i++)
+            if (indegree[i] > 0)
+                return false;
+                
+        return true;
     }
 }
-
-/*
-numCourses
-
-0 to numCourses-1
-
-[a,b]
-b->a
-
-2
-0,1
-
-[1,0]
-0->1 true
-
-2
-0,1
-[1,0],[0,1]
-0->1
-1->0
-
-false
-
-0->1
-^   |
-|   >2
-3<----
-
-visited[] = [1, 1, 1, 1]
-             0  1  2  3
-
-path[] = [1, 1, 1, 1]
-          0  1  2  3
-return true
-
-
-*/
