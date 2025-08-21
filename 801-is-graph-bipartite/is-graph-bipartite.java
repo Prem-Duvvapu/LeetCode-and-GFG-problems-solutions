df0a330b1fe4d242a6b8e82b4cf28ca1bd182dc8
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-08-21 11:27:45 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (66.96%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,25 +1,28 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        int n=graph.length;
-        int[] color=new int[n];
-        Arrays.fill(color,-1);
+        int n = graph.length;
+        int[] color = new int[n];
 
-        for (int i=0;i<n;i++)
-            if (color[i]==-1)
-                if (!dfs(i,0,color,graph,n))
+        Arrays.fill(color, -1);
+
+        for (int i=0;i<n;i++) {
+            if (color[i] == -1) {
+                if (!dfs(i,0,color,graph))
                     return false;
+            }
+        }
 
         return true;
     }
 
-    public boolean dfs(int node,int currNodeColor,int[] color,int[][] graph,int n) {
-        color[node]=currNodeColor;
+    private boolean dfs(int curr,int currColor,int[] color,int[][] graph) {
+        color[curr] = currColor;
 
-        for (int ngbr: graph[node]) {
-            if (color[ngbr]==-1) {
-                if (!dfs(ngbr,1-currNodeColor,color,graph,n))
+        for (int ngbr: graph[curr]) {
+            if (color[ngbr] == -1) {
+                if (!dfs(ngbr,1 - currColor,color,graph))
                     return false;
-            } else if (color[node]==color[ngbr]) {
+            } else if (color[ngbr] == currColor) {
                 return false;
             }
         }
@@ -27,31 +30,3 @@ class Solution {
         return true;
     }
 }
-
-
-/*
-
-
-
-0   1
-0 - 1
-|   \
-|    4 0
-|    /
-2 - 3
-1   0
-
-//a linear graph is always bipartitie
-0-1-2-3
-
-//a graph with even length cycle is also bipartite
-
-//a graph with odd length cycle is not bipartite
-
-color = [0,  1, 1, 0, 0]
-          0  1  2  3  4
-
-colors = {0,1}
-
-q=[ 4,3]
-*/
