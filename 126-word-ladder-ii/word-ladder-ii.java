a3a68f9e40daafa6158adb87e7abdd78aded821f
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-08-28 13:41:41 +0530

    Time: 47 ms (5.10%) | Memory: 45.9 MB (12.95%) - LeetSync

--- 126-word-ladder-ii/word-ladder-ii.java
+++ 126-word-ladder-ii/word-ladder-ii.java
@@ -1,71 +1,64 @@
 class Solution {
-    Queue<String> q;
-    Map<String,Integer> map;
-    List<List<String>> res;
-    int level;
-    int minSteps;
-    
-    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-        q = new ArrayDeque<>();
-        map = new HashMap<>();
-        Set<String> set = new HashSet<>();
-        res = new ArrayList<>();
+    List<List<String>> res = new ArrayList<>();
+    Set<String> set = new HashSet<>();
+    Map<String,Integer> map = new HashMap<>();
+    int minSteps = -1;
 
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        Queue<String> q = new LinkedList<>();
         for (String word: wordList)
             set.add(word);
 
-
+        int level = 1;
+        map.put(beginWord, level);
         q.add(beginWord);
-        level = 1;
-        minSteps = 0;
-        map.put(beginWord,level);
         set.remove(beginWord);
 
         while (!q.isEmpty()) {
             String curr = q.poll();
+
             if (curr.equals(endWord)) {
                 minSteps = map.get(curr);
                 break;
             }
-            
+
             level = map.get(curr)+1;
             for (int i=0;i<curr.length();i++) {
-                StringBuilder temp = new StringBuilder(curr);
                 for (char ch='a';ch<='z';ch++) {
-                    temp.setCharAt(i,ch);
-                    if (set.contains(temp.toString())) {
-                        q.add(temp.toString());
-                        map.put(temp.toString(),level);
-                        set.remove(temp.toString());
+                    String newWord= curr.substring(0,i)+ch+curr.substring(i+1);
+                    if (set.contains(newWord)) {
+                        q.add(newWord);
+                        map.put(newWord,level);
+                        set.remove(newWord);
                     }
                 }
             }
         }
 
-
-        List<String> temp = new ArrayList<>();
-        temp.add(endWord);
-        solve(endWord, temp);
+        // System.out.println(map);
+        
+        List<String> currList = new ArrayList<>();
+        currList.add(endWord);
+        solve(endWord,currList);
 
         return res;
     }
 
-    public void solve(String word, List<String> currList) {
-        if (currList.size()==minSteps) {
-            List<String> newList = new ArrayList<>(currList);
-            Collections.reverse(newList);
-            res.add(newList);
+    private void solve(String word,List<String> currList) {
+        if (currList.size() == minSteps) {
+            List<String> temp = new ArrayList<>(currList);
+            Collections.reverse(temp);
+            res.add(temp);
             return;
         }
 
         String curr = word;
         for (int i=0;i<curr.length();i++) {
-            StringBuilder temp = new StringBuilder(curr);
             for (char ch='a';ch<='z';ch++) {
-                temp.setCharAt(i,ch);        
-                if (map.containsKey(temp.toString()) && map.get(temp.toString()) < map.getOrDefault(curr,-1)) {
-                    currList.add(temp.toString());
-                    solve(temp.toString(),currList);
+                String newWord= curr.substring(0,i)+ch+curr.substring(i+1);
+                if (map.containsKey(newWord) && map.get(newWord) < map.getOrDefault(curr, -1)) {
+                    currList.add(newWord);
+                    solve(newWord, currList);
                     currList.remove(currList.size()-1);
                 }
             }
