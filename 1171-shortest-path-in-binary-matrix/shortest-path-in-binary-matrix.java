4501b4287e9f99bdba23598646b1e671abe35a41
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-08-30 10:49:45 +0530

    Time: 16 ms (59.88%) | Memory: 45.8 MB (41.10%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,46 +1,53 @@
-class Pair {
+class Tuple {
     int dist;
     int row;
     int col;
-    Pair(int _dist,int _row,int _col) {
-        dist=_dist;
-        row=_row;
-        col=_col;
+
+    Tuple(int dist,int row,int col) {
+        this.dist = dist;
+        this.row = row;
+        this.col = col;
     }
 }
 
 class Solution {
+    public static int[] dRow = {-1,-1,0,1,1,1,0,-1};
+    public static int[] dCol = {0,1,1,1,0,-1,-1,-1};
+
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int n=grid.length;
-        Queue<Pair> q=new LinkedList<>();
-        int[][] dist=new int[n][n];
-
-        for (int[] a: dist)
-            Arrays.fill(a,(int)1e9);
-
-        if (grid[0][0]==1 || grid[n-1][n-1]==1)
+        if (grid[0][0] != 0)
             return -1;
 
-        q.add(new Pair(0,0,0));
-        dist[0][0]=1;
+        int n = grid.length;
+        Queue<Tuple> q = new LinkedList<>();
+        int[][] dist = new int[n][n];
 
-        int[] drow={-1,-1,-1,0,1,1,1,0};
-        int[] dcol={-1,0,1,1,1,0,-1,-1};
+        for (int[] arr: dist)
+            Arrays.fill(arr,Integer.MAX_VALUE);
+
+        dist[0][0] = 1;
+        q.add(new Tuple(dist[0][0],0,0));
 
         while (!q.isEmpty()) {
-            Pair curr=q.poll();
+            Tuple curr = q.poll();
+            int currDist = curr.dist;
+            int currRow = curr.row;
+            int currCol = curr.col;
+
+            if (currRow == n-1 && currCol == n-1)
+                return currDist;
 
             for (int i=0;i<8;i++) {
-                int newRow=curr.row+drow[i];
-                int newCol=curr.col+dcol[i];
+                int newRow = currRow + dRow[i];
+                int newCol = currCol + dCol[i];
 
-                if (newRow>=0 && newRow<n && newCol>=0 && newCol<n && grid[newRow][newCol]==0 && dist[curr.row][curr.col]+1<dist[newRow][newCol]) {
-                    dist[newRow][newCol]=dist[curr.row][curr.col]+1;
-                    q.add(new Pair(dist[newRow][newCol],newRow,newCol));
+                if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < n && grid[newRow][newCol] == 0 && dist[newRow][newCol] > dist[currRow][currCol] + 1) {
+                    dist[newRow][newCol] = dist[currRow][currCol] + 1;
+                    q.add(new Tuple(dist[newRow][newCol], newRow, newCol));
                 }
             }
         }
 
-        return (dist[n-1][n-1]!=(int)1e9) ? dist[n-1][n-1] : -1;
+        return -1;
     }
 }
