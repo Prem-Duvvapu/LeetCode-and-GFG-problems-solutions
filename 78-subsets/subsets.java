11e5607a3e8ad0b885398596e03d6d560a6d146e
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-08-30 12:28:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (65.85%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,45 +1,25 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        int n=nums.length;
-        List<List<Integer>> res=new ArrayList<>();
-        solve(0,new ArrayList<>(),res,nums,n);
+        int n = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> currList = new ArrayList<>();
+
+        solve(0,currList,res,nums);
         return res;
     }
 
-    public void solve(int pos,List<Integer> currList,List<List<Integer>> res,int[] nums,int n) {
-        if (pos==n) {
+    private void solve(int pos,List<Integer> currList,List<List<Integer>> res,int[] nums) {
+        if (pos == nums.length) {
             res.add(new ArrayList<>(currList));
             return;
         }
 
-        //not pick
-        solve(pos+1,currList,res,nums,n);
+        // not pick
+        solve(pos+1, currList, res, nums);
 
-        //pick
+        // pick
         currList.add(nums[pos]);
-        solve(pos+1,currList,res,nums,n);
-        currList.remove(currList.size()-1);
+        solve(pos+1, currList, res, nums);
+        currList.remove(currList.size() - 1);
     }
 }
-
-/*
-1. subarrays - should be consecutive indexes
-[1][2][3][1,2][2,3][1,2,3]
-
-2. subsequences - can be consecutive or increasing order indexes
-[][1][2][3][1,2][1,3][2,3][1,2,3]
-
-3. subsets - any order
-[][1][2][3][1,2][1,3][3,2][3,2,1]
-(1,3) 
-
-
-1. return all subsequences
-2. return subsequences with sum k
-3. return cnt of subsequences with sum k
-
-[1, 2, 3]
-
-pick/not pick
-
-*/
