39ec7a4ca9a4c32883e040761d7c33475477b9a9
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-09-04 13:37:29 +0530

    Time: 12 ms (76.81%) | Memory: 49.1 MB (59.30%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
@@ -1,60 +1,60 @@
 class Pair {
     long time;
-    long node;
+    int node;
 
-    Pair(long time, long node) {
-        this.time=time;
-        this.node=node;
+    Pair(long time,int node) {
+        this.time = time;
+        this.node = node;
     }
 }
 
 class Solution {
-    int mod = (int)1e9+7;
+    public static final int MOD = 1_000_000_007;
 
     public int countPaths(int n, int[][] roads) {
-        long[] time=new long[n];
-        long[] numberOfWays=new long[n];
-        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b) -> Long.compare(a.time,b.time));
-        List<List<Pair>> adjList=new ArrayList<>();
+        List<List<Pair>> adjList = new ArrayList<>();
+        long[] timeArr = new long[n];
+        int[] ways = new int[n];
+        PriorityQueue<Pair> pq = new PriorityQueue<>((x,y) -> Long.compare(x.time, y.time));
+        int src = 0;
+        int dest = n-1;
+
+        Arrays.fill(timeArr, Long.MAX_VALUE);
 
         for (int i=0;i<n;i++)
             adjList.add(new ArrayList<>());
 
-        for (int[] road: roads) {
-            adjList.get(road[0]).add(new Pair((long)road[2],(long)road[1]));
-            adjList.get(road[1]).add(new Pair((long)road[2],(long)road[0]));
+        for (int[] e: roads) {
+            int u = e[0];
+            int v = e[1];
+            int t = e[2];
+
+            adjList.get(u).add(new Pair(t,v));
+            adjList.get(v).add(new Pair(t,u));
         }
 
-        Arrays.fill(time, Long.MAX_VALUE);
-        pq.add(new Pair(0L,0L));
-        time[0]=0L;
-        numberOfWays[0]=1L;
+        timeArr[src] = 0;
+        ways[src] = 1;
+        pq.add(new Pair(0,src));
 
         while (!pq.isEmpty()) {
-            Pair curr=pq.poll();
-            long currTime=curr.time;
-            long currNode=curr.node;
-            // System.out.println(currTime+" "+currNode);
-            // System.out.println("time "+Arrays.toString(time));
-            // System.out.println("ways "+Arrays.toString(numberOfWays));
-            // System.out.println("total pairs "+adjList.get(currNode).size());
+            Pair curr = pq.poll();
+            long currTime = curr.time;
+            int currNode = curr.node;
 
-            for (Pair p: adjList.get((int)currNode)) {
-                long newNode=p.node;
-                long newTime=p.time+currTime;
-                // System.out.println("pair "+newTime+" "+newNode);
+            for (Pair ngbr: adjList.get(currNode)) {
+                long newTime = currTime + ngbr.time;
 
-                if (newTime<time[(int)newNode]) {
-                    time[(int)newNode]=newTime;
-                    numberOfWays[(int)newNode]=numberOfWays[(int)currNode];
-                    pq.add(new Pair(newTime,newNode));
-                }
-                else if (newTime==time[(int)newNode]) {
-                    numberOfWays[(int)newNode] = (numberOfWays[(int)newNode]%mod + numberOfWays[(int)currNode]%mod)%mod;
+                if (newTime < timeArr[ngbr.node]) {
+                    timeArr[ngbr.node] = newTime;
+                    ways[ngbr.node] = ways[currNode];
+                    pq.add(new Pair(timeArr[ngbr.node], ngbr.node));
+                } else if (newTime == timeArr[ngbr.node]) {
+                    ways[ngbr.node] = (ways[ngbr.node] + ways[currNode])%MOD;
                 }
             }
         }
 
-        return (int)numberOfWays[n-1];
+        return ways[dest];
     }
 }
