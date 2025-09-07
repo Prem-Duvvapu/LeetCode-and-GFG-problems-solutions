10e54602b39ff267deda711d28c5f4565127debb
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-09-07 19:10:18 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (94.49%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,20 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
-        if (x==1.0 || n==0)
+        if (x == 1.0 || n == 0)
             return 1.0;
 
-        if (n<0) {
-            n=Math.abs(n);
-            x=1/x;
+        if (n < 0) {
+            x = 1/x;
+            n = Math.abs(n);
         }
 
-        double res=1.0;
-        while (n!=0) {
-            if ((n&1)==1)
-                res*=x;
+        double res = 1.0;
+        while (n != 0) {
+            if ((n&1) == 1)
+                res *= x;
 
-            n/=2;
-            x*=x;
+            n /= 2;
+            x *= x;
         }
 
         return res;
