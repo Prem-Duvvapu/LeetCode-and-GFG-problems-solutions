713bc545dfbac4393f3e6c2c7c1f5a9d42c379f1
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-09-11 13:53:42 +0530

    Time: 984 ms (48.99%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,2 +1,9 @@
 # Write your MySQL query statement below
-Select e.name,b.bonus from Employee e left join Bonus b on e.empId=b.empId where coalesce(b.bonus,0)<1000;
+select
+    e.name,
+    b.bonus
+from Employee e
+left join Bonus b
+on e.empId = b.empId
+where
+    coalesce(b.bonus,0) < 1000;
