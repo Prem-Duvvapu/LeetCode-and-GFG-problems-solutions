bd2e372011cbc2e985a5636367e55ef640ba3cce
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-09-12 19:50:42 +0530

    Time: 8 ms (91.26%) | Memory: 44.7 MB (44.05%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,43 +1,44 @@
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        int res=0;
-        int minCities=n+1;
-        int[][] dist=new int[n][n];
+        int res = -1;
+        int minCitiesCnt = n+1;
+        int[][] dist = new int[n][n];
 
-        for (int i=0;i<n;i++)
-            for (int j=0;j<n;j++)
-                if (i!=j)
-                    dist[i][j]=(int)1e8;
+        for (int i=0;i<n;i++) {
+            for (int j=0;j<n;j++) {
+                if (i != j)
+                    dist[i][j] = (int)1e8;
+            }
+        }
 
         for (int[] e: edges) {
-            int u=e[0];
-            int v=e[1];
-            int wt=e[2];
+            int u = e[0];
+            int v = e[1];
+            int wt = e[2];
 
-            dist[u][v]=wt;
-            dist[v][u]=wt;
+            dist[u][v] = wt;
+            dist[v][u] = wt;
         }
 
         for (int k=0;k<n;k++) {
             for (int i=0;i<n;i++) {
                 for (int j=0;j<n;j++) {
-                    if (dist[i][k]+dist[k][j]<dist[i][j])
-                        dist[i][j]=dist[i][k]+dist[k][j];
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
 
-
         for (int i=0;i<n;i++) {
-            int cnt=0;
+            int currCitiesCnt = 0;
             for (int j=0;j<n;j++) {
-                if (i!=j && dist[i][j]<=distanceThreshold)
-                    cnt++;
+                if (i != j && dist[i][j] <= distanceThreshold) {
+                    currCitiesCnt++;
+                }
             }
 
-            if (cnt<=minCities) {
-                minCities=cnt;
-                res=i;
+            if (currCitiesCnt <= minCitiesCnt) {
+                minCitiesCnt = currCitiesCnt;
+                res = i;
             }
         }
 
