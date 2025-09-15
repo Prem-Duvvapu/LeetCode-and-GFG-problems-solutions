a8f97d94e63980f63f58bca95a35f74173003ce6
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-09-15 18:58:07 +0530

    Time: 1365 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,8 +1,12 @@
-select
-    distinct(l1.num) as ConsecutiveNums
-from Logs l1, Logs l2, Logs l3
-where
-    l1.num = l2.num and 
-    l2.num = l3.num and
-    l2.id = l1.id+1 and
-    l3.id = l2.id+1;
+select distinct l1.num as ConsecutiveNums
+from Logs l1
+where l1.num = (
+    select l2.num
+    from Logs l2
+    where l2.id = l1.id + 1
+) 
+and l1.num = (
+    select l3.num
+    from Logs l3
+    where l3.id = l1.id + 2
+);
