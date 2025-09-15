893e69917273c22e375b431aacc248f65d605334
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-09-15 14:21:36 +0530

    Time: 375 ms (18.22%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
+++ 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
@@ -1,6 +1,1 @@
-select
-    distinct(class)
-from Courses
-where class in (
-    select class from Courses group by class having count(*) >= 5
-);
+select class from Courses group by class having count(*) >= 5;
