8fc253cd17da2f16ea5c76c1d1ad11d1b24ebd45
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-09-15 14:17:26 +0530

    Time: 799 ms (31.47%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -3,10 +3,7 @@ select
     year as first_year,
     quantity,
     price
-from
-    Sales
+from Sales s
 where (product_id, year) in (
-    select product_id,min(year)
-    from Sales
-    group by product_id
-)
+    select product_id, min(year) from Sales group by product_id
+);
