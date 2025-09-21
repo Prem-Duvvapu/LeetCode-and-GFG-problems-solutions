c94e45ca04f82273ffd144000ed86d79fe677da7
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-09-21 13:55:17 +0530

    Time: 139 ms (100.00%) | Memory: 45.7 MB (100.00%) - LeetSync

---
+++ 3928-split-and-merge-array-transformation/split-and-merge-array-transformation.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int minSplitMerge(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        Set<String> set= new HashSet<>();
+        Queue<String> q = new LinkedList<>();
+        int level = 0;
+        String src = "";
+
+        for (int i=0;i<n;i++)
+            src += ""+i;
+
+        q.add(src);
+        set.add(src);
+
+        while (!q.isEmpty()) {
+            int qlen = q.size();
+
+            while (qlen-- > 0) {
+                String curr = q.poll();
+                boolean match = true;
+
+                for (int i=0;i<n;i++)
+                    if (nums1[curr.charAt(i) - '0'] != nums2[i])
+                        match = false;
+
+                if (match)
+                    return level;
+
+                for (int i=0;i<n;i++) {
+                    for (int j=i;j<n;j++) {
+                        String base = curr.substring(0,i) + curr.substring(j+1);
+                        String sub = curr.substring(i,j+1);
+
+                        for (int k=0;k<base.length();k++) {
+                            StringBuilder sb = new StringBuilder(base);
+                            sb.insert(k,sub);
+
+                            if (set.contains(sb.toString()))
+                                continue;
+
+                            q.add(sb.toString());
+                            set.add(sb.toString());
+                        }
+
+                        StringBuilder sb = new StringBuilder(base);
+                            sb.append(sub);
+
+                            if (set.contains(sb.toString()))
+                                continue;
+
+                            q.add(sb.toString());
+                            set.add(sb.toString());
+                    }
+                }
+            }
+
+            level++;
+        }
+
+
+        return -1;
+    }
+}
