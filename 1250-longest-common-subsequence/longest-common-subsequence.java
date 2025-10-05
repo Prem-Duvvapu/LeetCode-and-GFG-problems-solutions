c751505a24571c32700bcfaa4d4f5d8f1ec8a671
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2025-10-05 11:22:51 +0530

    Time: 20 ms (74.59%) | Memory: 51.5 MB (10.25%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -3,10 +3,37 @@ class Solution {
         int n1=text1.length();
         int n2=text2.length();
         int[][] dp = new int[n1][n2];
-        for (int[] arr: dp)
-            Arrays.fill(arr,-1);
+        
+        if (text1.charAt(0) == text2.charAt(0))
+            dp[0][0] = 1;
 
-        return solve(n1-1,n2-1,text1,text2,dp);
+        for (int i=1;i<n1;i++) {
+            if (text1.charAt(i) == text2.charAt(0))
+                dp[i][0] = 1;
+            else
+                dp[i][0] = dp[i-1][0];
+        }
+
+        for (int j=1;j<n2;j++) {
+            if (text1.charAt(0) == text2.charAt(j))
+                dp[0][j] = 1;
+            else
+                dp[0][j] = dp[0][j-1];
+        }
+
+        for (int i=1;i<n1;i++) {
+            for (int j=1;j<n2;j++) {
+                if (text1.charAt(i)==text2.charAt(j)) {
+                    dp[i][j] = 1+dp[i-1][j-1];
+                } else {
+                    int moveI=dp[i-1][j];
+                    int moveJ=dp[i][j-1];
+                    dp[i][j] = Math.max(moveI,moveJ);
+                }
+            }
+        }
+
+        return dp[n1-1][n2-1];
     }
 
     public int solve(int i,int j,String text1,String text2,int[][] dp) {
