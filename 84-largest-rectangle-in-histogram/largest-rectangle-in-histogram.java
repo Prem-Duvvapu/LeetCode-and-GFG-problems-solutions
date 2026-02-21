1346c75c60d54a71bd29756f9b901a2ebc9a789b
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2026-02-21 09:49:42 +0530

    Time: 61 ms (75.24%) | Memory: 78.4 MB (46.22%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -6,28 +6,26 @@ class Solution {
 
         for (int i=0;i<n;i++) {
             while (!stack.isEmpty() && heights[stack.peek()]>heights[i]) {
-                int height=heights[stack.pop()];
-                int pse=stack.isEmpty() ? -1 : stack.peek();
-                int nse=i;
-                int width=nse-pse-1;
-                int currArea=height*width;
-
-                maxArea=Math.max(maxArea,currArea);
+                maxArea = findMaxArea(heights, stack, i, maxArea);
             }
 
             stack.push(i);
         }
 
         while (!stack.isEmpty()) {
-            int height=heights[stack.pop()];
-            int pse=stack.isEmpty() ? -1 : stack.peek();
-            int nse=n;
-            int width=nse-pse-1;
-            int currArea=height*width;
-
-            maxArea=Math.max(maxArea,currArea);
+            maxArea = findMaxArea(heights, stack, n, maxArea);
         }
 
         return maxArea;
+    }
+
+    private int findMaxArea(int[] heights, Stack<Integer> stack, int val, int maxArea) {
+        int height=heights[stack.pop()];
+        int pse=stack.isEmpty() ? -1 : stack.peek();
+        int nse=val;
+        int width=nse-pse-1;
+        int currArea=height*width;
+
+        return Math.max(maxArea, currArea);
     }
 }
