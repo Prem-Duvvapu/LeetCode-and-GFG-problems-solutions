3207a57a83564fdf12dc6685203597da837be1c0
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2026-06-07 17:11:21 +0530

    Time: 2 ms (94.96%) | Memory: 45.6 MB (81.44%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,44 +1,28 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        int n=candidates.length;
-        List<List<Integer>> res=new ArrayList<>();
-
-        solve(0,new ArrayList<>(),target,res,candidates,n);
-
+        List<List<Integer>> res = new ArrayList<>();
+        solve(0,target,new ArrayList<>(),res,candidates);
         return res;
     }
 
-    public void solve(int pos,List<Integer> currList,int req,List<List<Integer>> res,int[] candidates,int n) {
-        if (req==0) {
+    private void solve(int pos,int target,List<Integer> currList,List<List<Integer>> res,int[] candidates) {
+        if (target == 0) {
             res.add(new ArrayList<>(currList));
             return;
         }
 
-        if (req<0)
+        if (pos >= candidates.length) {
             return;
+        }
 
-        if (pos==n)
-            return;
+        // not pick
+        solve(pos+1,target,currList,res,candidates);
 
-        //pick
-        currList.add(candidates[pos]);
-        solve(pos,currList,req-candidates[pos],res,candidates,n);
-        currList.remove(currList.size()-1);
-
-        //not pick
-        solve(pos+1,currList,req,res,candidates,n);
+        // pick and stay
+        if (candidates[pos] <= target) {
+            currList.add(candidates[pos]);
+            solve(pos,target-candidates[pos],currList,res,candidates);
+            currList.remove(currList.size()-1);
+        }
     }
 }
-
-
-/*
-time: O(2^x)
-space: O(target) + O(x*y)
-
-
-
-0 -0
-   1
-   2
-   3
-*/
