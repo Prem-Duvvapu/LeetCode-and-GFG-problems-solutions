29c6960efe3df4c8e9158476d10ec8ea27b58c58
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2026-06-07 15:42:30 +0530

    Time: 28 ms (20.93%) | Memory: 48.2 MB (15.46%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.java
+++ 402-remove-k-digits/remove-k-digits.java
@@ -1,122 +1,44 @@
 class Solution {
     public String removeKdigits(String num, int k) {
-        int n=num.length();
-        Stack<Character> stack=new Stack<>();
-        Stack<Character> stack2=new Stack<>();
+        if ("0".equals(num)) {
+            return "0";
+        }
 
-        for (char ch: num.toCharArray()) { //O(2*n)
-            while (!stack.isEmpty() && stack.peek()>ch && k>0) {
-                stack.pop();
-                k--;
+        Stack<Character> stack = new Stack<>();
+        for (char ch: num.toCharArray()) {
+            while (!stack.isEmpty() && ch < stack.peek()) {
+                if (k > 0) {
+                    stack.pop();
+                    k--;
+                } else {
+                    break;
+                }
             }
-
             stack.push(ch);
         }
 
-        //O(n)
-        while (k-- > 0)
+        while (!stack.isEmpty() && k>0) {
             stack.pop();
-        
-        //O(n)
-        while (!stack.isEmpty())
-            stack2.push(stack.pop());
+            k--;
+        }
 
-        //O(n)
-        while (!stack2.isEmpty() && stack2.peek()=='0')
-            stack2.pop();
-
-        if (stack2.isEmpty())
+        if (stack.isEmpty()) {
             return "0";
+        }
 
-        StringBuilder res=new StringBuilder();
-        //O(n)
-        while (!stack2.isEmpty())
-            res.append(stack2.pop());
+        StringBuilder res = new StringBuilder();
+        while (!stack.isEmpty()) {
+            res.append(stack.pop());
+        }
 
-        return res.toString();
+        while (res.length()>0 && res.charAt(res.length()-1) == '0') {
+            res.deleteCharAt(res.length()-1);
+        }
+
+        if (res.length() == 0) {
+            return "0";
+        }
+
+        return res.reverse().toString();
     }
 }
-
-
-/*
-num = 1 4 3 2 2 1 9
-      0 1 2 3 4 5 6
-                  ^
-k = 1
-initialK=6
-n=7
-
-ans = oneDigit
-
-1*10,00,000 + 4*1,00,000 + 3*10,000 + 2*1,000 + 2*100 + 1*10 + 9
-
-a*1000+b*100+c*10+d
-
-4*1000
-
-5           1
-stack      val
-index
-
-1
-
-
-nums = 7 5 3
-k=0
-expected_ans=53
-
-3
-5
-
-
-nums=357
-k=2
-
-expected_ans=3
-
-7
-5
-3
-
-3 3 3
-3 5 7
-
-
-
-
-
-
- 6      9  9
- 5      1  10
- 3      2  200
- 0      1  1000
-
-stack  val
-index
-
-9121
-
-1219
-
-
-1000  k=1   expected_ans=0
-
-1000  k=3  expected_ans=0
-
-10000000789 k=3  expected_ans=
-
-
-7
-
-9
-8
-7
-0
-
-
-987
-
-789
-
-
-*/
