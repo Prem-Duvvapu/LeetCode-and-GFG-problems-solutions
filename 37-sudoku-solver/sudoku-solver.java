9bb3d4048751d3ab818eca61095f3c3ee199672f
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2026-06-14 00:12:58 +0530

    Time: 333 ms (45.40%) | Memory: 42.4 MB (95.79%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -3,106 +3,53 @@ class Solution {
         solve(0,0,board);
     }
 
-    public boolean solve(int r,int c,char[][] board) {
-        if (r==8 && c==9)
+    private boolean solve(int r,int c,char[][] board) {
+        if (c == board[0].length) {
+            c = 0;
+            r++;
+        }
+
+        if (r >= board.length) {
             return true;
-
-        if (c==9) {
-            r+=1;
-            c=0;
         }
 
-        if (board[r][c]=='.') {
-            for (char ch='1';ch<='9';ch++) {
-                if (isSafe(r,c,ch,board)) {
-                    board[r][c]=ch;
+        if (board[r][c] != '.') {
+            return solve(r,c+1,board);
+        }
 
-                    if (solve(r,c+1,board))
-                        return true;
-                        
-                    board[r][c]='.';
+        for (char ch ='1';ch<='9';ch++) {
+            if (isValid(ch,r,c,board)) {
+                board[r][c] = ch;
+                if (solve(r,c+1,board)) {
+                    return true;
                 }
+                board[r][c] = '.';
             }
-            return false;
         }
-        
-        return solve(r,c+1,board);
+
+        return false;
     }
 
-    public boolean isSafe(int r,int c,char ch,char[][] board) {
-        for (int i=0;i<9;i++) {
-            //row check
-            if (board[r][i]==ch)
+    private boolean isValid(char ch,int r,int c,char[][] board) {
+        for (int i=0;i<board.length;i++) {
+            // colCheck
+            if (board[i][c] == ch) {
                 return false;
+            }
 
-            //col check
-            if (board[i][c]==ch)
+            // rowCheck
+            if (board[r][i] == ch) {
                 return false;
+            }
 
-            //sub-box
-            int newRow = 3 * (r/3) + i/3; 
+            // gridCheck
+            int newRow = 3 * (r/3) + i/3;
             int newCol = 3 * (c/3) + i%3;
-
-            if (board[newRow][newCol]==ch)
+            if (board[newRow][newCol] == ch) {
                 return false;
+            }
         }
 
         return true;
     }
 }
-
-/*
-time:  O(9^81) 1 to 9 at every empty cell
-space: O(n*n) recursive stack space
-_ _ _    _
-9 9 9 
-
-9^81
-
-(0,3) (0,4) (0,5)
-(1,3) (1,4) (1,5)
-(2,3) (2,4) (2,5)
-
-gridNum=1
-
-i
-0 - (0,3)
-1 - (0,4)
-2 - (0,5)
-3 - (1,3)
-.
-.
-.
-8 - (2,5)
-
-r, c, i
-
-r -> 0 to 2
-c -> 3 to 5
-
-r=1 c=5 
-
-newRow=i/3
-newCol=3+i%3
-
-i   r  c
-0   0  3
-1   0  4
-2   0  5
-
-3   1  3
-4   1  4
-5   1  5
-
-6   2  3
-7   2  4
-8   2  5
-
-(r/3)*3+c/3
-
-0 1 2
-3 
-
-
-
-*/
