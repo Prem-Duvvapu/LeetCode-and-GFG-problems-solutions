cb6ade37598729834bb16d7f3bd4dd4d1dcec820
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2026-06-18 01:20:27 +0530

    Time: 6 ms (67.88%) | Memory: 46.5 MB (11.68%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,37 +1,31 @@
 class Solution {
-    Map<String,Boolean> map;
-    public boolean func(int idx,String s,StringBuilder sb,HashSet<String> hs){
-        if(idx==s.length()){
-            if(hs.contains(sb.toString())){
-                return true;
-            }
-            return false;
-        }
-
-        String key=idx+" "+sb.toString();
-        if (map.containsKey(key))
-            return map.get(key);
-        
-        if(!hs.contains(sb.toString())){
-            sb.append(s.charAt(idx));
-            return func(idx+1,s,sb,hs);
-        }
-        
-        sb.append(s.charAt(idx));
-        boolean x1=func(idx+1,s,sb,hs);
-        sb.setLength(0);
-        boolean x2=func(idx,s,sb,hs);
-        map.put(key,(x1 || x2));
-        return x1 || x2;
-    }
     public boolean wordBreak(String s, List<String> wordDict) {
-        map=new HashMap<>();
-        HashSet<String> hs=new HashSet<>();
-        for(int i=0;i<wordDict.size();i++){
-            hs.add(wordDict.get(i));
+        Boolean[] dp = new Boolean[s.length()];
+        Set<String> wordSet = new HashSet<>();
+        for (String word: wordDict) {
+            wordSet.add(word);
         }
 
-        return func(0,s,new StringBuilder(),hs);
-        
+        return solve(0,s,wordSet,dp);
+    }
+
+    private boolean solve(int pos,String s,Set<String> wordSet,Boolean[] dp) {
+        if (pos == s.length()) {
+            return true;
+        }
+
+        if (dp[pos] != null) {
+            return dp[pos];
+        }
+
+        for (int i=pos;i<s.length();i++) {
+            if (wordSet.contains(s.substring(pos,i+1))) {
+                if (solve(i+1,s,wordSet,dp)) {
+                    return dp[pos] = true;
+                }
+            }
+        }
+
+        return dp[pos] = false;
     }
 }
