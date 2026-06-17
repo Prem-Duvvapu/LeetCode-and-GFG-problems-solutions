14db984a1d7de446693fb12a29ecdd3565dd8ac6
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2026-06-18 01:46:20 +0530

    Time: 1 ms (99.92%) | Memory: 46.4 MB (91.65%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,45 +1,71 @@
 class Solution {
-    private void solve(int col,int n,char[][] chessBoard,boolean[] rowCheck,boolean[] lowerDiagonalCheck,boolean[] upperDiagonalCheck,List<List<String>> res) {
-        if (col==n) {
-            List<String> currList=new ArrayList<>();
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<>();
+        boolean[] isRowFilled = new boolean[n];
+        boolean[] isUpperDiagFilled = new boolean[2*n];
+        boolean[] isLowerDiagFilled = new boolean[2*n];
 
-            for (char[] arr: chessBoard)
-                currList.add(new String(arr));
+        char[][] board = new char[n][n];
+        for (char[] row: board) {
+            Arrays.fill(row, '.');
+        }
 
-            res.add(currList
-            );
+        solve(0,isRowFilled,isUpperDiagFilled,isLowerDiagFilled,board,n,res);
+        return res;
+    }
+
+    private void solve(int col,boolean[] isRowFilled,boolean[] isUpperDiagFilled,boolean[] isLowerDiagFilled, char[][] board,int n,List<List<String>> res) {
+        if (col == n) {
+            List<String> currBoard = new ArrayList<>();
+            for (char[] row: board) {
+                String currRow = new String(row);
+                currBoard.add(currRow);
+            }
+            res.add(currBoard);
             return;
         }
 
-        for (int row=0;row<n;row++) {
-            if (!rowCheck[row] && !lowerDiagonalCheck[row+col] && !upperDiagonalCheck[n-1+row-col]) {
-                chessBoard[row][col]='Q';
-                rowCheck[row]=true;
-                lowerDiagonalCheck[row+col]=true;
-                upperDiagonalCheck[n-1+row-col]=true;
+        for (int i=0;i<n;i++) {
+            if (isValid(i,col,isRowFilled,isUpperDiagFilled,isLowerDiagFilled,n)) {
+                board[i][col] = 'Q';
+                fillCells(i,col,isRowFilled,isUpperDiagFilled,isLowerDiagFilled,n);
 
-                solve(col+1,n,chessBoard,rowCheck,lowerDiagonalCheck,upperDiagonalCheck,res);
+                solve(col+1,isRowFilled,isUpperDiagFilled,isLowerDiagFilled,board,n,res);
 
-                chessBoard[row][col]='.';
-                rowCheck[row]=false;
-                lowerDiagonalCheck[row+col]=false;
-                upperDiagonalCheck[n-1+row-col]=false;
+                board[i][col] = '.';
+                unFillCells(i,col,isRowFilled,isUpperDiagFilled,isLowerDiagFilled,n);
             }
         }
     }
 
-    public List<List<String>> solveNQueens(int n) {
-        List<List<String>> res=new ArrayList<>();
-        char[][] chessBoard=new char[n][n];
-        boolean[] rowCheck=new boolean[n];
-        boolean[] lowerDiagonalCheck=new boolean[2*n-1];
-        boolean[] upperDiagonalCheck=new boolean[2*n-1];
+    private boolean isValid(int row,int col,boolean[] isRowFilled,boolean[] isUpperDiagFilled,boolean[] isLowerDiagFilled,int n) {
+        // rowCheck
+        if (isRowFilled[row]) {
+            return false;
+        }
 
-        for (int i=0;i<n;i++)
-            for (int j=0;j<n;j++)
-                chessBoard[i][j]='.';
+        // upperDiag check
+        if (isUpperDiagFilled[n-1+row-col]) {
+            return false;
+        }
 
-        solve(0,n,chessBoard,rowCheck,lowerDiagonalCheck,upperDiagonalCheck,res);
-        return res;
+        // lowerDiag check
+        if (isLowerDiagFilled[row+col]) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void fillCells(int row,int col,boolean[] isRowFilled,boolean[] isUpperDiagFilled,boolean[] isLowerDiagFilled,int n) {
+        isRowFilled[row] = true;
+        isUpperDiagFilled[n-1+row-col] = true;
+        isLowerDiagFilled[row+col] = true;
+    }
+
+    private void unFillCells(int row,int col,boolean[] isRowFilled,boolean[] isUpperDiagFilled,boolean[] isLowerDiagFilled,int n) {
+        isRowFilled[row] = false;
+        isUpperDiagFilled[n-1+row-col] = false;
+        isLowerDiagFilled[row+col] = false;
     }
 }
