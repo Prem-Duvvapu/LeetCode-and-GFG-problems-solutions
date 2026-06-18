fcc03f11c7ed6d3c03f11f6b095930afe88fe8f7
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2026-06-19 01:07:18 +0530

    Time: 7 ms (98.00%) | Memory: 65.1 MB (73.13%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,32 +1,30 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        int n=s.length();
-        List<List<String>> res=new ArrayList<>();
-        solve(0,new ArrayList<>(),res,s,n);
-
+        List<List<String>> res = new ArrayList<>();
+        solve(0,new ArrayList<>(),s,res);
         return res;
     }
 
-    public void solve(int pos,List<String> currList,List<List<String>> res,String s,int n) {
-        if (pos==n) {
+    private void solve(int pos,List<String> currList,String s,List<List<String>> res) {
+        if (pos == s.length()) {
             res.add(new ArrayList<>(currList));
             return;
         }
 
-        for (int i=pos;i<n;i++) {
+        for (int i=pos;i<s.length();i++) {
             if (isPalindrome(s,pos,i)) {
                 currList.add(s.substring(pos,i+1));
-                solve(i+1,currList,res,s,n);
+                solve(i+1,currList,s,res);
                 currList.remove(currList.size()-1);
             }
         }
     }
 
-    public boolean isPalindrome(String s,int left,int right) {
-        while (left<right) {
-            if (s.charAt(left)!=s.charAt(right))
+    private boolean isPalindrome(String s,int left,int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
                 return false;
-
+            }
             left++;
             right--;
         }
