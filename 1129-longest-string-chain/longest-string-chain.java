f7b5dab949e92739fce638f4170cd7b67b967a82
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2026-08-29 11:52:37 +0530

    Time: 52 ms (49.52%) | Memory: 46 MB (97.42%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.java
+++ 1129-longest-string-chain/longest-string-chain.java
@@ -1,46 +1,39 @@
 class Solution {
     public int longestStrChain(String[] words) {
-        int n=words.length;
-        Arrays.sort(words,(a,b)->(a.length()-b.length()));
-        int[] dp=new int[n];
+        int n = words.length;
+        Arrays.sort(words, (a,b) -> Integer.compare(a.length(),b.length()));
+        int[] dp = new int[n];
         Arrays.fill(dp,1);
-        int maxVal=0;
+        int maxChain = 0;
 
-        for (int i=0;i<n;i++)
-        {
-            for (int j=0;j<i;j++)
-            {
-                if (isPredecessor(words[j],words[i]) && dp[j]+1>dp[i])
-                    dp[i]=dp[j]+1;
+        for (int curr=0;curr<n;curr++) {
+            for (int prev=0;prev<curr;prev++) {
+                if (isPredecessor(words[curr], words[prev])) {
+                    dp[curr] = Math.max(dp[curr], 1 + dp[prev]);
+                }
             }
-            maxVal=Math.max(maxVal,dp[i]);
+
+            maxChain = Math.max(maxChain, dp[curr]);
         }
 
-        return maxVal;
+        return maxChain;
     }
 
-    private boolean isPredecessor(String t,String s)
-    {
-        if (s.length()!=t.length()+1)
+    public boolean isPredecessor(String longer, String shorter) {
+        if (longer.length() - shorter.length() != 1) {
             return false;
-
-        int i=0;//for s
-        int j=0;//for t
-
-        while (i<s.length())
-        {
-            if (s.charAt(i)==t.charAt(j))
-            {
-                i++;
-                j++;
-            }
-            else
-                i++;
-
-            if (j==t.length())
-                return true;
         }
 
-        return false;
+        int i = 0;
+        int j = 0;
+        while (i < longer.length() && j < shorter.length()) {
+            if (longer.charAt(i) == shorter.charAt(j)) {
+                j++;
+            }
+
+            i++;
+        }
+
+        return (j == shorter.length());
     }
 }
