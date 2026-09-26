5532e810445f4b1cd6aa48624ef6f60e6e81a6a3
Prem Duvvapu <95603241+Prem-Duvvapu@users.noreply.github.com>
2026-09-26 23:37:07 +0530

    Time: 1 ms (100.00%) | Memory: 43.4 MB (99.54%) - LeetSync

--- 1009-pancake-sorting/pancake-sorting.java
+++ 1009-pancake-sorting/pancake-sorting.java
@@ -1,41 +1,31 @@
 class Solution {
     public List<Integer> pancakeSort(int[] arr) {
+        int n = arr.length;
         List<Integer> res = new ArrayList<>();
 
-        for (int size = arr.length; size > 1; size--) {
+        for (int size=n;size>1;size--) {
+            int maxElementPos = 0;
 
-            // Find index of maximum element in arr[0...size-1]
-            int maxIndex = 0;
-
-            for (int i = 1; i < size; i++) {
-                if (arr[i] > arr[maxIndex]) {
-                    maxIndex = i;
+            for (int i=1;i<size;i++) {
+                if (arr[i] == size) {
+                    maxElementPos = i;
                 }
             }
 
-            // Already in correct position
-            if (maxIndex == size - 1) {
+            if (maxElementPos == size-1) {
                 continue;
             }
 
-            // Bring maximum element to front
-            if (maxIndex != 0) {
-                flip(arr, maxIndex + 1);
-                res.add(maxIndex + 1);
-            }
-
-            // Move maximum element to its correct position
-            flip(arr, size);
+            reverse(arr,0,maxElementPos);
+            res.add(maxElementPos+1);
+            reverse(arr,0,size-1);
             res.add(size);
-        }
+        }  
 
         return res;
     }
 
-    private void flip(int[] arr, int k) {
-        int left = 0;
-        int right = k - 1;
-
+    private void reverse(int[] arr,int left,int right) {
         while (left < right) {
             int temp = arr[left];
             arr[left] = arr[right];
